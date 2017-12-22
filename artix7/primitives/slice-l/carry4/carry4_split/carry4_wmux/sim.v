bc3c9162610057e36a4db83655e13914ce4c743f
Tim 'mithro' Ansell <me@mith.ro>
2017-12-22 18:12:36 +0100

    artix7: Fixing the carry muxes.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- artix7/primitives/slice-l/carry4/carry4_split/carry4_wmux/sim.v
+++ artix7/primitives/slice-l/carry4/carry4_split/carry4_wmux/sim.v
@@ -1,22 +1,22 @@
 /* 
  * Generated with mux_gen.py, run the following to regenerate in this directory;
- * ../../../../../../utils/mux_gen.py --outdir . '--name-mux' 'CARRY4_{W}MUX' '--width' '2' '--split-inputs' '--subckt' 'MUXCY'
+ * ../../../../../../utils/mux_gen.py --outdir . '--name-mux' 'CARRY4_{W}MUX' '--width' '2' '--split-inputs' '--subckt' 'MUXCY' '--name-inputs' 'CI,DI'
  */
 
 `include "../../../../../../vpr/muxes/logic/mux2/sim.v"
 
-module CARRY4_{W}MUX(I0, I1, S, O);
+module CARRY4_{W}MUX(CI, DI, S, O);
 
-	input wire I0;
-	input wire I1;
+	input wire CI;
+	input wire DI;
 
 	input wire S;
 
 	output wire O;
 
 	MUX2 mux (
-		.I0(I0),
-		.I1(I1),
+		.I0(CI),
+		.I1(DI),
 		.S0(S),
 		.O(O)
 	);
