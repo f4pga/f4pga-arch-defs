c26f5bd505c87874d61bfeed5601c828bdba1f00
Tim 'mithro' Ansell <me@mith.ro>
2017-12-22 18:28:15 +0100

    vpr: Adding muxes.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

---
+++ vpr/muxes/logic/mux2/sim.v
@@ -0,0 +1,13 @@
+`ifndef VPR_MUXES_LOGIC_MUX2
+`define VPR_MUXES_LOGIC_MUX2
+
+module MUX2(I0, I1, S0, O);
+	input wire I0;
+	input wire I1;
+	input wire S0;
+	output wire O;
+
+	assign O = S0 ? I0 : I1;
+endmodule
+
+`endif
