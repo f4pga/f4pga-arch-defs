62f6b84e1f55962f98286d5e7c2ff2e5681b8b2f
Tim 'mithro' Ansell <me@mith.ro>
2017-12-22 18:18:21 +0100

    artix7: Fixing sim.v on carry4-whole.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- artix7/primitives/slice-l/carry4/carry4_complete/sim.v
+++ artix7/primitives/slice-l/carry4/carry4_complete/sim.v
@@ -5,7 +5,10 @@ module CARRY4_COMPLETE(CO, O, CIN, DI, S);
 	input [3:0] DI;
 	input [3:0] S;
 
-	assign O = S ^ {CO[2:0], CIN};
+	assign O[0] = S ^ CIN;
+	assign O[1] = S ^ CO[0];
+	assign O[2] = S ^ CO[1];
+	assign O[3] = S ^ CO[2];
 	assign CO[0] = S[0] ? CIN : DI[0];
 	assign CO[1] = S[1] ? CO[0] : DI[1];
 	assign CO[2] = S[2] ? CO[1] : DI[2];
