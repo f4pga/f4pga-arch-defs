311fa06389cafb262240b880a80caa2a29ae47a8
Tim 'mithro' Ansell <me@mith.ro>
2018-02-17 14:17:28 -0800

    utils/lib: Tools for channel creation/allocation.

--- utils/lib/asserts.py
+++ utils/lib/asserts.py
@@ -1,2 +1,15 @@
 def assert_eq(a, b):
     assert a == b, "{} ({}) != {} ({})".format(a, repr(a), b, repr(b))
+
+def assert_type(obj, cls, msg="{obj} ({obj!} should be a {cls}, not {objcls}"):
+    """Raise a type error if obj is not an instance of cls."""
+    if not isinstance(obj, cls):
+        raise TypeError(msg.format(obj=obj, objcls=type(obj), cls=cls))
+
+def assert_len_eq(l):
+    """Check all lists in a list are equal length"""
+    # Sanity check
+    max_len = max(len(p) for p in l)
+    for i, p in enumerate(l):
+        assert len(p) == max_len, "Length check failed!\nl[{}] has {} elements != {} ({!r})\n{!r}".format(
+            i, len(p), max_len, p, l)
---
+++ utils/lib/rr_graph/__init__.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+
+class static_property(object):
+    """
+    Descriptor (non-data) for building an attribute on-demand on first use.
+    """
+    def __init__(self, factory):
+        """
+        <factory> is called such: factory(instance) to build the attribute.
+        """
+        self._attr_name = factory.__name__
+        self._factory = factory
+
+    def __get__(self, instance, owner):
+        # Build the attribute.
+        attr = self._factory(instance)
+
+        # Cache the value; hide ourselves.
+        setattr(instance, self._attr_name, attr)
+
+        return attr
+
+
+Position = namedtuple("P", ("x", "y"))
+Pos = Position # Shorter Alias
+P = Position   # Even shorter alias
+
+class Size(Position):
+    def __new__(cls, x, y):
+        assert x > 0
+        assert y > 0
+        return Position.__new__(cls, x, y)
+
+    @static_property
+    def width(self):
+        return self.x
+
+    @static_property
+    def height(self):
+        return self.y
+
+    def walk(self):
+        for x in self.x:
+            for y in self.y:
+                yield Position(x, y)
+
