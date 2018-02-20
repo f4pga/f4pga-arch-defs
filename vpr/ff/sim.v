601c4f33a67a878aa3b86da0d79ed8b8b93d9c59
David Shah <davey1576@gmail.com>
2018-02-20 13:08:49 +0000

    Move W5FF to use XML generator
    Signed-off-by: David Shah <davey1576@gmail.com>

---
+++ vpr/ff/sim.v
@@ -0,0 +1,19 @@
+(* blackbox *) (* CLASS="flipflop" *)
+module VPR_FF (D, Q, clk);
+
+(* PORT_CLASS = "D" *)
+(* SETUP = "clk 0.301e-9" *)
+(* CLK_TO_Q = "clk 0.301e-9" *)
+input wire D;
+
+(* PORT_CLASS = "Q" *)
+output reg Q;
+
+(* PORT_CLASS = "clk" *)
+input wire clk;
+
+always @ ( posedge clk ) begin
+	Q <= D;
+end
+
+endmodule
