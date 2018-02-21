aa17c753b3c42a75b86470342b7c9f77895dc108
David Shah <davey1576@gmail.com>
2018-02-21 18:26:52 +0000

    Fix mux output naming in SLICEL, and FF clock to Q
    Signed-off-by: David Shah <davey1576@gmail.com>

--- vpr/ff/sim.v
+++ vpr/ff/sim.v
@@ -3,10 +3,10 @@ module VPR_FF (D, Q, clk);
 
 (* PORT_CLASS = "D" *)
 (* SETUP = "clk 0.301e-9" *)
-(* CLK_TO_Q = "clk 0.301e-9" *)
 input wire D;
 
 (* PORT_CLASS = "Q" *)
+(* CLK_TO_Q = "clk 0.301e-9" *)
 output reg Q;
 
 (* PORT_CLASS = "clock" *)
