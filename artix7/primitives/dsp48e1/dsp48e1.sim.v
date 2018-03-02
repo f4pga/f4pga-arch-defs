fb0bd65524f634c40a9c9f90a9aef3d10a3b664d
Jeffrey Elms <elmsfu@freshred.net>
2018-03-01 16:49:56 -0800

    Clean up and added some comments
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- artix7/primitives/dsp48e1/dsp48e1.sim.v
+++ artix7/primitives/dsp48e1/dsp48e1.sim.v
@@ -1,10 +1,13 @@
-`include "DualAD_Preadder/sim.v"
-`include "DualB/sim.v"
-`include "Mult25x18/sim.v"
+// DSP48E1 - 7 Series DSP48E1 User Guide UG479 (v1.9) September 27, 2016
+
+`include "dual_ad_preadder/sim.v"
+`include "dual_b_reg/sim.v"
+`include "mult25x18/sim.v"
 `include "nmux4/sim.v"
 `include "nmux7/sim.v"
-`include "ALU/sim.v"
+`include "alu/sim.v"
 
+// Figure 2-1: 7 Series FPGA DSP48E1 Slice
 module DSP48E1(
        A, B, C, D,
        OPMODE,ALUMODE, CARRYIN, CARRYINSEL, INMODE,
@@ -16,6 +19,7 @@ module DSP48E1(
        ACOUT, BCOUT, PCOUT, P, CARRYOUT, CARRYCASCOUT, MULTSIGNOUT, PATTERNDETECT, PATTERNBDETECT, OVERFLOW, UNDERFLOW
        );
 
+   // Main inputs
    input wire [29:0] A;
    input wire [17:0] B;
    input wire [47:0] C;
@@ -26,6 +30,7 @@ module DSP48E1(
    input wire [2:0]  CARRYINSEL;
    input wire [4:0]  INMODE;
 
+   // Clock enable for registers
    input wire 	     CEA1;
    input wire 	     CEA2;
    input wire 	     CEB1;
@@ -35,11 +40,12 @@ module DSP48E1(
    input wire 	     CEM;
    input wire 	     CEP;
    input wire 	     CEAD;
-
    input wire 	     CEALUMODE;
    input wire 	     CECTRL;
    input wire 	     CECARRYIN;
    input wire 	     CEINMODE;
+
+   // Reset for registers
    input wire 	     RSTA;
    input wire 	     RSTB;
    input wire 	     RSTC;
@@ -51,8 +57,10 @@ module DSP48E1(
    input wire 	     RSTALUMODE;
    input wire 	     RSTINMODE;
 
+   // clock for all registers and flip-flops
    input wire 	     CLK;
 
+   // Interslice connections
    input wire [29:0] ACIN;
    input wire [17:0] BCIN;
    input wire [47:0] PCIN;
@@ -64,6 +72,7 @@ module DSP48E1(
    output wire [47:0] PCOUT;
    output wire [47:0] P;
 
+   // main outputs
    output wire [3:0]  CARRYOUT;
    output wire 	      CARRYCASCOUT;
    output wire 	      MULTSIGNOUT;
@@ -72,38 +81,48 @@ module DSP48E1(
    output wire 	      OVERFLOW;
    output wire 	      UNDERFLOW;
 
+   // wires for concatenating A and B registers to XMUX input
    wire [47:0] 	      XMUX_CAT;
    wire [29:0] 	      XMUX_A_CAT;
    wire [17:0] 	      XMUX_B_CAT;
 
+   // wires for multiplier inputs
    wire [24:0] 	      AMULT;
    wire [17:0] 	      BMULT;
 
+   // input register blocks for A, B, D
    DUALAD_PREADDER dualad_preadder (.A(A), .ACIN(ACIN), .D(D), .ACOUT(ACOUT), .XMUX(XMUX_A_CAT), .AMULT(AMULT));
    DUALB_REG dualb_reg (.B(B), .BCIN(BCIN), .BCOUT(BCOUT), .XMUX(XMUX_B_CAT), .BMULT(BMULT));
 
+   // concatenate for XMUX
    assign XMUX_CAT = {XMUX_A_CAT, XMUX_B_CAT};
 
+   // Multiplier output
    wire [85:0] 	      MULT_OUT;
 
+   // 25bit by 18bit multiplier
    MULT25X18 mult25x18 (.A(AMULT), .B(BMULT), .OUT(MULT_OUT));
 
+   // signals from muxes to ALU unit
    wire [47:0] 	      X;
    wire [47:0] 	      Y;
    wire [47:0] 	      Z;
 
+   // TODO(elmsfu): take in full OPMODE to check for undefined behaviors
+   // See table 2-7 for X mux selection
    NMUX4 #(.NBITS(48)) xmux (.I0(48'h000000000000), .I1({5'b00000, MULT_OUT[85:43]}), .I2(P), .I3(XMUX_CAT), .S(OPMODE[1:0]), .O(X));
+
+   // See table 2-8 for Y mux selection
    NMUX4 #(.NBITS(48)) ymux (.I0(48'h000000000000), .I1({5'b00000, MULT_OUT[42:0]}), .I2(48'hFFFFFFFFFFFF), .I3(C), .S(OPMODE[3:2]), .O(Y));
 
-   // TODO(elmsfu): z mux atually has 7 outputs from table 2-9, but 2 and 4 are both P
+   // See table 2-9 for Z mux selection
+   // Note: Z mux actually has 7 inputs but 2 and 4 are both P
    NMUX7 #(.NBITS(48)) zmux (.I0(48'h000000000000), .I1(PCIN), .I2(P), .I3(C), .I4(P), .I5({ {17{PCIN[47]}}, PCIN[47:17]}), .I6({ {17{P[47]}}, P[47:17]}), .S(OPMODE[6:4]), .O(Z));
 
+   // See table 2-10 for 3 input behavior
+   // See table 2-13 for 2 input behavior
    ALU alu (.X(X), .Y(Y), .Z(Z), .ALUMODE(ALUMODE), .CARRYIN(CARRYIN), .MULTSIGNIN(MULTSIGNIN), .OUT(P), .CARRYOUT(CARRYOUT), .MULTSIGNOUT(MULTSIGNOUT));
 
    assign PCOUT = P;
 
 endmodule // DSP48E1
-
-
-
-
