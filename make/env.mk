311d77f07d22bd4d2358d40039b55364c440fccc
Tim 'mithro' Ansell <me@mith.ro>
2018-03-05 21:34:25 -0800

    make: Rework the env Makefile a bit.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- make/env.mk
+++ make/env.mk
@@ -3,55 +3,69 @@ include make/inc/common.mk
 MINICONDA_FILE := Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_URL  := https://repo.continuum.io/miniconda/$(MINICONDA_FILE)
 
-CONDA_BIN_DIR := $(abspath ./env/conda/bin/)
+ENV_DIR     := $(abspath ./env)
+CONDA_DIR   := $(ENV_DIR)/conda
 
-CONDA := $(CONDA_BIN_DIR)/conda
-CONDA_YOSYS := $(CONDA_BIN_DIR)/yosys
-CONDA_VPR   := $(CONDA_BIN_DIR)/vpr
-CONDA_MAKE  := $(CONDA_BIN_DIR)/make
+CONDA_BIN   := $(CONDA_DIR)/bin/conda
+CONDA_YOSYS := $(CONDA_DIR)/bin/yosys
+CONDA_VPR   := $(CONDA_DIR)/bin/vpr
+CONDA_MAKE  := $(CONDA_DIR)/bin/make
 
-env/$(MINICONDA_FILE):
-	mkdir -p env
+$(ENV_DIR)/$(MINICONDA_FILE):
+	mkdir -p $(ENV_DIR)
 	wget $(MINICONDA_URL) -O $(TARGET)
 
-$(CONDA): env/$(MINICONDA_FILE)
-	sh $(PREREQ_FIRST) -p env/conda -b -f
-	$(CONDA) config --system --set always_yes yes
+$(CONDA_BIN): $(ENV_DIR)/$(MINICONDA_FILE)
+	sh $(PREREQ_FIRST) -p $(CONDA_DIR) -b -f
+	$(CONDA_BIN) config --system --set always_yes yes
 
-env/conda/envs: $(CONDA)
-	$(CONDA) config --system --add envs_dirs $(TARGET)
+$(CONDA_DIR)/envs: $(CONDA_BIN)
+	$(CONDA_BIN) config --system --add envs_dirs $(TARGET)
 
-env/conda/pkgs: $(CONDA)
-	$(CONDA) config --system --add pkgs_dirs $(TARGET)
+$(CONDA_DIR)/pkgs: $(CONDA_BIN)
+	$(CONDA_BIN) config --system --add pkgs_dirs $(TARGET)
 
 # TODO(mithro): Move to a "conda-symbiflow-packages" rather then leaching off
 # the TimVideos packages.
-env/conda/.timvideos.channel: $(CONDA) env/conda/envs env/conda/pkgs
-	$(CONDA) config --add channels timvideos
-	$(CONDA) config --add channels conda-forge
+$(CONDA_DIR)/.timvideos.channel: $(CONDA_BIN) $(CONDA_DIR)/envs $(CONDA_DIR)/pkgs
+	$(CONDA_BIN) config --add channels timvideos
+	touch $(TARGET)
 
-CONDA_SETUP := $(CONDA) env/conda/envs env/conda/pkgs env/conda/.timvideos.channel
+$(CONDA_DIR)/.conda-forge.channel: $(CONDA_BIN) $(CONDA_DIR)/envs $(CONDA_DIR)/pkgs
+	$(CONDA_BIN) config --add channels conda-forge
+	touch $(TARGET)
 
-$(CONDA_YOSYS): $(CONDA_SETUP)
-	$(CONDA) install yosys
+$(CONDA_DIR)/lib/python3.6/site-packages/lxml:
+	$(CONDA_BIN) install lxml
 
-$(CONDA_VPR): $(CONDA_SETUP)
-	$(CONDA) install vtr
+CONDA_SETUP := \
+  $(CONDA_BIN) \
+  $(CONDA_DIR)/envs \
+  $(CONDA_DIR)/pkgs \
+  $(CONDA_DIR)/.timvideos.channel \
+  $(CONDA_DIR)/.conda-forge.channel \
 
-$(CONDA_MAKE): $(CONDA_SETUP)
-	$(CONDA) install make
+
+$(CONDA_YOSYS): | $(CONDA_SETUP)
+	$(CONDA_BIN) install yosys
+
+$(CONDA_VPR): | $(CONDA_SETUP)
+	$(CONDA_BIN) install vtr
+
+$(CONDA_MAKE): | $(CONDA_SETUP)
+	$(CONDA_BIN) install make
 
 make: $(CONDA_MAKE)
 	@true
 
 .PHONY: make
 
-env: $(CONDA_YOSYS) $(CONDA_VPR)
+env: $(CONDA_YOSYS) $(CONDA_VPR) $(CONDA_DIR)/lib/python3.6/site-packages/lxml
 	@echo $(PREREQ_ALL)
 
 .PHONY: env
 
 # If the environment exists, put it into the path.
 ifneq (,$(wildcard $(abspath env)))
-PATH := $(CONDA)/bin:$(PATH)
+PATH := $(CONDA_DIR)/bin:$(PATH)
 endif
