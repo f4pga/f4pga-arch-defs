5cbb0362e36b598264f3135064f471cb4bce212c
David Shah <davey1576@gmail.com>
2018-03-12 11:08:50 +0000

    testarch: Improve Verilog style consistency
    Signed-off-by: David Shah <davey1576@gmail.com>

--- testarch/primitives/ff/ff.sim.v
+++ testarch/primitives/ff/ff.sim.v
@@ -1,16 +1,16 @@
 (* blackbox *) (* CLASS="flipflop" *)
 module FF(clk, D, Q);
 
-(* PORT_CLASS = "clock" *)
-input wire clk;
+	(* PORT_CLASS = "clock" *)
+	input wire clk;
 
-(* PORT_CLASS = "D" *) (* SETUP = "clk 10e-12" *)
-input wire D;
+	(* PORT_CLASS = "D" *) (* SETUP = "clk 10e-12" *)
+	input wire D;
 
-(* PORT_CLASS = "Q" *) (* CLK_TO_Q = "clk 10e-12" *)
-output wire Q;
+	(* PORT_CLASS = "Q" *) (* CLK_TO_Q = "clk 10e-12" *)
+	output wire Q;
 
-always @(posedge clk)
+	always @(posedge clk)
 		Q <= D;
 
 endmodule
