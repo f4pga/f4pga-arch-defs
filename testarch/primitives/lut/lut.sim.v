1115974f2f5cc524a55c48b7ab3229256b9e7edf
David Shah <davey1576@gmail.com>
2018-03-12 18:39:37 +0000

    testarch: Improve sim.v consistency
    Signed-off-by: David Shah <davey1576@gmail.com>

--- testarch/primitives/lut/lut.sim.v
+++ testarch/primitives/lut/lut.sim.v
@@ -1,12 +1,12 @@
 (* blackbox *)  (* CLASS="lut" *)
 module LUT (in, out);
-  (* PORT_CLASS = "lut_in" *)
-  input [3:0] in;
+	(* PORT_CLASS = "lut_in" *)
+	input [3:0] in;
 
-  (* PORT_CLASS = "lut_out" *)
-  (* DELAY_MATRIX_in = "10e-12; 10e-12; 10e-12; 10e-12" *)
-  output out;
+	(* PORT_CLASS = "lut_out" *)
+	(* DELAY_MATRIX_in = "10e-12; 10e-12; 10e-12; 10e-12" *)
+	output out;
 
-  parameter [15:0] INIT = 16'hDEAD;
-  assign out = INIT[in];
+	parameter [15:0] INIT = 16'hDEAD;
+	assign out = INIT[in];
 endmodule
