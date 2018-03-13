5cf859c3699f6e4766e77932d91070d276ffbaae
Jeffrey Elms <elmsfu@freshred.net>
2018-03-13 00:38:41 -0700

    Adding cocotb to third_party and iverilog to conda
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- make/inc/env.mk
+++ make/inc/env.mk
@@ -4,34 +4,37 @@ INC_ENV_MK := 1
 ENV_DIR     := $(abspath $(TOP_DIR)/env)
 CONDA_DIR   := $(ENV_DIR)/conda
 
-CONDA_BIN   := $(CONDA_DIR)/bin/conda
-CONDA_YOSYS := $(CONDA_DIR)/bin/yosys
-CONDA_VPR   := $(CONDA_DIR)/bin/vpr
-CONDA_MAKE  := $(CONDA_DIR)/bin/make
-CONDA_XSLT  := $(CONDA_DIR)/bin/xsltproc
-CONDA_PYTEST:= $(CONDA_DIR)/bin/pytest
-CONDA_YAPF  := $(CONDA_DIR)/bin/yapf
-CONDA_NODE  := $(CONDA_DIR)/bin/node
-CONDA_NPM   := $(CONDA_DIR)/bin/npm
+CONDA_BIN      := $(CONDA_DIR)/bin/conda
+CONDA_YOSYS    := $(CONDA_DIR)/bin/yosys
+CONDA_VPR      := $(CONDA_DIR)/bin/vpr
+CONDA_MAKE     := $(CONDA_DIR)/bin/make
+CONDA_XSLT     := $(CONDA_DIR)/bin/xsltproc
+CONDA_PYTEST   := $(CONDA_DIR)/bin/pytest
+CONDA_YAPF     := $(CONDA_DIR)/bin/yapf
+CONDA_NODE     := $(CONDA_DIR)/bin/node
+CONDA_NPM      := $(CONDA_DIR)/bin/npm
+CONDA_IVERILOG := $(CONDA_DIR)/bin/iverilog
 
 # If the environment exists, put it into the path and use it.
 ifneq (,$(wildcard $(abspath $(ENV_DIR))))
-PATH   := $(CONDA_DIR)/bin:$(PATH)
-YOSYS  ?= $(CONDA_YOSYS)
-VPR    ?= $(CONDA_VPR)
-XSLT   ?= $(CONDA_XSLT)
-PYTEST ?= $(CONDA_PYTEST)
-YAPF   ?= $(CONDA_YAPF)
-NODE   ?= $(CONDA_NODE)
-NPM    ?= $(CONDA_NPM)
+PATH      := $(CONDA_DIR)/bin:$(PATH)
+YOSYS     ?= $(CONDA_YOSYS)
+VPR       ?= $(CONDA_VPR)
+XSLT      ?= $(CONDA_XSLT)
+PYTEST    ?= $(CONDA_PYTEST)
+YAPF      ?= $(CONDA_YAPF)
+NODE      ?= $(CONDA_NODE)
+NPM       ?= $(CONDA_NPM)
+IVERILOG  ?= $(CONDA_IVERILOG)
 else
-YOSYS  ?= yosys
-VPR    ?= vpr
-XSLT   ?= xsltproc
-PYTEST ?= pytest-3
-YAPF   ?= yapf
-NODE   ?= node
-NPM    ?= npm
+YOSYS     ?= yosys
+VPR       ?= vpr
+XSLT      ?= xsltproc
+PYTEST    ?= pytest-3
+YAPF      ?= yapf
+NODE      ?= node
+NPM       ?= npm
+IVERILOG  ?= iverilog
 endif
 
 # Tools in third_party
