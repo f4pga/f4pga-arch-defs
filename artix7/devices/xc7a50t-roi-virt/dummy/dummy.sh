164df99a96c42bb0ea80b956acf5e0f38d5bf79d
David Shah <davey1576@gmail.com>
2018-03-14 17:33:45 +0000

    arch: Upgrade VPR arch: remove default_ prefix from <fc> attributes
    Signed-off-by: David Shah <davey1576@gmail.com>

--- artix7/devices/xc7a50t-roi-virt/dummy/dummy.sh
+++ artix7/devices/xc7a50t-roi-virt/dummy/dummy.sh
@@ -16,7 +16,7 @@ cat > $OUTPUT <<EOF
     <direct name="I" input="$NAME.I" output="DUMMY.I" />
     <direct name="O" input="DUMMY.O" output="$NAME.O" />
    </interconnect>
-   <fc default_in_type="frac" default_in_val="1.0" default_out_type="frac" default_out_val="1.0">
+   <fc in_type="frac" in_val="1.0" out_type="frac" out_val="1.0">
     <fc_override fc_type="abs" fc_val="0" segment_name="global" />
    </fc>
    <pinlocations pattern="custom">
