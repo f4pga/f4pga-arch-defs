5278256cb748a9e130a3f128cebc3bdfbcf053e0
Jeffrey Elms <elmsfu@freshred.net>
2018-03-21 09:35:33 -0700

    DSP48E1: expand dual_b_reg
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

---
+++ artix7/primitives/dsp48e1/nreg/nreg.sim.v
@@ -0,0 +1,18 @@
+`include "reg.sim.v"
+
+module NREG(D, Q, CLK, CE, RESET);
+   parameter NBITS = 4;
+
+   input wire [NBITS-1:0] D;
+   output reg [NBITS-1:0] Q;
+   input wire CLK;
+   input wire CE;
+   input wire RESET;
+
+   genvar     ii;
+
+   for (ii=0; ii<NBITS; ii++) begin: bitreg
+      REG breg (.D(D[ii]), .Q(Q[ii]), .CLK(CLK), .CE(CE), .RESET(RESET));
+   end
+
+endmodule // NREG
---
+++ artix7/primitives/dsp48e1/nreg/reg.sim.v
@@ -0,0 +1,15 @@
+module REG (D, Q, CLK, CE, RESET);
+   input wire D;
+   input wire CLK;
+   input wire CE;
+   input wire RESET;
+   output reg Q;
+
+   always @(posedge CLK) begin :logic
+     if (~RESET)
+       Q <= 1'b0;
+     else if (CE)
+       Q <= D;
+   end
+
+endmodule // REG
