53a1d21f53e7614eb174bd3a574b86b1379e5f45
Joel Holdsworth <joel@airwebreathe.org.uk>
2018-04-09 10:51:50 +0100

    utils: Added missing shebangs to python scripts
    Signed-off-by: Joel Holdsworth <joel@airwebreathe.org.uk>
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- utils/lib/rr_graph/__init__.py
+++ utils/lib/rr_graph/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from collections import namedtuple
 
 class static_property(object):
--- utils/lib/rr_graph/__main__.py
+++ utils/lib/rr_graph/__main__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from .. import rr_graph
 
 if __name__ == "__main__":
