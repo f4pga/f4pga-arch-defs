912d807d5cec4f41c0191d8568c423cf632b2aeb
Jeffrey Elms <elmsfu@freshred.net>
2018-04-26 16:21:20 -0700

    simtest python style cleanup
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- vpr/muxes/logic/mux2/simtest/test_mux2.py
+++ vpr/muxes/logic/mux2/simtest/test_mux2.py
@@ -1,11 +1,16 @@
+"""
+Test mux2 correctness
+"""
+
 import cocotb
-from cocotb.triggers import Timer
-from cocotb.result import TestFailure
 from cocotb.regression import TestFactory
+from cocotb.result import TestFailure
+from cocotb.triggers import Timer
+
 
 @cocotb.coroutine
-def mux2_basic_test(dut, inputs=(1,0,0)):
-    """Test for MUX2 options"""
+def mux2_basic_test(dut, inputs=(1, 0, 0)):
+    """Test for MUX2 options."""
 
     yield Timer(2)
     I0, I1, S0 = inputs
@@ -13,19 +18,20 @@ def mux2_basic_test(dut, inputs=(1,0,0)):
     dut.I1 = I1
     dut.S0 = S0
     if S0:
-      expected = I1
+        expected = I1
     else:
-      expected = I0
+        expected = I0
     yield Timer(2)
 
     if dut.O != expected:
-      raise TestFailure(
-          'Result is incorrect for I0(%d) I1(%d) S0(%d): %s(O) != %s (expected)' % (I0, I1, S0, dut.O, expected))
+        raise TestFailure(
+            'Result is incorrect for I0(%d) I1(%d) S0(%d): %s(O) != %s (expected)'\
+            % (I0, I1, S0, dut.O, expected))
     else:
-      dut._log.info('I0(%d) I1(%d) S0(%d) output(%d) Ok!'%(I0, I1, S0, dut.O))
+        dut._log.info('I0(%d) I1(%d) S0(%d) output(%d) Ok!'%(I0, I1, S0, dut.O))
 
 
 factory = TestFactory(mux2_basic_test)
-input_permutations = [(x, y, z) for x in [0,1] for y in [0,1] for z in [0,1]]
+input_permutations = [(x, y, z) for x in [0, 1] for y in [0, 1] for z in [0, 1]]
 factory.add_option("inputs", input_permutations)
 factory.generate_tests()
