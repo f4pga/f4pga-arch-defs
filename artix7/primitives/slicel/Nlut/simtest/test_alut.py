41aef0d2e45f15424f5ec85e9aae093cdbf967fc
Jeffrey Elms <elmsfu@freshred.net>
2018-04-26 16:21:20 -0700

    simtest python style cleanup
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- artix7/primitives/slicel/Nlut/simtest/test_alut.py
+++ artix7/primitives/slicel/Nlut/simtest/test_alut.py
@@ -1,42 +1,46 @@
+"""
+Test alut to cover all Nlut
+"""
+
 import cocotb
 from cocotb.binary import BinaryValue
-from cocotb.triggers import Timer
-from cocotb.result import TestFailure
 from cocotb.regression import TestFactory
-from cocotb.generators.bit import random_50_percent
+from cocotb.result import TestFailure
+from cocotb.triggers import Timer
 
-import random
 
-class LutModel:
-  def __init__(self, init, inputs):
-    self.init = init
-    self.inputs = BinaryValue(inputs)
+class LutModel(object):
+    """
+    Model for LUT
+    """
+    def __init__(self, init, inputs):
+        self.init = init
+        self.inputs = BinaryValue(inputs)
 
-  @property
-  def O5(self):
-    print 'asd %s'%self.init.binstr
-    print 'asd %s'%self.inputs.integer
-    return eval(self.init.binstr[-1-self.inputs.integer])
+    @property
+    def O5(self):
+        return eval(self.init.binstr[-1-self.inputs.integer])
 
-  @property
-  def O6(self):
-    return eval(self.init.binstr[-1-self.inputs.integer])
+    @property
+    def O6(self):
+        return eval(self.init.binstr[-1-self.inputs.integer])
 
 @cocotb.coroutine
 def lut_basic_test(dut, inputs):
-  """Test for LUT options"""
+    """Test for LUT options"""
 
-  yield Timer(1)
+    yield Timer(1)
 
-  for ii in range(6):
-    setattr(dut, 'A%d'%(ii + 1), inputs & (1<<ii))
-  yield Timer(1)
-  model = LutModel(dut.INIT.value, inputs)
+    for i in range(6):
+        setattr(dut, 'A%d'%(i + 1), inputs & (1 << i))
+    yield Timer(1)
+    model = LutModel(dut.INIT.value, inputs)
 
-  if dut.O5 != model.O5 or dut.O6 != model.O6:
-    raise TestFailure('No match (dut:model) O5(%d:%d) O6(%d:%d)'%(dut.O5, model.O5, dut.O6, model.O6))
-  else:
-    dut._log.info('Match')
+    if dut.O5 != model.O5 or dut.O6 != model.O6:
+        raise TestFailure('No match (dut:model) O5(%d:%d) O6(%d:%d)' % \
+                          (dut.O5, model.O5, dut.O6, model.O6))
+    else:
+        dut._log.info('Match')
 
 
 factory = TestFactory(lut_basic_test)
