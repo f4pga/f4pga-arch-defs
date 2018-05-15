91d228d2805c1c42cf9241a01bddf71873f24e90
Jeffrey Elms <elmsfu@freshred.net>
2018-05-15 11:12:55 -0700

    dsp48/vpr: whitespace cleanup
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- artix7/primitives/dsp48e1/carryinsel_logic/carryinsel_logic.sim.v
+++ artix7/primitives/dsp48e1/carryinsel_logic/carryinsel_logic.sim.v
@@ -7,7 +7,7 @@ module CARRYINSEL_LOGIC (CARRYIN, CARRYCASCIN, CARRYCASCOUT, A, B, P, PCIN, CARR
 
    parameter MREG = 1;
    parameter CARRYINREG = 1;
-      
+
    input wire 	     CARRYIN;
    input wire 	     CARRYCASCIN;
    input wire 	     CARRYCASCOUT;
@@ -31,16 +31,14 @@ module CARRYINSEL_LOGIC (CARRYIN, CARRYCASCIN, CARRYCASCOUT, A, B, P, PCIN, CARR
    REG 		     round_reg (.D(ROUND), .Q(ROUND_REG), .CE(CEM), .CLK(CLK), .RESET(RSTALLCARRYIN));
    // TODO(elms): not sure if the select is correct: "signal can be optionally registered to match the MREG pipeline delay"
    CARRYIN_MUX #(.S(MREG)) round_mux (.BYPASS(ROUND), .REG(ROUND_REG), .O(ROUND_MUX_OUT));
-      
+
    wire 	     CARRYIN_REG;
    wire 	     CARRYIN_MUX_OUT;
-   
+
    REG 		     carryin_reg (.D(CARRYIN), .Q(CARRYIN_REG), .CE(CECARRYIN), .CLK(CLK), .RESET(RSTALLCARRYIN));
    CARRYIN_MUX #(.S(CARRYINREG)) carryin_mux (.BYPASS(CARRYIN), .REG(CARRYIN_REG), .O(CARRYIN_MUX_OUT));
 
    MUX8 carryinsel_mux (.I0(CARRYIN_MUX_OUT), .I1(~PCIN[47]), .I2(CARRYCASCIN), .I3(PCIN[47]), .I4(CARRYCASCOUT), .I5(~P[47]), .I6(ROUND_MUX_OUT), .I7(P[47]),
 			.S0(CARRYINSEL[0]), .S1(CARRYINSEL[1]), .S2(CARRYINSEL[2]), .O(CIN));
-   
+
 endmodule // CARRYINSEL_LOGIC
-
-
--- vpr/muxes/logic/mux7/mux7.sim.v
+++ vpr/muxes/logic/mux7/mux7.sim.v
@@ -4,34 +4,34 @@
 `include "../mux2/mux2.sim.v"
 
 module MUX7(I0, I1, I2, I3, I4, I5, I6, S0, S1, S2, O);
-	input wire I0;
-	input wire I1;
-	input wire I2;
-	input wire I3;
-	input wire I4;
-	input wire I5;
-   	input wire I6;
-	input wire S0;
-	input wire S1;
-	input wire S2;
-	output wire O;
+   input wire I0;
+   input wire I1;
+   input wire I2;
+   input wire I3;
+   input wire I4;
+   input wire I5;
+   input wire I6;
+   input wire S0;
+   input wire S1;
+   input wire S2;
+   output wire O;
 
-	wire m0;
-	wire m1;
-	wire m2;
-	wire m3;
-	wire m4;
-   	wire m5;
+   wire        m0;
+   wire        m1;
+   wire        m2;
+   wire        m3;
+   wire        m4;
+   wire        m5;
 
-	MUX2 mux0    (.I0(I0), .I1(I1), .S0(S0), .O(m0));
-	MUX2 mux1    (.I0(I2), .I1(I3), .S0(S0), .O(m1));
-	MUX2 mux2    (.I0(I4), .I1(I5), .S0(S0), .O(m2));
-	MUX2 mux3    (.I0(I6), .I1(1'bX), .S0(S0), .O(m3));
-   
-	MUX2 mux4    (.I0(m0), .I1(m1), .S0(S1), .O(m4));
-	MUX2 mux5    (.I0(m2), .I1(m3), .S0(S1), .O(m5));
+   MUX2 mux0    (.I0(I0), .I1(I1), .S0(S0), .O(m0));
+   MUX2 mux1    (.I0(I2), .I1(I3), .S0(S0), .O(m1));
+   MUX2 mux2    (.I0(I4), .I1(I5), .S0(S0), .O(m2));
+   MUX2 mux3    (.I0(I6), .I1(1'bX), .S0(S0), .O(m3));
 
-	MUX2 mux6    (.I0(m4), .I1(m5), .S0(S2), .O(O));
+   MUX2 mux4    (.I0(m0), .I1(m1), .S0(S1), .O(m4));
+   MUX2 mux5    (.I0(m2), .I1(m3), .S0(S1), .O(m5));
+
+   MUX2 mux6    (.I0(m4), .I1(m5), .S0(S2), .O(O));
 endmodule
 
 `endif
