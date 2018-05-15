3d92ed24ae1ef3fcc77a3939f589a578039e77cb
Jeffrey Elms <elmsfu@freshred.net>
2018-05-15 13:37:36 -0700

    dsp48: Adding v2x and PB_TYPE to pass CI
    Signed-off-by: Jeffrey Elms <elmsfu@freshred.net>

--- artix7/primitives/dsp48e1/carryinsel_logic/carryinsel_logic.sim.v
+++ artix7/primitives/dsp48e1/carryinsel_logic/carryinsel_logic.sim.v
@@ -24,6 +24,7 @@ module CARRYINSEL_LOGIC (CARRYIN, CARRYCASCIN, CARRYCASCOUT, A, B, P, PCIN, CARR
    input wire 	     CEM;
    input wire 	     CLK;
 
+`ifndef PB_TYPE
    wire 	     ROUND;
    wire 	     ROUND_REG;
    wire 	     ROUND_MUX_OUT;
@@ -40,5 +41,6 @@ module CARRYINSEL_LOGIC (CARRYIN, CARRYCASCIN, CARRYCASCOUT, A, B, P, PCIN, CARR
 
    MUX8 carryinsel_mux (.I0(CARRYIN_MUX_OUT), .I1(~PCIN[47]), .I2(CARRYCASCIN), .I3(PCIN[47]), .I4(CARRYCASCOUT), .I5(~P[47]), .I6(ROUND_MUX_OUT), .I7(P[47]),
 			.S0(CARRYINSEL[0]), .S1(CARRYINSEL[1]), .S2(CARRYINSEL[2]), .O(CIN));
+`endif //  `ifndef PB_TYPE
 
 endmodule // CARRYINSEL_LOGIC
