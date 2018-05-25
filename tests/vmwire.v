26166b48fa55c01e96c9d57d52fc610a50391efc
Tim 'mithro' Ansell <me@mith.ro>
2018-05-25 14:45:48 -0700

    tests: Adding multiwire test.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

---
+++ tests/vmwire.v
@@ -0,0 +1,8 @@
+module top(input i0, input i1, input i2, output o0, output o1, output o2, output o3, output o4, output o5);
+   assign o0 = i0;
+   assign o1 = i1;
+   assign o2 = i1;
+   assign o3 = i2;
+   assign o4 = i2;
+   assign o5 = i2;
+endmodule // top
