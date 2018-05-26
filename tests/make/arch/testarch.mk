7d8faf553dcc9dddeafca73c48e674ce9d99f4ea
Tim 'mithro' Ansell <me@mith.ro>
2018-05-26 15:25:13 -0700

    ice40: Virtual routing seems to work.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- tests/make/arch/testarch.mk
+++ tests/make/arch/testarch.mk
@@ -5,4 +5,6 @@ DEVICE_TYPE   ?= clutff-unidir-s4
 DEVICE        ?= 2x4
 YOSYS_SCRIPT  ?= synth; abc -lut 4;
 RR_PATCH_TOOL ?= $(TOP_DIR)/utils/testarch_graph.py
-RR_PATCH_CMD  ?= $(RR_PATCH_TOOL)
+RR_PATCH_CMD  ?= $(RR_PATCH_TOOL) \
+	--read_rr_graph $(OUT_RRXML_VIRT) \
+	--write_rr_graph $(OUT_RRXML_REAL)
