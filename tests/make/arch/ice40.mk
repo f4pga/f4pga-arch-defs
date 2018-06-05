302ebf6a5a9e2997bb4ce0c31dbdd6d067007522
Tim 'mithro' Ansell <me@mith.ro>
2018-06-05 13:16:24 -0700

    ice40: Allow setting $ICEBOX to get icebox tools.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- tests/make/arch/ice40.mk
+++ tests/make/arch/ice40.mk
@@ -14,11 +14,18 @@ RR_PATCH_CMD  ?= $(RR_PATCH_TOOL) \
 	--read_rr_graph $(OUT_RRXML_VIRT) \
 	--write_rr_graph $(OUT_RRXML_REAL)
 
+ifneq ($(ICEBOX),)
+HLC_TO_BIT ?= $(ICEBOX)/icebox_hlc2asc.py
+BIT_TO_V ?= $(ICEBOX)/icebox_vlog.py
+else
 HLC_TO_BIT ?= icebox_hlc2asc
+BIT_TO_V ?= icebox_vlog
+endif
+
 HLC_TO_BIT_CMD ?= $(HLC_TO_BIT) $(OUT_HLC) > $(OUT_BITSTREAM) || rm $(OUT_BITSTREAM)
 INPUT_PCF_FILE ?= $(SOURCE).pcf
-BIT_TO_V_CMD ?= icebox_vlog -c -n top -p $(INPUT_PCF_FILE) -d $(PACKAGE) $(OUT_BITSTREAM) > $(OUT_BIT_VERILOG) || rm $(OUT_BIT_VERILOG)
-EQUIV_CHECK_SCRIPT = rename top gate; read_verilog $(SOURCE).v; rename top gold; hierarchy; proc; miter -equiv -flatten -ignore_gold_x -make_outputs -make_outcmp gold gate miter; sat -verify-no-timeout -timeout 20 -seq 5 -prove trigger 0 -prove-skip 1 -show-inputs -show-outputs miter
+BIT_TO_V_CMD ?= $(BIT_TO_V) -c -n top -p $(INPUT_PCF_FILE) -d $(PACKAGE) $(OUT_BITSTREAM) > $(OUT_BIT_VERILOG) || rm $(OUT_BIT_VERILOG)
+EQUIV_CHECK_SCRIPT = rename top gate; read_verilog $(SOURCE).v; rename top gold; hierarchy; proc; miter -equiv -flatten -ignore_gold_x -make_outputs -make_outcmp gold gate miter; sat -verify-no-timeout -timeout 20 -seq 1000 -prove trigger 0 -prove-skip 1 -show-inputs -show-outputs miter
 
 ICE_DEVICE:=$(shell echo $(DEVICE) | sed -e's/^..//' -e's/K/k/')
 
