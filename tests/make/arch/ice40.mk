09d47162dcddce491dda2e3f85194229cd2000ab
Tim 'mithro' Ansell <me@mith.ro>
2018-06-05 18:48:20 -0700

    tests: Use abspath for arachne.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- tests/make/arch/ice40.mk
+++ tests/make/arch/ice40.mk
@@ -24,7 +24,7 @@ endif
 
 HLC_TO_BIT_CMD ?= $(HLC_TO_BIT) $(OUT_HLC) > $(OUT_BITSTREAM) || rm $(OUT_BITSTREAM)
 INPUT_PCF_FILE ?= $(abspath $(SOURCE).pcf)
-BIT_TO_V_CMD ?= $(BIT_TO_V) -L -c -n top -p $(INPUT_PCF_FILE) -d $(PACKAGE) $(OUT_BITSTREAM) > $(OUT_BIT_VERILOG) || rm $(OUT_BIT_VERILOG)
+BIT_TO_V_CMD ?= $(BIT_TO_V) -c -n top -p $(INPUT_PCF_FILE) -d $(PACKAGE) $(OUT_BITSTREAM) > $(OUT_BIT_VERILOG) || rm $(OUT_BIT_VERILOG)
 EQUIV_CHECK_SCRIPT = rename top gate; read_verilog $(SOURCE).v; rename top gold; hierarchy; proc; miter -equiv -flatten -ignore_gold_x -make_outputs -make_outcmp gold gate miter; sat -dump_vcd out.vcd -verify-no-timeout -timeout 20 -seq 1000 -prove trigger 0 -prove-skip 1 -show-inputs -show-outputs miter
 
 ICE_DEVICE:=$(shell echo $(DEVICE) | sed -e's/^..//' -e's/K/k/')
@@ -34,12 +34,12 @@ OUT_BLIF=$(OUT_LOCAL)/$(SOURCE).blif
 ARACHNE_PNR ?= arachne-pnr
 arachne-pnr: | $(OUT_LOCAL)
 	mkdir -p $(OUT_LOCAL)
-	$(YOSYS) -p "synth_ice40 -nocarry -blif $(OUT_BLIF)" ../$(SOURCE_V).sb_io.v
+	$(YOSYS) -p "synth_ice40 -nocarry -blif $(OUT_BLIF)" $(abspath $(SOURCE).v)
 	$(ARACHNE_PNR) -d $(ICE_DEVICE) \
 		--post-pack-blif $(OUT_LOCAL)/arachne-pack.blif \
 		--post-pack-verilog $(OUT_LOCAL)/arachne-pack.v \
 		--post-place-blif $(OUT_LOCAL)/arachne-place.blif \
-		--pcf-file ../$(SOURCE).pcf \
+		--pcf-file $(abspath $(SOURCE).pcf) \
 		--package $(PACKAGE) \
 		-o $(OUT_LOCAL)/arachne.asc \
 		$(OUT_BLIF)
