0003baf55bb3bffb6ec29bfec8ea5f1d1396ef61
Tim 'mithro' Ansell <me@mith.ro>
2018-06-06 17:36:14 -0700

    ice40: Adding lots of 4 input LUT tests.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

---
+++ tests/ice40/icelut/route-through-in1/lut.v
@@ -0,0 +1,9 @@
+// 4-input, route-through LUT test.
+module top( (* keep *) input [3:0] I, output O);
+    always @(I)
+    case(I)
+        4'b1000 : O = 1;
+        4'b0000 : O = 0;
+        default : O = 0;
+    endcase
+endmodule // top
