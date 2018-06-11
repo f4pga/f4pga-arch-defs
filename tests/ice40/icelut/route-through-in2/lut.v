b460cb0a638cc95c4427a39c14db61fa7526750e
Tim 'mithro' Ansell <me@mith.ro>
2018-06-10 17:10:35 -0700

    tests/ice40: Fix the route-through examples.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- tests/ice40/icelut/route-through-in2/lut.v
+++ tests/ice40/icelut/route-through-in2/lut.v
@@ -2,8 +2,7 @@
 module top( (* keep *) input [3:0] I, output O);
     always @(I)
     case(I)
-        4'b0100 : O = 1;
-        4'b0000 : O = 0;
-        default : O = 0;
+        4'b?1?? : O = 1;
+        4'b?0?? : O = 0;
     endcase
 endmodule // top
