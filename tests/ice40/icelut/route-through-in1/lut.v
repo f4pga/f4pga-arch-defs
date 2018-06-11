530a1273c4c1c4161239085ef381291c4908e287
Tim 'mithro' Ansell <me@mith.ro>
2018-06-11 16:38:42 -0700

    tests/ice40: Specific LUT instance.

--- tests/ice40/icelut/route-through-in1/lut.v
+++ tests/ice40/icelut/route-through-in1/lut.v
@@ -1,8 +1,13 @@
 // 4-input, route-through LUT test.
 module top( (* keep *) input [3:0] I, output O);
-    always @*
-    case(I)
-        4'b??1? : O = 1;
-        4'b??0? : O = 0;
-    endcase
+    //Cell instances
+    SB_LUT4 #(
+        .LUT_INIT(16'b0100000000000000)
+    ) LUT  (
+        .I0(1'b0),
+        .I1(I[1]),
+        .I2(1'b0),
+        .I3(1'b0),
+        .O(O)
+    );
 endmodule // top
