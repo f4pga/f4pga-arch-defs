010132499a50873cebc0a5523092b63f86fb2429
Tim 'mithro' Ansell <me@mith.ro>
2018-06-11 21:02:07 -0700

    ice40: Adding Python code to generate pin layouts.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

---
+++ utils/eblif.py
@@ -0,0 +1,74 @@
+
+
+class BLIF:
+    def __init__(self, model=None, inputs=tuple(), outputs=tuple()):
+        self.model = model
+        self.inputs = inputs
+        self.outputs = outputs
+
+    def inputs(self):
+        pass
+
+    def outputs(self):
+        pass
+
+
+top_level = [
+    "model",
+    "inputs",
+    "outputs",
+    "names",
+    "latch",
+    "subckt",
+]
+
+sub_level = [
+    "cname",
+    "attr",
+]
+
+
+def parse_blif(f):
+    current = None
+
+    data = {}
+    def add(d):
+        if d['type'] not in data:
+            data[d['type']] = []
+        data[d['type']].append(d)
+
+    current = None
+    for oline in f:
+        line = oline
+        if '#' in line:
+            line = line[:line.find('#')]
+        line = line.strip()
+        if not line:
+            continue
+
+        if line.startswith("."):
+            args = line.split(" ", maxsplit=1)
+            if len(args) < 2:
+                args.append("")
+
+            ctype = args.pop(0)
+            assert ctype.startswith("."), ctype
+            ctype = ctype[1:]
+
+            if ctype in top_level:
+                if current:
+                    add(current)
+                current = {
+                    'type': ctype,
+                    'args': args[-1].split(),
+                    'data': [],
+                }
+            else:
+                current[ctype] = args[-1].split()
+            continue
+        current['data'].append(line.strip().split())
+    assert len(data['inputs']) == 1
+    data['inputs'] = data['inputs'][0]
+    assert len(data['outputs']) == 1
+    data['outputs'] = data['outputs'][0]
+    return data
