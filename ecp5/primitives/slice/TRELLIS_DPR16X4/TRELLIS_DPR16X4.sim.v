7ce9be2289b4eed0302d3814d287b065e0408bce
David Shah <davey1576@gmail.com>
2018-07-13 16:14:26 +0200

    ecp5: Simulation fixes
    Signed-off-by: David Shah <davey1576@gmail.com>

--- ecp5/primitives/slice/TRELLIS_DPR16X4/TRELLIS_DPR16X4.sim.v
+++ ecp5/primitives/slice/TRELLIS_DPR16X4/TRELLIS_DPR16X4.sim.v
@@ -15,7 +15,7 @@ module TRELLIS_DPR16X4 (
 	integer i;
 	initial begin
 		for (i = 0; i < 16; i = i + 1)
-			mem[i] <= INITVAL[4*i :+ 4];
+			mem[i] <= {INITVAL[i+3], INITVAL[i+2], INITVAL[i+1], INITVAL[i]};
 	end
 
 	wire muxwck = (WCKMUX == "INV") ? ~WCK : WCK;
