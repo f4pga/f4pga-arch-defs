29b731350223867170ecef7377bc25dd966fcf92
litghost <537074+litghost@users.noreply.github.com>
2018-07-31 17:40:36 -0700

    Refactored CMake files and started to add file level and function comments.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

---
+++ make/file_targets.cmake
@@ -0,0 +1,191 @@
+# This CMake include file defines utility functions for handling generated
+# files that are required in another CMake directory other than the one where
+# it was generated.
+#
+# Key functions:
+#   MAKE_FILE_TARGET - Creates a file target.  All source files, whether
+#                      generated or not should call this function.
+#   GET_FILE_TARGET - Given a absolute or local source path, what is the target
+#                     that will build this file.
+#   GET_FILE_LOCATION - Given a absolute or local source path, what is the
+#                       actual location of the file.
+#
+# When writing targets that rely on files using these functions, never use a
+# raw source path, e.g. ${CMAKE_CURRENT_SOURCE_DIR}/example_dir/example.file
+# or example.file.  Instead always call GET_FILE_LOCATION on the path.  This
+# is because the actual source location may not be in the source tree, but
+# instead be in the build tree.
+#
+# When writing targets that rely on files using these functions, always DEPEND
+# on both the file location (from GET_FILE_LOCATION) and the file target
+# (from GET_FILE_TARGET).  If you only DEPEND on the file location, the file
+# will not be generated the first time through the build.
+#
+# Utility functions:
+#   APPEND_FILE_LOCATION - Appends to a list the file location of specified
+#                          file.
+#   APPEND_FILE_DEPENDENCY - Appends to a list both the file location and
+#                            file target of specified file.
+
+
+function(GET_FILE_TARGET var src_file)
+  # Sets var in PARENT_SCOPE to file target for given src_file.
+  if(${src_file} MATCHES "^/")
+    set(SOURCE_LOCATION ${src_file})
+  else()
+    set(SOURCE_LOCATION ${CMAKE_CURRENT_SOURCE_DIR}/${src_file})
+  endif()
+  string(REPLACE "/" "_" TARGET_PATH ${SOURCE_LOCATION})
+  set(${var} file_${TARGET_PATH} PARENT_SCOPE)
+endfunction()
+
+function(GET_FILE_LOCATION var src_file)
+  # Sets var in PARENT_SCOPE to file location for given src_file.
+  GET_FILE_TARGET(SRC_TARGET ${src_file})
+  get_target_property(SRC_LOCATION ${SRC_TARGET} LOCATION)
+  if("${SRC_LOCATION}" STREQUAL "NOT_FOUND")
+    message(FATAL_ERROR "File ${src_file} is not a valid verilog target, missing LOCATION.")
+  endif()
+
+  set(${var} ${SRC_LOCATION} PARENT_SCOPE)
+endfunction()
+
+function(APPEND_FILE_DEPENDENCY var src_file)
+  # Appends to list var in PARENT_SCOPE both file location and file target
+  # for given src_file.
+  GET_FILE_TARGET(SRC_TARGET ${src_file})
+  get_target_property(SRC_LOCATION ${SRC_TARGET} LOCATION)
+  if("${SRC_LOCATION}" STREQUAL "NOT_FOUND")
+    message(FATAL_ERROR "File ${src_file} is not a valid verilog target, missing LOCATION.")
+  endif()
+
+  list(APPEND ${var} ${SRC_TARGET})
+  list(APPEND ${var} ${SRC_LOCATION})
+  set(${var} "${${var}}" PARENT_SCOPE)
+endfunction()
+
+function(APPEND_FILE_INCLUDES var src_file)
+  # Appends to list var in PARENT_SCOPE both includes listed for file target
+  # for given src_file.
+  GET_FILE_TARGET(SRC_TARGET ${src_file})
+  get_target_property(SRC_INCLUDES ${SRC_TARGET} INCLUDES)
+  if("${SRC_INCLUDES}" STREQUAL "NOT_FOUND")
+    message(FATAL_ERROR "File ${SRC} is not a valid verilog target, missing INCLUDES list.")
+  endif()
+
+  list(APPEND ${var} ${SRC_INCLUDES})
+  set(${var} "${${var}}" PARENT_SCOPE)
+endfunction()
+
+function(GET_VERILOG_INCLUDES var file)
+  # Appends to list var in PARENT_SCOPE all verilog source dependency based on
+  # scanning input at configure time.
+  #
+  # Note this function cannot be used at generation time because
+  # execute_process is called during configure step and generated files don't
+  # exist yet.
+  execute_process(
+    COMMAND grep "^`include" "${file}"
+    COMMAND cut -d \" -f 2
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    OUTPUT_VARIABLE INCLUDES)
+
+  string(REPLACE "\n" ";" INCLUDES_LIST "${INCLUDES}")
+  foreach(INCLUDE ${INCLUDES_LIST})
+    string(STRIP ${INCLUDE} INCLUDE)
+    if(NOT "${INCLUDE}" STREQUAL "")
+      get_filename_component(ABS_PATH_TO_INCLUDE ${CMAKE_CURRENT_SOURCE_DIR}/${INCLUDE}
+        ABSOLUTE
+        BASE_DIR ${symbiflow-arch-defs_SOURCE_DIR})
+      list(APPEND ${var} ${ABS_PATH_TO_INCLUDE})
+    endif()
+  endforeach()
+  set(${var} "${${var}}" PARENT_SCOPE)
+endfunction()
+
+function(GET_XML_INCLUDES var file)
+  # Appends to list var in PARENT_SCOPE all xml source dependency based on
+  # scanning input at configure time.
+  #
+  # Note this function cannot be used at generation time because
+  # execute_process is called during configure step and generated files don't
+  # exist yet.
+  get_target_property_required(PYTHON3 env PYTHON3)
+  execute_process(
+    COMMAND ${PYTHON3} ${symbiflow-arch-defs_SOURCE_DIR}/utils/deps_xml.py --file_per_line ${CMAKE_CURRENT_SOURCE_DIR}/${file}
+    WORKING_DIRECTORY ${symbiflow-arch-defs_SOURCE_DIR}
+    OUTPUT_VARIABLE INCLUDES)
+
+  string(REPLACE "\n" ";" INCLUDES_LIST "${INCLUDES}")
+  foreach(INCLUDE ${INCLUDES_LIST})
+    string(STRIP ${INCLUDE} INCLUDE)
+    if(NOT "${INCLUDE}" STREQUAL "")
+      get_filename_component(ABS_PATH_TO_INCLUDE ${INCLUDE}
+        ABSOLUTE
+        BASE_DIR ${symbiflow-arch-defs_SOURCE_DIR})
+      list(APPEND ${var} ${ABS_PATH_TO_INCLUDE})
+    endif()
+  endforeach()
+  set(${var} "${${var}}" PARENT_SCOPE)
+endfunction()
+
+function(MAKE_FILE_TARGET)
+  # MAKE_FILE_TARGET(
+  #   FILE <source file location>
+  #   [GENERATED | SCANNER_TYPE <verilog|xml>]
+  #   )
+  #
+  # Creates new file target for given source location.  Even if
+  # MAKE_FILE_TARGET is being called on a file that is located in the binary
+  # directory, always pass the source location it would have if the source and
+  # binary directories were the same.  This is important for dependency
+  # definitions that assume all inputs are in one folder.
+  #
+  # MAKE_FILE_TARGET ensures that all sources are in one folder, because of how
+  # the verilog include directive is defined.
+  #
+  # SCANNER_TYPE argument can be used if GENERATED is not set.  Valid
+  # SCANNER_TYPE are "verilog" and "xml".
+  #
+  # GENERATED must be passed if the source file is generated and is placed
+  # within the binary directory.
+  set(options GENERATED)
+  set(oneValueArgs FILE SCANNER_TYPE)
+  set(multiValueArgs)
+  cmake_parse_arguments(MAKE_FILE_TARGET "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+  GET_FILE_TARGET(TARGET_NAME ${MAKE_FILE_TARGET_FILE})
+
+  SET(INCLUDE_FILES "")
+  if(NOT ${MAKE_FILE_TARGET_GENERATED})
+    if("${MAKE_FILE_TARGET_SCANNER_TYPE}" STREQUAL "")
+    elseif("${MAKE_FILE_TARGET_SCANNER_TYPE}" STREQUAL "verilog")
+      GET_VERILOG_INCLUDES(INCLUDE_FILES ${MAKE_FILE_TARGET_FILE})
+    elseif("${MAKE_FILE_TARGET_SCANNER_TYPE}" STREQUAL "xml")
+      GET_XML_INCLUDES(INCLUDE_FILES ${MAKE_FILE_TARGET_FILE})
+    else()
+      message(FATAL_ERROR "Unknown SCANNER_TYPE=${MAKE_FILE_TARGET_SCANNER_TYPE}.")
+    endif()
+  endif()
+
+  SET(INCLUDE_FILES_TARGETS "")
+  foreach(INCLUDE ${INCLUDE_FILES})
+    APPEND_FILE_DEPENDENCY(INCLUDE_FILES_TARGETS ${INCLUDE})
+  endforeach()
+
+  if(NOT ${MAKE_FILE_TARGET_GENERATED})
+    add_custom_command(
+      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${MAKE_FILE_TARGET_FILE}
+        COMMAND ${CMAKE_COMMAND} -E copy
+          ${CMAKE_CURRENT_SOURCE_DIR}/${MAKE_FILE_TARGET_FILE}
+          ${CMAKE_CURRENT_BINARY_DIR}/${MAKE_FILE_TARGET_FILE}
+          DEPENDS ${INCLUDE_FILES_TARGETS}
+          )
+  endif()
+
+  add_custom_target(${TARGET_NAME}
+    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${MAKE_FILE_TARGET_FILE} ${INCLUDE_FILES_TARGETS})
+
+  set_target_properties(${TARGET_NAME} PROPERTIES LOCATION ${CMAKE_CURRENT_BINARY_DIR}/${MAKE_FILE_TARGET_FILE})
+  set_target_properties(${TARGET_NAME} PROPERTIES INCLUDES "")
+endfunction(MAKE_FILE_TARGET)
---
+++ make/gen.cmake
@@ -0,0 +1,233 @@
+function(V2X)
+  # V2X(
+  #   NAME <name>
+  #   [TOP_MODULE <top module>]
+  #   SRCS <src1> <src2>
+  #   )
+  #
+  # V2X converts SRCS from verilog to .pb_type.xml and .model.xml via the
+  # utilities in <root>/util/vlog/vlog_to_<x>.
+  #
+  # V2X requires all files in SRCS to have a file target via MAKE_FILE_TARGET.
+  #
+  # V2X will generate a dummy target <name> that will build both xml outputs.
+  set(options)
+  set(oneValueArgs NAME TOP_MODULE)
+  set(multiValueArgs SRCS)
+  cmake_parse_arguments(V2X "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+  set(INCLUDES "")
+
+  set(DEPENDS_LIST "")
+  set(REAL_SOURCE_LIST "")
+  foreach(SRC ${V2X_SRCS})
+    if(NOT "${SRC}" MATCHES "\\.sim\\.v$")
+      message(FATAL_ERROR "File ${SRC} does not end with .sim.v")
+    endif()
+
+    APPEND_FILE_DEPENDENCY(DEPENDS_LIST ${SRC})
+    APPEND_FILE_INCLUDES(INCLUDES ${SRC})
+  endforeach()
+
+  list(GET V2X_SRCS 0 FIRST_SOURCE_FILE)
+  GET_FILE_LOCATION(FIRST_SOURCE ${FIRST_SOURCE_FILE})
+
+  set(TOP_ARG "")
+  if(NOT ${V2X_TOP_MODULE} STREQUAL "")
+    set(TOP_ARG "--top=${TOP_MODULE}")
+  endif()
+
+  string(REPLACE ";" "," INCLUDES_LIST "${INCLUDES}")
+
+  set(INCLUDE_ARG "")
+  if(NOT "${INCLUDES_LIST}" STREQUAL "")
+    set(INCLUDE_ARG "--includes=${INCLUDES_LIST}")
+  endif()
+
+  add_custom_command(
+    OUTPUT "${V2X_NAME}.pb_type.xml"
+    DEPENDS ${DEPENDS_LIST} ${symbiflow-arch-defs_SOURCE_DIR}/utils/vlog/vlog_to_pbtype.py
+    COMMAND ${symbiflow-arch-defs_SOURCE_DIR}/utils/vlog/vlog_to_pbtype.py ${TOP_ARG} -o ${CMAKE_CURRENT_BINARY_DIR}/${V2X_NAME}.pb_type.xml ${FIRST_SOURCE} ${INCLUDE_ARG}
+    WORKING_DIRECTORY ${symbiflow-arch-defs_SOURCE_DIR}/utils/vlog/
+    )
+
+  MAKE_FILE_TARGET(FILE "${V2X_NAME}.pb_type.xml" GENERATED)
+
+  add_custom_command(
+    OUTPUT "${V2X_NAME}.model.xml"
+    DEPENDS ${DEPENDS_LIST} ${symbiflow-arch-defs_SOURCE_DIR}/utils/vlog/vlog_to_model.py
+    COMMAND ${symbiflow-arch-defs_SOURCE_DIR}/utils/vlog/vlog_to_model.py ${TOP_ARG} -o ${CMAKE_CURRENT_BINARY_DIR}/${V2X_NAME}.model.xml ${FIRST_SOURCE} ${INCLUDE_ARG}
+    WORKING_DIRECTORY ${symbiflow-arch-defs_SOURCE_DIR}/utils/vlog/
+    )
+
+  MAKE_FILE_TARGET(FILE "${V2X_NAME}.model.xml" GENERATED)
+
+  add_custom_target(${V2X_NAME}
+    DEPENDS
+      "${V2X_NAME}.model.xml"
+      "${V2X_NAME}.pb_type.xml"
+      )
+endfunction(V2X)
+
+function(MUX_GEN)
+  # MUX_GEN(
+  #   NAME <name>
+  #   TYPE "routing"|"logic"
+  #   MUX_NAME <name of mux>
+  #   WIDTH <mux width>
+  #   [SPLIT_INPUTS]
+  #   [INPUTS <comma seperate list of inputs>]
+  #   [SPLIT_SELECTS]
+  #   [SELECTS <comma seperate list of selects>]
+  #   [SUBCKT <subckt>]
+  #   [COMMENT <comment>]
+  #   [OUTPUT <mux output name>]
+  #   [DATA_WIDTH <data width>]
+  #   [NTEMPLATE_PREFIXES <list of prefixes>]
+  #   )
+  #
+  #  Generate <name>.sim.v, <name>.pb_type.xml, and <name>.model.xml for mux
+  #  with given parameters using <root>/utils/mux_gen.py. A target <name> will
+  #  be created that will generate all outputs.
+  #
+  #  If <name> starts with "ntemplate.", NTEMPLATE_PREFIXES can be used to call
+  #  N_TEMPLATE function on each output with the specified prefixes.
+  #
+  #  For other mux arguments, see <root>/utils/mux_gen.py for details.
+  set(options SPLIT_INPUTS SPLIT_SELECTS)
+  set(oneValueArgs NAME MUX_NAME TYPE WIDTH INPUTS SELECTS SUBCKT COMMENT OUTPUT DATA_WIDTH)
+  set(multiValueArgs NTEMPLATE_PREFIXES)
+  cmake_parse_arguments(MUX_GEN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  if("${MUX_GEN_TYPE}" STREQUAL "routing")
+    if(NOT "${MUX_GEN_SUBCKT}" STREQUAL "")
+      message(FATAL_ERROR "Can not use SUBCKT=${MUX_GEN_SUBCKT} with routing mux.")
+    endif()
+  elseif("${MUX_GEN_TYPE}" STREQUAL "logic")
+  else()
+    message(FATAL_ERROR "MUX_GEN type must be \"routing\" or \"logic\".")
+  endif()
+
+  set(MUX_GEN_ARGS "")
+  list(APPEND MUX_GEN_ARGS "--outdir=${CMAKE_CURRENT_BINARY_DIR}")
+  list(APPEND MUX_GEN_ARGS "--outfilename=${MUX_GEN_NAME}")
+  list(APPEND MUX_GEN_ARGS "--type=${MUX_GEN_TYPE}")
+  list(APPEND MUX_GEN_ARGS "--width=${MUX_GEN_WIDTH}")
+  list(APPEND MUX_GEN_ARGS "--name-mux=${MUX_GEN_MUX_NAME}")
+
+  if(NOT "${MUX_GEN_COMMENT}" STREQUAL "")
+    list(APPEND MUX_GEN_ARGS "--comment=\"${MUX_GEN_COMMENT}\"")
+  endif()
+
+  if(NOT "${MUX_GEN_OUTPUT}" STREQUAL "")
+    list(APPEND MUX_GEN_ARGS "--name-out=${MUX_GEN_OUTPUT}")
+  endif()
+
+  if(${MUX_GEN_SPLIT_INPUTS})
+    list(APPEND MUX_GEN_ARGS "--split-inputs=1")
+  endif()
+
+  if(NOT "${MUX_GEN_INPUTS}" STREQUAL "")
+    list(APPEND MUX_GEN_ARGS "--name-inputs=${MUX_GEN_INPUTS}")
+  endif()
+
+  if(${MUX_GEN_SPLIT_SELECTS})
+    list(APPEND MUX_GEN_ARGS "--split-selects=1")
+  endif()
+
+  if(NOT "${MUX_GEN_SELECTS}" STREQUAL "")
+    list(APPEND MUX_GEN_ARGS "--name-selects=${MUX_GEN_SELECTS}")
+  endif()
+
+  if(NOT "${MUX_GEN_SUBCKT}" STREQUAL "")
+    list(APPEND MUX_GEN_ARGS "--subckt=${MUX_GEN_SUBCKT}")
+  endif()
+
+  if(NOT "${MUX_GEN_DATA_WIDTH}" STREQUAL "")
+    list(APPEND MUX_GEN_ARGS "--data-width=${MUX_GEN_DATA_WIDTH}")
+  endif()
+
+  set(OUTPUTS "")
+  list(APPEND OUTPUTS
+    "${MUX_GEN_NAME}.sim.v"
+    "${MUX_GEN_NAME}.pb_type.xml"
+    "${MUX_GEN_NAME}.model.xml"
+    )
+
+  add_custom_command(
+    OUTPUT ${OUTPUTS}
+    DEPENDS ${symbiflow-arch-defs_SOURCE_DIR}/utils/mux_gen.py ${symbiflow-arch-defs_SOURCE_DIR}/vpr/muxes/logic/mux${MUX_GEN_WIDTH}/mux${MUX_GEN_WIDTH}.sim.v
+    COMMAND ${symbiflow-arch-defs_SOURCE_DIR}/utils/mux_gen.py ${MUX_GEN_ARGS})
+
+  MAKE_FILE_TARGET(FILE "${MUX_GEN_NAME}.sim.v"
+    GENERATED)
+  MAKE_FILE_TARGET(FILE "${MUX_GEN_NAME}.pb_type.xml"
+    GENERATED)
+  MAKE_FILE_TARGET(FILE "${MUX_GEN_NAME}.model.xml"
+    GENERATED)
+
+  add_custom_target(${MUX_GEN_NAME}
+    DEPENDS ${OUTPUTS}
+      )
+
+  if(NOT "${MUX_GEN_NTEMPLATE_PREFIXES}" STREQUAL "")
+    foreach(OUTPUT ${OUTPUTS})
+      string(REPLACE "ntemplate." "" N_TEMPLATE_NAME ${OUTPUT})
+      N_TEMPLATE(
+        NAME ${N_TEMPLATE_NAME}
+        PREFIXES ${MUX_GEN_NTEMPLATE_PREFIXES}
+        SRCS ${OUTPUT}
+        )
+    endforeach()
+  endif()
+endfunction(MUX_GEN)
+
+function(N_TEMPLATE)
+  # N_TEMPLATE(
+  #   NAME <name>
+  #   SRCS <list of sources>
+  #   PREFIXES <list of prefixes>
+  #   [APPLY_V2X]
+  #   )
+  #
+  # N_TEMPLATE converts files with prefix ntemplate.<rest> to <rest> and
+  # applies the template prefix, converting all N's to <prefix>.
+  #
+  # If APPLY_V2X is set, V2X will be invoked with NAME = <prefix><name> and
+  # the output of the templating process.
+  set(options APPLY_V2X)
+  set(oneValueArgs NAME)
+  set(multiValueArgs SRCS PREFIXES)
+  cmake_parse_arguments(N_TEMPLATE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+  set(OUTPUTS "")
+
+  foreach(PREFIX ${N_TEMPLATE_PREFIXES})
+    foreach(SRC ${N_TEMPLATE_SRCS})
+      string(REPLACE "ntemplate." "" SRC_NO_NTEMPLATE ${SRC})
+      string(REPLACE "N" ${PREFIX} SRC_WITH_PREFIX ${SRC_NO_NTEMPLATE})
+      GET_FILE_TARGET(SRC_TARGET ${SRC})
+      GET_FILE_LOCATION(SRC_LOCATION ${SRC})
+      add_custom_command(
+        OUTPUT ${SRC_WITH_PREFIX}
+        DEPENDS ${symbiflow-arch-defs_SOURCE_DIR}/utils/n.py ${SRC_LOCATION} ${SRC_TARGET}
+        COMMAND ${symbiflow-arch-defs_SOURCE_DIR}/utils/n.py ${PREFIX} ${SRC_LOCATION} ${CMAKE_CURRENT_BINARY_DIR}/${SRC_WITH_PREFIX}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        )
+
+      MAKE_FILE_TARGET(FILE ${SRC_WITH_PREFIX} GENERATED
+        DEPENDS ${SRC}
+        )
+
+      list(APPEND OUTPUTS ${SRC_WITH_PREFIX})
+
+      if(${APPLY_V2X})
+        V2X(NAME ${PREFIX}${NAME} SRCS ${SRC_WITH_PREFIX})
+      endif()
+    endforeach(SRC)
+  endforeach(PREFIX)
+
+  add_custom_target(
+    ${N_TEMPLATE_NAME} ALL
+    DEPENDS ${OUTPUTS})
+endfunction(N_TEMPLATE)
