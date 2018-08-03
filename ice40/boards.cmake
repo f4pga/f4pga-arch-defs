ae50b38bc454798d7e686b4f653480b19fdaa335
litghost <537074+litghost@users.noreply.github.com>
2018-08-03 13:21:05 -0700

    Address review comments.
    - Move ice40 board definitions into seperate file.
    - Adjust formatting on some CMakeLists.txt files.
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

---
+++ ice40/boards.cmake
@@ -0,0 +1,18 @@
+define_board(BOARD icestick DEVICE hx1k PACKAGE tq144 PROG_TOOL ${ICEPROG_TOOL})
+
+define_board(
+  BOARD iceblink40-lp1k
+  DEVICE lp1k
+  PACKAGE qn84
+  PROG_TOOL ${ICEPROG_TOOL}
+  PROG_CMD ${ICEPROG_TOOL} -ew
+)
+
+define_board(
+  BOARD hx8k-b-evn
+  DEVICE hx8k
+  PACKAGE ct256
+  PROG_TOOL ${ICEPROG_TOOL}
+  PROG_CMD ${ICEPROG_TOOL} -S
+)
+
