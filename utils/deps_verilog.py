854b955f0d8f359691b00f27be0b757a29e17abf
Keith Rothman <537074+litghost@users.noreply.github.com>
2018-08-08 16:49:50 -0700

    Add $readmemb directive to verilog scanner.
    Add missing add_file_target for murax system RAM symbols.
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- utils/deps_verilog.py
+++ utils/deps_verilog.py
@@ -16,30 +16,43 @@ from lib.deps import write_deps
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "inputfile", type=argparse.FileType('r'), help="Input Verilog file")
+parser.add_argument(
+    "--file_per_line", action='store_true', help="Output dependencies file per line, rather than Make .d format.")
+
+def read_dependencies(f):
+  for line in f:
+      line = line.strip()
+      if line.startswith("`include"):
+        _, includefile = line.split(" ", 1)
+        assert_eq(_, "`include")
+        assert_eq(includefile[0], '"')
+        assert_eq(includefile[-1], '"')
+        yield includefile[1:-1]
+
+      if line.startswith("$readmemb"):
+        _, includefile = line.split("(", 1)
+        assert_eq(_, "$readmemb")
+        includefile, _ = includefile.split(",", 1)
+        assert_eq(includefile[0], '"')
+        assert_eq(includefile[-1], '"')
+        yield includefile[1:-1]
 
 
 def main(argv):
     args = parser.parse_args(argv[1:])
 
-    inputpath = os.path.abspath(args.inputfile.name)
-    inputdir = os.path.dirname(inputpath)
-
-    data = StringIO()
-    for line in args.inputfile:
-        line = line.strip()
-        if not line.startswith("`include"):
-            continue
-        _, includefile = line.split(" ", 1)
-        assert_eq(_, "`include")
-        assert_eq(includefile[0], '"')
-        assert_eq(includefile[-1], '"')
-        includefile = includefile[1:-1]
-
+    if args.file_per_line:
+      for dep in read_dependencies(args.inputfile):
+        print(dep)
+    else:
+      data = StringIO()
+      inputpath = os.path.abspath(args.inputfile.name)
+      inputdir = os.path.dirname(inputpath)
+      for includefile in read_dependencies(args.inputfile):
         includefile_path = os.path.abspath(os.path.join(inputdir, includefile))
-
         add_dependency(data, inputpath, includefile_path)
 
-    write_deps(args.inputfile.name, data)
+      write_deps(args.inputfile.name, data)
 
 
 if __name__ == "__main__":
