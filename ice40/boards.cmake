37a2c5e5d20a8bba116d9540bee4cdd60f39561d
Keith Rothman <537074+litghost@users.noreply.github.com>
2018-08-09 14:10:02 -0700

    Add dummy boards.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- ice40/boards.cmake
+++ ice40/boards.cmake
@@ -1,5 +1,11 @@
+# Lattice iCEstick
+# http://www.latticesemi.com/icestick
+# ---------------------------------------------
 define_board(BOARD icestick DEVICE hx1k PACKAGE tq144 PROG_TOOL ${ICEPROG_TOOL})
 
+# Lattice iCEblink40-LP1K Evaluation Kit
+# **HX** version is different!
+# ---------------------------------------------
 define_board(
   BOARD iceblink40-lp1k
   DEVICE lp1k
@@ -8,6 +14,9 @@ define_board(
   PROG_CMD "${ICEPROG_TOOL} -ew"
 )
 
+# iCE40-HX8K Breakout Board Evaluation Kit
+# iCE40-HX8K-CT256
+# ---------------------------------------------
 define_board(
   BOARD hx8k-b-evn
   DEVICE hx8k
@@ -16,10 +25,12 @@ define_board(
   PROG_CMD "${ICEPROG_TOOL} -S"
 )
 
+# DPControl icevision board
+# iCE40UP5K-SG48
+# ---------------------------------------------
 define_board(
   BOARD icevision
   DEVICE up5k
   PACKAGE sg48
   PROG_TOOL ${ICEPROG_TOOL}
 )
-
