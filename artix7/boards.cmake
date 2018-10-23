e2e812ec153d7a08ee4ad246922e45c432ef18bf
Keith Rothman <537074+litghost@users.noreply.github.com>
2018-10-23 15:59:38 -0700

    Working artix7 .bit generation directly from symbiflow.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- artix7/boards.cmake
+++ artix7/boards.cmake
@@ -2,5 +2,6 @@ define_board(
   BOARD basys3
   DEVICE xc7a50t
   PACKAGE test
-  PROG_TOOL false
+  PROG_TOOL openocd
+  PROG_CMD "openocd -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \"init; pld load 0 \${OUT_BIN}; exit\""
 )
