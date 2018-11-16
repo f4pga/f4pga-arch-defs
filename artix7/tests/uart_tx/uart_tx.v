11c18115b4b8562ee8e8da5c95711b27d42b4b83
Keith Rothman <537074+litghost@users.noreply.github.com>
2018-11-16 13:07:15 -0800

    Fix FF INIT.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- artix7/tests/uart_tx/uart_tx.v
+++ artix7/tests/uart_tx/uart_tx.v
@@ -3,11 +3,11 @@ module UART_TX(
     input [7:0] data,
     output tx, data_accepted
 );
-    localparam START = 2'd1, DATA = 2'd2, END = 2'd3;
+    localparam START = (1 << 0), DATA = (1 << 1), END = (1 << 2);
 
     reg [7:0] data_reg;
     reg [2:0] data_counter;
-    reg [1:0] state = 0;
+    reg [3:0] state = 0;
     reg tx_reg;
     reg data_accepted_reg;
 
@@ -21,35 +21,32 @@ module UART_TX(
             data_accepted_reg <= 0;
         end else if(baud_edge) begin
             case(state)
-                START: begin
-                    tx_reg <= 0;
-                    data_counter <= 0;
-                    state <= DATA;
-                    data_accepted_reg <= 0;
-                end
-                DATA: begin
-                    tx_reg <= data_reg[data_counter];
-                    if(data_counter != 7) begin
-                        data_counter <= data_counter + 1;
-                    end else begin
-                        state <= END;
-                    end
-                    data_accepted_reg <= 0;
-                end
-                END: begin
-                    tx_reg <= 1;
-                    if(data_ready) begin
-                        data_reg <= data;
-                        data_accepted_reg <= 1;
-                        state <= START;
-                    end else begin
-                        data_accepted_reg <= 0;
-                    end
-                end
-                default: begin
-                    tx_reg <= 1;
+            START: begin
+                tx_reg <= 0;
+                data_counter <= 0;
+                state <= DATA;
+            end
+            DATA: begin
+                tx_reg <= data_reg[data_counter];
+                if(data_counter != 7) begin
+                    data_counter <= data_counter + 1;
+                end else begin
                     state <= END;
+                    data_counter <= 0;
                 end
+            end
+            END: begin
+                tx_reg <= 1;
+                if(data_ready) begin
+                    data_accepted_reg <= 1;
+                    data_reg <= data;
+                    state <= START;
+                end
+            end
+            default: begin
+                tx_reg <= 1;
+                state <= END;
+            end
             endcase
         end else begin
             data_accepted_reg <= 0;
