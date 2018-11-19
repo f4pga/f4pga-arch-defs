bda4f2a3edb8006c49cf7ecbd71206818970cc58
Keith Rothman <537074+litghost@users.noreply.github.com>
2018-11-19 14:50:36 -0800

    Continue to remove old CYINIT bugs.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- artix7/primitives/common_slice/cyinit_fabric.sim.v
+++
@@ -1,9 +0,0 @@
-(*blackbox*)
-module CYINIT_FABRIC(output CI_CHAIN, input CI_FABRIC);
-  (* DELAY_CONST_CI_FABRIC="10e-12" *)
-  wire CI_CHAIN;
-  wire CI_FABRIC;
-
-  assign CI_CHAIN = CI_FABRIC;
-endmodule
-
