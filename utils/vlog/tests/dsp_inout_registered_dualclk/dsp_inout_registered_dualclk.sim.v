8f1fc37f2ae8315f605ef265923cf3bf4a51be73
Alessandro Comodi <acomodi@antmicro.com>
2019-01-04 18:27:52 +0100

    utils/vlog/tests: enabled additional vlog tests
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- utils/vlog/tests/dsp_inout_registered_dualclk/dsp_inout_registered_dualclk.sim.v
+++ utils/vlog/tests/dsp_inout_registered_dualclk/dsp_inout_registered_dualclk.sim.v
@@ -1,6 +1,6 @@
 `include "../dsp_combinational/dsp_combinational.sim.v"
 
-/* DSP Block with register on both the inputs and the output */
+/* DSP Block with register on both the inputs and the output, which use different clocks*/
 module dsp_inout_registered_dualclk (iclk, oclk, a, b, m, out);
 	localparam DATA_WIDTH = 64;
 
@@ -22,7 +22,7 @@ module dsp_inout_registered_dualclk (iclk, oclk, a, b, m, out);
 	end
 
 	wire [DATA_WIDTH-1:0] c_out;
-	dsp_combinational comb (.a(a), .b(b), .m(m), .out(c_out));
+	dsp_combinational comb (.a(q_a), .b(q_b), .m(q_m), .out(c_out));
 
 	/* Output register on oclk */
 	reg [DATA_WIDTH-1:0] q_out;
