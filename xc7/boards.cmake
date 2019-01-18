d24faec249633ac24d1f62e6e56f8b5f5969c833
Alessandro Comodi <acomodi@antmicro.com>
2019-01-18 14:46:25 +0100

    .cmake: added zynq arch
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/boards.cmake
+++ xc7/boards.cmake
@@ -1,10 +1,19 @@
 get_target_property_required(OPENOCD env OPENOCD)
 get_target_property_required(OPENOCD_TARGET env OPENOCD_TARGET)
 
+#define_board(
+#  BOARD basys3
+#  DEVICE xc7a50t
+#  PACKAGE test
+#  PROG_TOOL ${OPENOCD_TARGET}
+#  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
+#)
+
+
 define_board(
-  BOARD basys3
-  DEVICE xc7a50t
+  BOARD zybo
+  DEVICE xc7z010
   PACKAGE test
   PROG_TOOL ${OPENOCD_TARGET}
-  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
+  #  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
 )
