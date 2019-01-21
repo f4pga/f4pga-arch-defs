4abe2b25b75d72d9cf8de4a4dbd0da6478c2b0f3
Alessandro Comodi <acomodi@antmicro.com>
2019-01-21 19:10:56 +0100

    xc7/cmake: fixes to add_xc7_arch_define
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/archs/artix7.cmake
+++ xc7/archs/artix7.cmake
@@ -1,13 +1,15 @@
-set(ROI_PART xc7a35tcpg236-1)
-set(ROI_DIR ${PRJXRAY_DB_DIR}/artix7/harness/basys3/swbut)
-
 project_xray_prepare_database(
   PART artix7
 )
 
-add_arch_define(
+add_xc7_arch_define(
   ARCH artix7
-  ROI_PART ${ROI_PART}
-  ROI_DIR ${ROI_DIR}
+  ROI_PART "xc7a35tcpg236-1"
+  ROI_DIR "${PRJXRAY_DB_DIR}/artix7/harness/basys3/swbut"
+  # -flatten is used to ensure that the output eblif has only one module.
+  # Some of symbiflow expects eblifs with only one module.
+  #
+  # opt -undriven makes sure all nets are driven, if only by the $undef
+  # net.
   YOSYS_SCRIPT "synth_xilinx -vpr -flatten $<SEMICOLON> opt_expr -undriven $<SEMICOLON> opt_clean"
 )
--- xc7/archs/zynq7.cmake
+++ xc7/archs/zynq7.cmake
@@ -1,13 +1,15 @@
-set(ROI_PART xc7z010clg400-1)
-set(ROI_DIR ${PRJXRAY_DB_DIR}/zynq7/harness/zybo/swbut)
+project_xray_prepare_database(
+  PART zynq7
+)
 
-#project_xray_prepare_database(
-  #  PART zynq7
-  #)
-
-add_arch_define(
+add_xc7_arch_define(
   ARCH zynq7
-  ROI_PART ${ROI_PART}
-  ROI_DIR ${ROI_DIR}
+  ROI_PART "xc7z010clg400-1"
+  ROI_DIR "${PRJXRAY_DB_DIR}/zynq7/harness/zybo/swbut"
+  # -flatten is used to ensure that the output eblif has only one module.
+  # Some of symbiflow expects eblifs with only one module.
+  #
+  # opt -undriven makes sure all nets are driven, if only by the $undef
+  # net.
   YOSYS_SCRIPT "synth_xilinx -vpr -flatten $<SEMICOLON> opt_expr -undriven $<SEMICOLON> opt_clean"
 )
