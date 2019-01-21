08676c800249654c5d57b441da5afacc7e4c37d0
Alessandro Comodi <acomodi@antmicro.com>
2019-01-21 18:15:14 +0100

    xc7/boards.cmake: enabling zybo and basys3
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/boards.cmake
+++ xc7/boards.cmake
@@ -1,19 +1,18 @@
 get_target_property_required(OPENOCD env OPENOCD)
 get_target_property_required(OPENOCD_TARGET env OPENOCD_TARGET)
 
-#define_board(
-#  BOARD basys3
-#  DEVICE xc7a50t
-#  PACKAGE test
-#  PROG_TOOL ${OPENOCD_TARGET}
-#  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
-#)
-
+define_board(
+  BOARD basys3
+  DEVICE xc7a50t
+  PACKAGE test
+  PROG_TOOL ${OPENOCD_TARGET}
+  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
+)
 
 define_board(
   BOARD zybo
   DEVICE xc7z010
   PACKAGE test
   PROG_TOOL ${OPENOCD_TARGET}
-  #  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
+  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
 )
