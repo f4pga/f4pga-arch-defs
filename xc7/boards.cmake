0bbebac9ad61f5f39d735d706175340b59b4c402
Alessandro Comodi <acomodi@antmicro.com>
2019-01-30 11:44:21 +0100

    xc7/boards: added TODO comment
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/boards.cmake
+++ xc7/boards.cmake
@@ -9,6 +9,7 @@ define_board(
   PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
 )
 
+# TODO: https://github.com/SymbiFlow/symbiflow-arch-defs/issues/344
 define_board(
   BOARD zybo
   DEVICE xc7z010
