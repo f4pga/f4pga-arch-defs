9fec346762eb449fed7cc03979f795d474507cf4
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-02-05 13:39:32 -0800

    Add BRAM automated test program.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

---
+++ xc7/tests/bram_test/lfsr.v
@@ -0,0 +1,19 @@
+module LFSR #(
+    parameter WIDTH = 16,
+    parameter POLY = 16'hD008
+) (
+    input rst,
+    input clk,
+    input [WIDTH-1:0] seed,
+    output reg [WIDTH-1:0] r
+);
+    wire feedback = ^(r & POLY);
+
+    always @(posedge clk) begin
+        if(rst) begin
+            r <= seed;
+        end else begin
+            r <= {r[WIDTH-2:0], feedback};
+        end
+    end
+endmodule
