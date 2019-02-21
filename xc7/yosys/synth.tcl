f73bdd5b35ba2dc7324a565097c6ffe59be4dd7f
Maciej Kurc <mkurc@antmicro.com>
2019-02-21 09:22:14 +0100

    Removed debug statements, enabled BRAM inference.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/yosys/synth.tcl
+++ xc7/yosys/synth.tcl
@@ -1,11 +1,10 @@
 yosys -import
-echo on
 
 # -flatten is used to ensure that the output eblif has only one module.
 # Some of symbiflow expects eblifs with only one module.
 #
 # Since we do not support BRAMs for now they are not inferred.
-synth_xilinx -vpr -flatten -no-brams
+synth_xilinx -vpr -flatten
 
 # Map Xilinx tech library to 7-series VPR tech library.
 read_verilog -lib $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/cells_sim.v
