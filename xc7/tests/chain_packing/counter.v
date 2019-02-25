f36d9f2ccfc1b3a5706c43b49b08b75dff198c11
Alessandro Comodi <acomodi@antmicro.com>
2019-02-25 16:42:03 +0100

    chain_packing: added fifth counter to include SLICEMs
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/tests/chain_packing/counter.v
+++ xc7/tests/chain_packing/counter.v
@@ -4,7 +4,7 @@ module top (
 	output LED2,
 	output LED3,
 	output LED4,
-	//output LED5
+	output LED5
 );
 
 	localparam LOG2DELAY = 8;
@@ -13,19 +13,19 @@ module top (
 	reg [LOG2DELAY-1:0] counter1 = 0;
 	reg [LOG2DELAY-1:0] counter2 = 0;
 	reg [LOG2DELAY-1:0] counter3 = 0;
-	//reg [LOG2DELAY-1:0] counter4 = 0;
+	reg [LOG2DELAY-1:0] counter4 = 0;
 
 	always @(posedge clk) begin
 		counter0 <= counter0 + 1;
 		counter1 <= counter1 + 1;
 		counter2 <= counter2 + 1;
 		counter3 <= counter3 + 1;
-		//counter4 <= counter4 + 1;
+		counter4 <= counter4 + 1;
 	end
 
 	assign LED1 = counter0[LOG2DELAY-1];
 	assign LED2 = counter1[LOG2DELAY-1];
 	assign LED3 = counter2[LOG2DELAY-1];
 	assign LED4 = counter3[LOG2DELAY-1];
-	//assign LED5 = counter4[LOG2DELAY-1];
+	assign LED5 = counter4[LOG2DELAY-1];
 endmodule
