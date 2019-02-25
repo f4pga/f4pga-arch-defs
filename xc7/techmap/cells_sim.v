c24110ef384487a54102ece0789e41dd551848dd
Maciej Kurc <mkurc@antmicro.com>
2019-02-25 10:18:39 +0100

    Removed unnecessary inversion parameters from _ZINI FFs
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/techmap/cells_sim.v
+++ xc7/techmap/cells_sim.v
@@ -4,52 +4,40 @@
 module FDRE_ZINI (output reg Q, input C, CE, D, R);
   parameter [0:0] ZINI = 1'b0;
   parameter [0:0] IS_C_INVERTED = 1'b0;
-  parameter [0:0] IS_D_INVERTED = 1'b0;
-  parameter [0:0] IS_R_INVERTED = 1'b0;
   initial Q <= !ZINI;
   generate case (|IS_C_INVERTED)
-    1'b0: always @(posedge C) if (R == !IS_R_INVERTED) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
-    1'b1: always @(negedge C) if (R == !IS_R_INVERTED) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
+    1'b0: always @(posedge C) if (R) Q <= 1'b0; else if (CE) Q <= D;
+    1'b1: always @(negedge C) if (R) Q <= 1'b0; else if (CE) Q <= D;
   endcase endgenerate
 endmodule
 
 module FDSE_ZINI (output reg Q, input C, CE, D, S);
   parameter [0:0] ZINI = 1'b0;
   parameter [0:0] IS_C_INVERTED = 1'b0;
-  parameter [0:0] IS_D_INVERTED = 1'b0;
-  parameter [0:0] IS_S_INVERTED = 1'b0;
   initial Q <= !ZINI;
   generate case (|IS_C_INVERTED)
-    1'b0: always @(posedge C) if (S == !IS_S_INVERTED) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
-    1'b1: always @(negedge C) if (S == !IS_S_INVERTED) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
+    1'b0: always @(posedge C) if (S) Q <= 1'b1; else if (CE) Q <= D;
+    1'b1: always @(negedge C) if (S) Q <= 1'b1; else if (CE) Q <= D;
   endcase endgenerate
 endmodule
 
 module FDCE_ZINI (output reg Q, input C, CE, D, CLR);
   parameter [0:0] ZINI = 1'b0;
   parameter [0:0] IS_C_INVERTED = 1'b0;
-  parameter [0:0] IS_D_INVERTED = 1'b0;
-  parameter [0:0] IS_CLR_INVERTED = 1'b0;
   initial Q <= !ZINI;
-  generate case ({|IS_C_INVERTED, |IS_CLR_INVERTED})
-    2'b00: always @(posedge C, posedge CLR) if ( CLR) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
-    2'b01: always @(posedge C, negedge CLR) if (!CLR) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
-    2'b10: always @(negedge C, posedge CLR) if ( CLR) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
-    2'b11: always @(negedge C, negedge CLR) if (!CLR) Q <= 1'b0; else if (CE) Q <= D ^ IS_D_INVERTED;
+  generate case (|IS_C_INVERTED)
+    1'b0: always @(posedge C, posedge CLR) if (CLR) Q <= 1'b0; else if (CE) Q <= D;
+    1'b1: always @(negedge C, posedge CLR) if (CLR) Q <= 1'b0; else if (CE) Q <= D;
   endcase endgenerate
 endmodule
 
 module FDPE_ZINI (output reg Q, input C, CE, D, PRE);
   parameter [0:0] ZINI = 1'b0;
   parameter [0:0] IS_C_INVERTED = 1'b0;
-  parameter [0:0] IS_D_INVERTED = 1'b0;
-  parameter [0:0] IS_PRE_INVERTED = 1'b0;
   initial Q <= !ZINI;
-  generate case ({|IS_C_INVERTED, |IS_PRE_INVERTED})
-    2'b00: always @(posedge C, posedge PRE) if ( PRE) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
-    2'b01: always @(posedge C, negedge PRE) if (!PRE) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
-    2'b10: always @(negedge C, posedge PRE) if ( PRE) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
-    2'b11: always @(negedge C, negedge PRE) if (!PRE) Q <= 1'b1; else if (CE) Q <= D ^ IS_D_INVERTED;
+  generate case (|IS_C_INVERTED)
+    1'b0: always @(posedge C, posedge PRE) if (PRE) Q <= 1'b1; else if (CE) Q <= D;
+    1'b1: always @(negedge C, posedge PRE) if (PRE) Q <= 1'b1; else if (CE) Q <= D;
   endcase endgenerate
 endmodule
 
