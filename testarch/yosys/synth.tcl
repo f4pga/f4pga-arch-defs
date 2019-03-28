3f532da8f3b108111357be128b59c397eb3a439b
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-03-28 10:46:49 -0700

    Add 10x10 test arch and add const sources.
    This is required to explore global routing errors found in #511.
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- testarch/yosys/synth.tcl
+++ testarch/yosys/synth.tcl
@@ -6,3 +6,8 @@ opt_clean
 
 write_blif -attr -cname -param $::env(OUT_EBLIF)
 write_verilog $::env(OUT_SYNTH_V)
+write_blif -attr -cname -param \
+ -true VCC VCC \
+ -false GND GND \
+ -undef VCC VCC \
+ $::env(OUT_EBLIF)
