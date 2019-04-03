f4pga/f4pga-arch-defs

commit 393e85351aa3924df097547daddd77e29f3a3b79

Working basic artix7 symbiflow (counter works!).
Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

utils/lib/rr_graph/channel.py
@@ -785,11 +785,9 @@ class ChannelGrid(dict):
         return occupied, net
 
     def fill_empty(self, segment_id, name=None):
-        tracks = []
         for pos, ti, t in self.foreach_track():
             if t is None:
-                tracks.append(
-                    self.create_track(
+                yield self.create_track(
                         Track(
                             pos,
                             pos,
@@ -797,8 +795,7 @@ class ChannelGrid(dict):
                             name=name,
                             type_hint=self.chan_type,
                             direction=Track.Direction.BI),
-                        idx=ti))
-        return tracks
+                        idx=ti)
 
     def channel_widths(self):
         """Return (min channel width, max channel width, row/col widths)"""

commit a6bd6011a8f09095f859b566e57a0cfb912f0e80

rr_graph: fix regression in channel.py fill_empty
introduced in 393e85351aa3924df097547daddd77e29f3a3b79 by using yield
in conjunction with a state modification.

Signed-off-by: Elms <elms@freshred.net>

utils/lib/rr_graph/channel.py
@@ -785,9 +785,11 @@ class ChannelGrid(dict):
         return occupied, net
 
     def fill_empty(self, segment_id, name=None):
+        tracks = []
         for pos, ti, t in self.foreach_track():
             if t is None:
-                yield self.create_track(
+                tracks.append(
+                    self.create_track(
                         Track(
                             pos,
                             pos,
@@ -796,6 +798,8 @@ class ChannelGrid(dict):
                             type_hint=self.chan_type,
                             direction=Track.Direction.BI),
                         idx=ti)
+                    )
+        return tracks
 
     def channel_widths(self):
         """Return (min channel width, max channel width, row/col widths)"""