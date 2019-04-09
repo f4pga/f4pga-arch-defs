eafd8a07d3de3e1c41a4f00ddd7af84b1accaf59
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-04-08 17:00:44 -0700

    Add support for naming top-level port names based on pcf file.
    This is useful for binding to test benches and equivilence miter
    circuits,
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- xc7/utils/prjxray_create_ioplace.py
+++ xc7/utils/prjxray_create_ioplace.py
@@ -1,36 +1,10 @@
 """ Convert a PCF file into a VPR io.place file. """
 from __future__ import print_function
 import argparse
-from collections import namedtuple
 import csv
 import sys
-import re
 import vpr_io_place
-
-PcfIoConstraint = namedtuple('PcfIoConstraint', 'net pad line_str line_num')
-
-
-def parse_simple_pcf(f):
-    for line_number, line in enumerate(f):
-        line_number += 1
-
-        # Remove comments.
-        args = re.sub(r"#.*", "", line.strip()).split()
-
-        if not args or args[0] != 'set_io':
-            continue
-
-        # Ignore arguments to set_io.
-        args = [arg for arg in args if arg[0] != '-']
-
-        assert len(args) == 3, args
-
-        yield PcfIoConstraint(
-            net=args[1],
-            pad=args[2],
-            line_str=line.strip(),
-            line_num=line_number,
-        )
+from lib.parse_pcf import parse_simple_pcf
 
 
 def main():
