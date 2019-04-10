e5ab5c4774fe53ceb5f616c0c14903c0ab89b5b3
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-04-10 08:46:44 -0700

    Run make format.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- xc7/tests/ff_sr_ce/generate.py
+++ xc7/tests/ff_sr_ce/generate.py
@@ -3,11 +3,16 @@ import re
 
 LOCALPARAM_RE = re.compile(r'^(\s*)localparam\s+([^\s]+)\s*=\s*\d+\s*;\s*$')
 
+
 def main():
     parser = argparse.ArgumentParser(description="")
 
     parser.add_argument('--template', required=True)
-    parser.add_argument('--params', required=True, help="Comma seperated parameter list to set.")
+    parser.add_argument(
+        '--params',
+        required=True,
+        help="Comma seperated parameter list to set."
+    )
 
     args = parser.parse_args()
 
@@ -26,13 +31,16 @@ def main():
                 param = m.group(2)
 
                 if param in params:
-                    print('{}localparam {} = {};'.format(
-                        prefix_ws, param, params[param]
-                        ))
+                    print(
+                        '{}localparam {} = {};'.format(
+                            prefix_ws, param, params[param]
+                        )
+                    )
                 else:
                     print(l.rstrip())
             else:
                 print(l.rstrip())
+
 
 if __name__ == "__main__":
     main()
