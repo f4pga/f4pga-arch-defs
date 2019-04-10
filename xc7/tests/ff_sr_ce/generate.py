88558749db5e54bd1e8353fb67c20d645f0c1611
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-04-10 11:32:42 -0700

    Correct output VCD names on templating.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- xc7/tests/ff_sr_ce/generate.py
+++ xc7/tests/ff_sr_ce/generate.py
@@ -1,13 +1,16 @@
 import argparse
+import os.path
 import re
 
 LOCALPARAM_RE = re.compile(r'^(\s*)localparam\s+([^\s]+)\s*=\s*\d+\s*;\s*$')
+DUMPVARS_RE = re.compile(r'^(\s*)\$dumpfile\([^\)]+\)\s*;\s*$')
 
 
 def main():
     parser = argparse.ArgumentParser(description="")
 
     parser.add_argument('--template', required=True)
+    parser.add_argument('--output', required=True)
     parser.add_argument(
         '--params',
         required=True,
@@ -23,23 +26,36 @@ def main():
         k, v = param.split('=')
         params[k] = v
 
-    with open(args.template) as f:
-        for l in f:
-            m = LOCALPARAM_RE.match(l)
-            if m:
-                prefix_ws = m.group(1)
-                param = m.group(2)
+    with open(args.output, 'w') as f_out:
+        with open(args.template) as f:
+            for l in f:
+                m = LOCALPARAM_RE.match(l)
+                m2 = DUMPVARS_RE.match(l)
+                if m:
+                    prefix_ws = m.group(1)
+                    param = m.group(2)
 
-                if param in params:
-                    print(
-                        '{}localparam {} = {};'.format(
-                            prefix_ws, param, params[param]
+                    if param in params:
+                        print(
+                            '{}localparam {} = {};'.format(
+                                prefix_ws, param, params[param]
+                            ),
+                            file=f_out
                         )
+                    else:
+                        print(l.rstrip(), file=f_out)
+                elif m2:
+                    prefix_ws = m2.group(1)
+                    base = os.path.basename(args.output)
+                    root, _ = os.path.splitext(base)
+                    print(
+                        '{}$dumpfile("testbench_{}.vcd");'.format(
+                            prefix_ws, root
+                        ),
+                        file=f_out
                     )
                 else:
-                    print(l.rstrip())
-            else:
-                print(l.rstrip())
+                    print(l.rstrip(), file=f_out)
 
 
 if __name__ == "__main__":
