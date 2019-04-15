ccc77bf1ddb08e3c029f25549063eedd68c9d02f
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-04-15 11:52:58 -0700

    Add comment about disabling round robin packing.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- xc7/make/arch_define.cmake
+++ xc7/make/arch_define.cmake
@@ -23,6 +23,8 @@ function(ADD_XC7_ARCH_DEFINE)
       --place_algorithm bounding_box
       --enable_timing_computations off
       --allow_unrelated_clustering on
+      # TODO: Consider removing round robin packing once tile split and
+      # equivilant tiles are in.
       --round_robin_prepacking on
     RR_PATCH_TOOL
       ${symbiflow-arch-defs_SOURCE_DIR}/xc7/utils/prjxray_routing_import.py
