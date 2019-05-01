1a65cf5f5706fc5e1d69ae276ad767a91bdd6d41
Elms <elms@freshred.net>
2019-05-01 12:08:00 -0700

    ice40: format-python add back non-zero timing for SB_RAM
    Signed-off-by: Elms <elms@freshred.net>

--- ice40/utils/ice40_import_bel_timing.py
+++ ice40/utils/ice40_import_bel_timing.py
@@ -9,117 +9,121 @@ import re
 import logging
 from collections import namedtuple
 
+
 class pin_map(namedtuple('pin_map', 'sdf arch is_clk')):
     """
     """
+
 
 """
 arch_name  - (sdfcelltype, [pin_map ] )
 """
 
 _arch_to_sdf = {
-    'LUT4': ('LogicCell40', [
-        pin_map('in0', 'in[0]', False),
-        pin_map('in1', 'in[1]', False),
-        pin_map('in2', 'in[2]', False),
-        pin_map('in3', 'in[3]', False),
-        pin_map('lcout', 'out', False)
-    ]
-    ),
-    'SB_LUT4': ('LogicCell40', [
-        pin_map('in0', 'I0', False),
-        pin_map('in1', 'I1', False),
-        pin_map('in2', 'I2', False),
-        pin_map('in3', 'I3', False),
-        pin_map('lcout', 'O', False)
-    ]
-    ),
-    'SB_RAM256x16': ('SB_RAM40_4K', [
-        pin_map('RDATA[0]', 'RDATA[0]', False),
-        pin_map('RDATA[1]', 'RDATA[1]', False),
-        pin_map('RDATA[2]', 'RDATA[2]', False),
-        pin_map('RDATA[3]', 'RDATA[3]', False),
-        pin_map('RDATA[4]', 'RDATA[4]', False),
-        pin_map('RDATA[5]', 'RDATA[5]', False),
-        pin_map('RDATA[6]', 'RDATA[6]', False),
-        pin_map('RDATA[7]', 'RDATA[7]', False),
-        pin_map('RDATA[8]', 'RDATA[8]', False),
-        pin_map('RDATA[9]', 'RDATA[9]', False),
-        pin_map('RDATA[10]', 'RDATA[10]', False),
-        pin_map('RDATA[11]', 'RDATA[11]', False),
-        pin_map('RDATA[12]', 'RDATA[12]', False),
-        pin_map('RDATA[13]', 'RDATA[13]', False),
-        pin_map('RDATA[14]', 'RDATA[14]', False),
-        pin_map('RDATA[15]', 'RDATA[15]', False),
-        pin_map('RCLK', 'RCLK', True),
-        pin_map('RE', 'RE', False),
-        pin_map('RCLKE', 'RCLKE', False),
-        pin_map('RADDR[0]', 'RADDR[0]', False),
-        pin_map('RADDR[1]', 'RADDR[1]', False),
-        pin_map('RADDR[2]', 'RADDR[2]', False),
-        pin_map('RADDR[3]', 'RADDR[3]', False),
-        pin_map('RADDR[4]', 'RADDR[4]', False),
-        pin_map('RADDR[5]', 'RADDR[5]', False),
-        pin_map('RADDR[6]', 'RADDR[6]', False),
-        pin_map('RADDR[7]', 'RADDR[7]', False),
-        pin_map('RADDR[8]', 'RADDR[8]', False),
-        pin_map('RADDR[9]', 'RADDR[9]', False),
-        pin_map('RADDR[10]', 'RADDR[10]', False),
-
-        pin_map('WDATA[0]', 'WDATA[0]', False),
-        pin_map('WDATA[1]', 'WDATA[1]', False),
-        pin_map('WDATA[2]', 'WDATA[2]', False),
-        pin_map('WDATA[3]', 'WDATA[3]', False),
-        pin_map('WDATA[4]', 'WDATA[4]', False),
-        pin_map('WDATA[5]', 'WDATA[5]', False),
-        pin_map('WDATA[6]', 'WDATA[6]', False),
-        pin_map('WDATA[7]', 'WDATA[7]', False),
-        pin_map('WDATA[8]', 'WDATA[8]', False),
-        pin_map('WDATA[9]', 'WDATA[9]', False),
-        pin_map('WDATA[10]', 'WDATA[10]', False),
-        pin_map('WDATA[11]', 'WDATA[11]', False),
-        pin_map('WDATA[12]', 'WDATA[12]', False),
-        pin_map('WDATA[13]', 'WDATA[13]', False),
-        pin_map('WDATA[14]', 'WDATA[14]', False),
-        pin_map('WDATA[15]', 'WDATA[15]', False),
-        pin_map('WCLK', 'WCLK', True),
-        pin_map('WE', 'WE', False),
-        pin_map('WCLKE', 'WCLKE', False),
-        pin_map('WADDR[0]', 'WADDR[0]', False),
-        pin_map('WADDR[1]', 'WADDR[1]', False),
-        pin_map('WADDR[2]', 'WADDR[2]', False),
-        pin_map('WADDR[3]', 'WADDR[3]', False),
-        pin_map('WADDR[4]', 'WADDR[4]', False),
-        pin_map('WADDR[5]', 'WADDR[5]', False),
-        pin_map('WADDR[6]', 'WADDR[6]', False),
-        pin_map('WADDR[7]', 'WADDR[7]', False),
-        pin_map('WADDR[8]', 'WADDR[8]', False),
-        pin_map('WADDR[9]', 'WADDR[9]', False),
-        pin_map('WADDR[10]', 'WADDR[10]', False),
-
-        pin_map('MASK[0]', 'MASK[0]', False),
-        pin_map('MASK[1]', 'MASK[1]', False),
-        pin_map('MASK[2]', 'MASK[2]', False),
-        pin_map('MASK[3]', 'MASK[3]', False),
-        pin_map('MASK[4]', 'MASK[4]', False),
-        pin_map('MASK[5]', 'MASK[5]', False),
-        pin_map('MASK[6]', 'MASK[6]', False),
-        pin_map('MASK[7]', 'MASK[7]', False),
-        pin_map('MASK[8]', 'MASK[8]', False),
-        pin_map('MASK[9]', 'MASK[9]', False),
-        pin_map('MASK[10]', 'MASK[10]', False),
-        pin_map('MASK[11]', 'MASK[11]', False),
-        pin_map('MASK[12]', 'MASK[12]', False),
-        pin_map('MASK[13]', 'MASK[13]', False),
-        pin_map('MASK[14]', 'MASK[14]', False),
-        pin_map('MASK[15]', 'MASK[15]', False),
-
-    ]
-    ),
-    'SB_CARRY': ('ICE_CARRY_IN_MUX', [
-    ]
-    )
+    'LUT4':
+        (
+            'LogicCell40', [
+                pin_map('in0', 'in[0]', False),
+                pin_map('in1', 'in[1]', False),
+                pin_map('in2', 'in[2]', False),
+                pin_map('in3', 'in[3]', False),
+                pin_map('lcout', 'out', False)
+            ]
+        ),
+    'SB_LUT4':
+        (
+            'LogicCell40', [
+                pin_map('in0', 'I0', False),
+                pin_map('in1', 'I1', False),
+                pin_map('in2', 'I2', False),
+                pin_map('in3', 'I3', False),
+                pin_map('lcout', 'O', False)
+            ]
+        ),
+    'SB_RAM256x16':
+        (
+            'SB_RAM40_4K', [
+                pin_map('RDATA[0]', 'RDATA[0]', False),
+                pin_map('RDATA[1]', 'RDATA[1]', False),
+                pin_map('RDATA[2]', 'RDATA[2]', False),
+                pin_map('RDATA[3]', 'RDATA[3]', False),
+                pin_map('RDATA[4]', 'RDATA[4]', False),
+                pin_map('RDATA[5]', 'RDATA[5]', False),
+                pin_map('RDATA[6]', 'RDATA[6]', False),
+                pin_map('RDATA[7]', 'RDATA[7]', False),
+                pin_map('RDATA[8]', 'RDATA[8]', False),
+                pin_map('RDATA[9]', 'RDATA[9]', False),
+                pin_map('RDATA[10]', 'RDATA[10]', False),
+                pin_map('RDATA[11]', 'RDATA[11]', False),
+                pin_map('RDATA[12]', 'RDATA[12]', False),
+                pin_map('RDATA[13]', 'RDATA[13]', False),
+                pin_map('RDATA[14]', 'RDATA[14]', False),
+                pin_map('RDATA[15]', 'RDATA[15]', False),
+                pin_map('RCLK', 'RCLK', True),
+                pin_map('RE', 'RE', False),
+                pin_map('RCLKE', 'RCLKE', False),
+                pin_map('RADDR[0]', 'RADDR[0]', False),
+                pin_map('RADDR[1]', 'RADDR[1]', False),
+                pin_map('RADDR[2]', 'RADDR[2]', False),
+                pin_map('RADDR[3]', 'RADDR[3]', False),
+                pin_map('RADDR[4]', 'RADDR[4]', False),
+                pin_map('RADDR[5]', 'RADDR[5]', False),
+                pin_map('RADDR[6]', 'RADDR[6]', False),
+                pin_map('RADDR[7]', 'RADDR[7]', False),
+                pin_map('RADDR[8]', 'RADDR[8]', False),
+                pin_map('RADDR[9]', 'RADDR[9]', False),
+                pin_map('RADDR[10]', 'RADDR[10]', False),
+                pin_map('WDATA[0]', 'WDATA[0]', False),
+                pin_map('WDATA[1]', 'WDATA[1]', False),
+                pin_map('WDATA[2]', 'WDATA[2]', False),
+                pin_map('WDATA[3]', 'WDATA[3]', False),
+                pin_map('WDATA[4]', 'WDATA[4]', False),
+                pin_map('WDATA[5]', 'WDATA[5]', False),
+                pin_map('WDATA[6]', 'WDATA[6]', False),
+                pin_map('WDATA[7]', 'WDATA[7]', False),
+                pin_map('WDATA[8]', 'WDATA[8]', False),
+                pin_map('WDATA[9]', 'WDATA[9]', False),
+                pin_map('WDATA[10]', 'WDATA[10]', False),
+                pin_map('WDATA[11]', 'WDATA[11]', False),
+                pin_map('WDATA[12]', 'WDATA[12]', False),
+                pin_map('WDATA[13]', 'WDATA[13]', False),
+                pin_map('WDATA[14]', 'WDATA[14]', False),
+                pin_map('WDATA[15]', 'WDATA[15]', False),
+                pin_map('WCLK', 'WCLK', True),
+                pin_map('WE', 'WE', False),
+                pin_map('WCLKE', 'WCLKE', False),
+                pin_map('WADDR[0]', 'WADDR[0]', False),
+                pin_map('WADDR[1]', 'WADDR[1]', False),
+                pin_map('WADDR[2]', 'WADDR[2]', False),
+                pin_map('WADDR[3]', 'WADDR[3]', False),
+                pin_map('WADDR[4]', 'WADDR[4]', False),
+                pin_map('WADDR[5]', 'WADDR[5]', False),
+                pin_map('WADDR[6]', 'WADDR[6]', False),
+                pin_map('WADDR[7]', 'WADDR[7]', False),
+                pin_map('WADDR[8]', 'WADDR[8]', False),
+                pin_map('WADDR[9]', 'WADDR[9]', False),
+                pin_map('WADDR[10]', 'WADDR[10]', False),
+                pin_map('MASK[0]', 'MASK[0]', False),
+                pin_map('MASK[1]', 'MASK[1]', False),
+                pin_map('MASK[2]', 'MASK[2]', False),
+                pin_map('MASK[3]', 'MASK[3]', False),
+                pin_map('MASK[4]', 'MASK[4]', False),
+                pin_map('MASK[5]', 'MASK[5]', False),
+                pin_map('MASK[6]', 'MASK[6]', False),
+                pin_map('MASK[7]', 'MASK[7]', False),
+                pin_map('MASK[8]', 'MASK[8]', False),
+                pin_map('MASK[9]', 'MASK[9]', False),
+                pin_map('MASK[10]', 'MASK[10]', False),
+                pin_map('MASK[11]', 'MASK[11]', False),
+                pin_map('MASK[12]', 'MASK[12]', False),
+                pin_map('MASK[13]', 'MASK[13]', False),
+                pin_map('MASK[14]', 'MASK[14]', False),
+                pin_map('MASK[15]', 'MASK[15]', False),
+            ]
+        ),
+    'SB_CARRY': ('ICE_CARRY_IN_MUX', [])
 }
+
 
 def get_scale(timescale):
     """Convert sdf timescale to scale factor
@@ -177,11 +181,7 @@ def main():
         help='arch xml file to write with updaed timing calues'
     )
 
-    parser.add_argument(
-        '-v',
-        type=bool,
-        help='verbose output'
-    )
+    parser.add_argument('-v', type=bool, help='verbose output')
 
     logging.basicConfig(level=logging.INFO)
 
@@ -242,8 +242,12 @@ def main():
             if pb_name in _arch_to_sdf.keys():
                 cell_name, pin_table = _arch_to_sdf.get(pb_name, None)
                 for timing in flat_timing[cell_name]:
+
                     def try_translate_pin(table, timing, name):
-                        res = [entry for entry in table if entry.sdf == timing[name]]
+                        res = [
+                            entry for entry in table
+                            if entry.sdf == timing[name]
+                        ]
                         assert len(res) <= 1
 
                         if len(res) == 1:
@@ -253,34 +257,50 @@ def main():
 
                     if timing['type'] == 'hold' or timing['type'] == 'setup':
                         topin = try_translate_pin(pin_table, timing, 'to_pin')
-                        frompin = try_translate_pin(pin_table, timing, 'from_pin')
+                        frompin = try_translate_pin(
+                            pin_table, timing, 'from_pin'
+                        )
                         if topin is None or frompin is None:
                             continue
-                        attribs = {'clock': frompin.arch,
-                                   'port': '{}.{}'.format(pb_name, topin.arch),
-                                   'value': get_pessimistic(timing)
+                        attribs = {
+                            'clock': frompin.arch,
+                            'port': '{}.{}'.format(pb_name, topin.arch),
+                            'value': get_pessimistic(timing)
                         }
-                        hold_setup_el = ET.SubElement(el, 'T_{}'.format(timing['type']), attribs)
+                        hold_setup_el = ET.SubElement(
+                            el, 'T_{}'.format(timing['type']), attribs
+                        )
                         # hold_setup_el.tail = '\n'
                         logging.info(ET.tostring(hold_setup_el))
                     elif timing['type'] == 'iopath':
                         topin = try_translate_pin(pin_table, timing, 'to_pin')
-                        frompin = try_translate_pin(pin_table, timing, 'from_pin')
+                        frompin = try_translate_pin(
+                            pin_table, timing, 'from_pin'
+                        )
                         if topin is None or frompin is None:
                             continue
                         if frompin.is_clk:
-                            attribs = {'clock': '{}'.format(frompin.arch),
-                            'port': '{}.{}'.format(pb_name, topin.arch),
-                            'max': get_pessimistic(timing)
+                            attribs = {
+                                'clock': '{}'.format(frompin.arch),
+                                'port': '{}.{}'.format(pb_name, topin.arch),
+                                'max': get_pessimistic(timing)
                             }
-                            iopath_el = ET.SubElement(el, 'T_clock_to_Q', attribs)
+                            iopath_el = ET.SubElement(
+                                el, 'T_clock_to_Q', attribs
+                            )
                             # iopath_el.tail = '\n'
                         else:
-                            attribs = {'in_port': '{}.{}'.format(pb_name, frompin.arch),
-                            'out_port': '{}.{}'.format(pb_name, topin.arch),
-                            'max': get_pessimistic(timing)
+                            attribs = {
+                                'in_port':
+                                    '{}.{}'.format(pb_name, frompin.arch),
+                                'out_port':
+                                    '{}.{}'.format(pb_name, topin.arch),
+                                'max':
+                                    get_pessimistic(timing)
                             }
-                            iopath_el = ET.SubElement(el, 'delay_constant', attribs)
+                            iopath_el = ET.SubElement(
+                                el, 'delay_constant', attribs
+                            )
                             # iopath_el.tail = '\n'
                         logging.info(ET.tostring(iopath_el))
                     elif timing['type'] == 'recovery':
@@ -290,6 +310,7 @@ def main():
 
     xml_str = ET.tostring(tree, pretty_print=True).decode('utf-8')
     args.write_arch_xml.write(xml_str)
+
 
 if __name__ == '__main__':
     main()
