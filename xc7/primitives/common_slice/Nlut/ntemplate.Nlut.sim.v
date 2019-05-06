0a638f42c7d3eaef0eeac9e102c9fc7d8f319923
Karol Gugala <kgugala@antmicro.com>
2019-05-06 10:12:04 +0200

    xc7: NtemplateNlut remove modes attribute
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- xc7/primitives/common_slice/Nlut/ntemplate.Nlut.sim.v
+++ xc7/primitives/common_slice/Nlut/ntemplate.Nlut.sim.v
@@ -2,54 +2,35 @@
 // A fracturable 6 input LUT. Can either be;
 //  - 2 * 5 input, 1 output LUT
 //  - 1 * 6 input, 1 output LUT
-(* blackbox *)  (* CLASS="lut" *)
+`include "../muxes/f6mux/f6mux.sim.v"
+`include "ntemplate.N5lut.sim.v"
+
 module {N}LUT(A1, A2, A3, A4, A5, A6, O6, O5);
 
-	(* PORT_CLASS = "lut_in" *)
 	input wire A1;
-	(* PORT_CLASS = "lut_in" *)
 	input wire A2;
-	(* PORT_CLASS = "lut_in" *)
 	input wire A3;
-	(* PORT_CLASS = "lut_in" *)
 	input wire A4;
-	(* PORT_CLASS = "lut_in" *)
 	input wire A5;
-	(* PORT_CLASS = "lut_in" *)
 	input wire A6;
-
-	(* PORT_CLASS = "lut_out" *)
-	(* DELAY_CONST_A1 = "{iopath_A1_O6}" *)
-	(* DELAY_CONST_A2 = "{iopath_A2_O6}" *)
-	(* DELAY_CONST_A3 = "{iopath_A3_O6}" *)
-	(* DELAY_CONST_A4 = "{iopath_A4_O6}" *)
-	(* DELAY_CONST_A5 = "{iopath_A5_O6}" *)
-	(* DELAY_CONST_A6 = "{iopath_A6_O6}" *)
-	output wire O6;
-	(* PORT_CLASS = "lut_out" *)
-	(* DELAY_CONST_A1 = "{iopath_A1_O5}" *)
-	(* DELAY_CONST_A2 = "{iopath_A2_O5}" *)
-	(* DELAY_CONST_A3 = "{iopath_A3_O5}" *)
-	(* DELAY_CONST_A4 = "{iopath_A4_O5}" *)
-	(* DELAY_CONST_A5 = "{iopath_A5_O5}" *)
 	output wire O5;
+	output wire O6;
 
-	parameter [63:0] INIT = 0;
-	// LUT5 (upper)
-	wire [15: 0] upper_s4 = A5 ?       INIT[63:48] :     INIT[47:32];
-	wire [ 7: 0] upper_s3 = A4 ?   upper_s4[15: 8] : upper_s4[ 7: 0];
-	wire [ 3: 0] upper_s2 = A3 ?   upper_s3[ 7: 4] : upper_s3[ 3: 0];
-	wire [ 1: 0] upper_s1 = A2 ?   upper_s2[ 3: 2] : upper_s2[ 1: 0];
-	wire         upper_O  = A1 ?   upper_s1[    1] : upper_s1[    0];
+	wire upper_O;
+	wire lower_O;
 
-	// LUT5 (lower)
-	wire [15: 0] lower_s4 = A5 ?       INIT[31:16] :     INIT[15: 0];
-	wire [ 7: 0] lower_s3 = A4 ?   lower_s4[15: 8] : lower_s4[ 7: 0];
-	wire [ 3: 0] lower_s2 = A3 ?   lower_s3[ 7: 4] : lower_s3[ 3: 0];
-	wire [ 1: 0] lower_s1 = A2 ?   lower_s2[ 3: 2] : lower_s2[ 1: 0];
-	wire         lower_O  = A1 ?   lower_s1[    1] : lower_s1[    0];
+	{N}5LUT LUT5_0 (.in({{A5, A4, A3, A2, A1}}),
+			.out(lower_O));
+
+	{N}5LUT LUT5_1 (.in({{A5, A4, A3, A2, A1}}),
+			.out(upper_O));
+
 	assign O5 = lower_O;
 
-	// MUXF6
-	assign O6 = A6 ? upper_O : lower_O;
+	F6MUX F6MUX_0 (.I0(upper_O),
+		       .I1(lower_O),
+		       .S(A6),
+		       .OUT(O6));
+
 endmodule
+
