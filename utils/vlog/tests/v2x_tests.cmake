975909ee5822597b9b226aa154879d1e1ae0f8fe
Tim 'mithro' Ansell <me@mith.ro>
2019-05-06 19:32:05 -0700

    utils: Generate layout based on leaf model which will be used.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- utils/vlog/tests/v2x_tests.cmake
+++ utils/vlog/tests/v2x_tests.cmake
@@ -157,6 +157,7 @@ function(V2X_TEST_BOTH)
   v2x_test_generic(NAME ${NAME} TOP_MODULE ${MODULE} TYPE pb_type)
   v2x_test_generic(NAME ${NAME} TOP_MODULE ${TOP_MODULE} TYPE model)
   vpr_test_pbtype(NAME ${NAME})
+  add_dependencies(all_v2x_tests test_${NAME})
 
   add_custom_target(${NAME}_diff ALL DEPENDS ${NAME}_pb_type_diff ${NAME}_model_diff)
 endfunction(V2X_TEST_BOTH)
