6364863c9a6c9dac7ab194f0031252c63bf24035
Maciej Kurc <mkurc@antmicro.com>
2019-05-08 09:34:52 +0200

    Fixed verilog code style.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- utils/vlog/tests/fasm_features_ic_direct/fasm_features_ic_direct.sim.v
+++ utils/vlog/tests/fasm_features_ic_direct/fasm_features_ic_direct.sim.v
@@ -1,32 +1,22 @@
 `include "../common/and_gate.sim.v"
 `include "../common/xor_gate.sim.v"
 
-module fasm_features_ic_direct (
-  input  INP0,
-  input  INP1,
-  output OUT0,
-  output OUT1,
-  output OUT2,
-  output OUT3
-);
-
-wire INP0;
-wire INP1;
-
+module fasm_features_ic_direct (I0, I1, O0, O1, O2, O3);
+  input  wire I0;
+  input  wire I1;
 (* FASM_MUX = "out0_fasm_mux" *)
-wire OUT0;
+  output wire O0;
 (* FASM_MUX = "out1_fasm_mux" *)
-wire OUT1;
+  output wire O1;
 (* FASM_MUX = "out2_fasm_mux" *)
-wire OUT2;
+  output wire O2;
+  output wire O3;
 
-wire OUT3;
+ and_gate gate1(I0, I1, O0);
+ xor_gate gate2(I0, I1, O1);
 
-and_gate gate1(INP0, INP1, OUT0);
-xor_gate gate2(INP0, INP1, OUT1);
-
-assign OUT2 = INP0;
-assign OUT3 = INP1;
+ assign O2 = I0;
+ assign O3 = I1;
 
 endmodule
 
--- utils/vlog/tests/fasm_features_modes/fasm_features_modes.sim.v
+++ utils/vlog/tests/fasm_features_modes/fasm_features_modes.sim.v
@@ -5,23 +5,22 @@
 (* FASM_PREFIX_AND = "TOP_FASM_PREFIX_FOR_AND" *)
 (* FASM_PREFIX_XOR = "TOP_FASM_PREFIX_FOR_XOR" *)
 (* MODES = "AND; XOR" *)
-module fasm_features_modes (
-  input  wire I0,
-  input  wire I1,
-  output wire O
-);
+module fasm_features_modes (I0, I1, O);
+  input  wire I0;
+  input  wire I1;
+  output wire O;
 
-parameter MODE = "AND";
+  parameter MODE = "AND";
 
-generate
-    if (MODE == "AND") begin
-        (* FASM_PREFIX = "THIS_IS_THE_AND_GATE" *)
-        and_gate gate(I0, I1, O);
-    end else if (MODE == "XOR") begin
-        (* FASM_FEATURES = "THIS_IS_THE_XOR_GATE" *)
-        xor_gate gate(I0, I1, O);
-    end
-endgenerate
+ generate
+     if (MODE == "AND") begin
+         (* FASM_PREFIX = "THIS_IS_THE_AND_GATE" *)
+         and_gate gate(I0, I1, O);
+     end else if (MODE == "XOR") begin
+         (* FASM_FEATURES = "THIS_IS_THE_XOR_GATE" *)
+         xor_gate gate(I0, I1, O);
+     end
+ endgenerate
 
 endmodule
 
