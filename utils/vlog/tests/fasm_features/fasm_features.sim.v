36f25ab18b967d7d876314ce5a5216d28a022aa4
Maciej Kurc <mkurc@antmicro.com>
2019-05-09 11:53:04 +0200

    Updated test for LUT FASM features so they reflect realworld cases more closely
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- utils/vlog/tests/fasm_features/fasm_features.sim.v
+++ utils/vlog/tests/fasm_features/fasm_features.sim.v
@@ -1,7 +1,6 @@
 (* blackbox *)
-(* FASM_PREFIX = "MY_CELL" *)
-(* FASM_FEATURES = "USE_SOMETHING" *)
-(* FASM_LUT = "XLUT.INIT[7:0] = XLUT[0]; XLUT.INIT[15:8] = XLUT[1]" *)
+(* FASM_PREFIX = "MY_FASM_PREFIX" *)
+(* FASM_FEATURES = "MY_FASM_FEATURE_A MY_FASM_FEATURE_B" *)
 module fasm_features (I, O);
   input  wire I;
   output wire O;
