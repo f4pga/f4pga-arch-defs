52f3a3b46e6407f0ff5ed50ce1a9717973c8a0ca
Maciej Kurc <mkurc@antmicro.com>
2019-05-13 17:49:29 +0200

    Updated tests to work with VPR verification. Disabled tests which need more work to make them pass
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- utils/vlog/tests/fasm_features_lut/fasm_features_lut.sim.v
+++ utils/vlog/tests/fasm_features_lut/fasm_features_lut.sim.v
@@ -2,16 +2,13 @@
 (* CLASS = "lut" *)
 (* FASM_TYPE = "LUT" *)
 (* FASM_LUT = "FASM_FEATURES_LUT.INIT[1:0] = XLUT[0] FASM_FEATURES_LUT.INIT[3:2] = XLUT[1]" *)
-module fasm_features_lut (I0, I1, I2, I3, O);
-  input  wire I0;
-  input  wire I1;
-  input  wire I2;
-  input  wire I3;
+module fasm_features_lut (I, O);
+  input  wire [3:0] I;
   output wire O;
 
   parameter [15:0] INIT = 0;
 
-  assign O = INIT[{I3,I2,I1,I0}];
+  assign O = INIT[I];
 
 endmodule
 
