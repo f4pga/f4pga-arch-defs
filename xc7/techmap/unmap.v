590cdbb9e330b6a5ca8832a7c982aeacc791dcdd
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-05-22 17:04:00 -0700

    Output pre-symbiflow techmap'd Verilog.
    This verilog can be used with Vivado synthesis if xc7/techmap/unmap.v is
    applied.
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

---
+++ xc7/techmap/unmap.v
@@ -0,0 +1,17 @@
+module CARRY4_COUT(output [3:0] CO, O, output COUT, input CI, CYINIT, input [3:0] DI, S);
+
+wire [3:0] CO_INTERNAL;
+
+assign COUT = CO_INTERNAL[3];
+assign CO = CO_INTERNAL;
+
+CARRY4 _TECHMAP_REPLACE_ (
+    .CO(CO_INTERNAL),
+    .O(O),
+    .CI(CI),
+    .CYINIT(CYINIT),
+    .DI(DI),
+    .S(S)
+);
+
+endmodule
