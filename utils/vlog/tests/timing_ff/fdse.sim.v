bcebea0d116d179a32729321a4d150ac88816768
Maciej Kurc <mkurc@antmicro.com>
2019-05-24 12:27:45 +0200

    Added FF timing test
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

---
+++ utils/vlog/tests/timing_ff/fdse.sim.v
@@ -0,0 +1,14 @@
+module fdse (DIN, CE, CLK, S, OUT);
+ (* SDF_ALIAS = "D" *)
+ input  wire DIN;
+ input  wire CE;
+ input  wire CLK;
+ input  wire S;
+ (* SDF_ALIAS = "Q" *)
+ output reg  OUT;
+
+ always @(posedge CLK or posedge S)
+  if      (S)   OUT <= 1'd0;
+  else if (CE)  OUT <= DIN;
+
+endmodule
