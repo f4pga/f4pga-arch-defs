202c4935b1623f41c66773ebb8a05294e6b7dfb3
Tim 'mithro' Ansell <me@mith.ro>
2019-05-27 13:58:55 -0700

    infra: Keep running and then rerun the errors.
    To make it easier to see the errors when there is a failure on Kokoro
    while still running everything in parallel, we first;
    
     1. Run tests in parallel and keep going on failure until all targets
        which can build have done so (`-k`).
    
     2. Re-run in single threaded mode, stopping at the first error.
    
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- .github/kokoro/xc7.sh
+++ .github/kokoro/xc7.sh
@@ -11,7 +11,9 @@ echo "Running xc7 tests"
 echo "----------------------------------------"
 (
 	cd build
-	VPR_NUM_WORKERS=${CORES} make -j ${MAX_CORES} --output-sync=target \
-		all_xc7
+	export VPR_NUM_WORKERS=${CORES}
+	export MAKE_ARGS="-j${MAX_CORES} --output-sync=target"
+	# Run as many tests as we can.  Rerun individually on failure.
+	make -k ${MAKE_ARGS} all_xc7 || make all_xc7
 )
 echo "----------------------------------------"
