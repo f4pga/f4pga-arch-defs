2c32a53b01e3e447b31049c1bc64b8baef12a2fd
Tim 'mithro' Ansell <me@mith.ro>
2019-06-05 11:25:45 -0700

    v2x: Fix the multiple_instance test.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- utils/vlog/tests/multiple_instance/multiple_instance.sim.v
+++ utils/vlog/tests/multiple_instance/multiple_instance.sim.v
@@ -1,29 +1,33 @@
 `include "../fig41-full-adder/adder.sim.v"
-module MULTIPLE_INSTANCE (a, b, c, d, cin, cout, sum);
+module MULTIPLE_INSTANCE (a, b, c, d, cin, cout, sum, a2b);
 	localparam DATA_WIDTH = 4;
 
-	input wire [DATA_WIDTH-1:0] a;
-	input wire [DATA_WIDTH-1:0] b;
-	input wire [DATA_WIDTH-1:0] c;
-	input wire [DATA_WIDTH-1:0] d;
-	input wire cin;
-	output wire cout;
+	input  wire [DATA_WIDTH-1:0] a;
+	input  wire [DATA_WIDTH-1:0] b;
+	input  wire [DATA_WIDTH-1:0] c;
+	input  wire [DATA_WIDTH-1:0] d;
 	output wire [DATA_WIDTH*2-1:0] sum;
 
-	wire [DATA_WIDTH-1:0] a2b;
+	input  wire [DATA_WIDTH-1:0] cin;
+	output wire [DATA_WIDTH-1:0] cout;
+
+	/* FIXME: Once carry works make this an internal only wire */
+	output wire [DATA_WIDTH-1:0] a2b;
 
 	genvar i;
+	/* n = 0..DATA_WIDTH
+	 *
+	 *       cin[n]
+	 *        ↓
+	 *   a[n] + b[n] → sum[n]
+	 *        ↓
+	 *   c[n] + d[n] → sum[4+n]
+	 *        ↓
+	 *      cout[n]
+	 */
 	for(i=0; i<DATA_WIDTH; i=i+1) begin
-		ADDER comba (.a(a[i]), .b(b[i]), .cin(cin), .cout(a2b[i]), .sum(sum[i]));
-	end
-
-	genvar j;
-	for(j=0; j<DATA_WIDTH; j=j+1) begin
-		if ( j < DATA_WIDTH-1 ) begin
-			ADDER combb (.a(c[j]), .b(d[j]), .cin(a2b[j]), .sum(sum[DATA_WIDTH+j]));
-		end else begin
-			ADDER combb (.a(c[j]), .b(d[j]), .cin(a2b[j]), .cout(cout), .sum(sum[DATA_WIDTH+j]));
-		end
+		ADDER comb_apb (.a(a[i]), .b(b[i]), .cin(cin[i]), .cout(a2b[i]),  .sum(sum[i]));
+		ADDER comb_cpd (.a(c[i]), .b(d[i]), .cin(a2b[i]), .cout(cout[i]), .sum(sum[DATA_WIDTH+i]));
 	end
 
 endmodule
