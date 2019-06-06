5517c3478fe72464b5ad5e1cb95dfedf22f5fde5
Elms <elmsfu@freshred.net>
2019-06-05 22:52:37 -0700

    flake8: all but ice40 and xc7 excludes are passing
    Signed-off-by: Elms <elmsfu@freshred.net>

--- vpr/muxes/logic/mux2/simtest/test_mux2.py
+++ vpr/muxes/logic/mux2/simtest/test_mux2.py
@@ -23,10 +23,11 @@ def mux2_basic_test(dut, inputs=(1, 0, 0)):
         expected = I0
     yield Timer(2)
 
-    if dut.O != expected:
+    if dut.O != expected:  # noqa: E741
         raise TestFailure(
-            'Result is incorrect for I0(%d) I1(%d) S0(%d): %s(O) != %s (expected)'\
-            % (I0, I1, S0, dut.O, expected))
+            'Result is incorrect for I0(%d) I1(%d) S0(%d): %s(O) != %s (expected)'
+            % (I0, I1, S0, dut.O, expected)
+        )
     else:
         dut._log.info(
             'I0(%d) I1(%d) S0(%d) output(%d) Ok!' % (I0, I1, S0, dut.O)
