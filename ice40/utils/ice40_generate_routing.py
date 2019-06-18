f5758ba2dec5d4d2012efa32bb559b6d4c205994
Alessandro Comodi <acomodi@antmicro.com>
2019-06-18 17:35:24 +0200

    Run make format_python
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- ice40/utils/ice40_generate_routing.py
+++ ice40/utils/ice40_generate_routing.py
@@ -372,7 +372,6 @@ if True:
     ]
 
     #span4_horz_{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47}
-    io_tracks.extend([
     for i in range(0, 48):
         io_tracks.extend([
             Track(name="sp4_v_term[%02i]" % i, type="span4", aliases=("span4_vert[%s]" % i, "sp4_v_t[%s]" % i, "sp4_v_b[%s]" % i)),
