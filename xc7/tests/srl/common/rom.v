b7ade5075a68667a2c0f38d279f265ab464f05dc
Maciej Kurc <mkurc@antmicro.com>
2019-07-02 15:29:59 +0200

    Changed ROM size to 512 in SRL testers
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/tests/srl/common/rom.v
+++ xc7/tests/srl/common/rom.v
@@ -1,12 +1,12 @@
 module ROM
 (
 input  wire       clk,
-input  wire [7:0] adr,
+input  wire [8:0] adr,
 output reg        dat
 );
 
 // ROM content
-parameter [255:0] CONTENT = 256'd0;
+parameter [511:0] CONTENT = 512'd0;
 
 // Data output
 always @(posedge clk)
