0c1cc5aef0c496e007f97ed73a9b87ba6dfbf20b
Karol Gugala <kgugala@antmicro.com>
2019-08-13 16:26:06 +0200

    tests: vlog: update carry tests
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- utils/vlog/tests/carry/cblock/cblock.sim.v
+++ utils/vlog/tests/carry/cblock/cblock.sim.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-(* blackbox *)
+(* whitebox *)
 module CBLOCK (
 	I,
 	O,
@@ -8,12 +8,21 @@ module CBLOCK (
 	COUT
 );
 	input wire [3:0] I;
+	(* carry="C" *)
+	input wire CIN;
 
+	(* DELAY_MATRIX_I="30e-12 30e-12 30e-12 30e-12" *)
+	(* DELAY_CONST_CIN="30e-12" *)
 	output wire O;
 
 	(* carry="C" *)
-	input wire CIN;
-	(* carry="C" *)
+	(* DELAY_MATRIX_I="30e-12 30e-12 30e-12 30e-12" *)
+	(* DELAY_CONST_CIN="30e-12" *)
 	output wire COUT;
 
+	wire [4:0] internal_sum;
+
+	assign internal_sum = I + CIN;
+	assign O = internal_sum[4];
+	assign COUT = internal_sum[3];
 endmodule
