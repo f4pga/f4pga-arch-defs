1a06ca7e04af245bb3514f902df1030ccce59704
Karol Gugala <kgugala@antmicro.com>
2019-08-14 10:09:21 +0200

    test: vlog: fix logicbox indentation
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- utils/vlog/tests/logicbox/logicbox.sim.v
+++ utils/vlog/tests/logicbox/logicbox.sim.v
@@ -2,7 +2,7 @@
 module LOGICBOX (I, O);
 	input wire I;
 	// we need this delay to make VPR see
-       	// the connection between I and O
+	// the connection between I and O
 	(* DELAY_CONST_I="30e-12" *)
 	output wire O;
 
