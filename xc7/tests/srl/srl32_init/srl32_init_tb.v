9c4e2aec47b067bdf73d2665fc8b48993ebea8d7
Maciej Kurc <mkurc@antmicro.com>
2019-08-19 15:25:19 +0200

    Working "testbinch" targets for SRL32 shift and init tests.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/tests/srl/srl32_init/srl32_init_tb.v
+++ xc7/tests/srl/srl32_init/srl32_init_tb.v
@@ -1,7 +1,7 @@
 `timescale 1 ns / 1 ps
 `default_nettype none
 
-module test;
+module tb;
 
 `include "../../../../library/tbassert.v"
 
@@ -11,16 +11,20 @@ reg clk;
 initial clk <= 1'd0;
 always #5 clk <= !clk;
 
+initial begin
+//  $dumpfile("testbench_srl32_init_tb.vcd");
+//  $dumpfile("testbench_synth_srl32_init_tb.vcd");
+  $dumpfile("testbinch_srl32_init_tb.vcd");
+  $dumpvars;
+  #10000 $finish();
+end
+
 // ============================================================================
 // DUT
 wire [15:0] led;
-wire error;
+wire [ 7:0] error;
 
-top #
-(
-.PRESCALER  (4)
-)
-dut
+top dut
 (
 .clk    (clk),
 .rx     (1'b1),
@@ -29,10 +33,10 @@ dut
 .led    (led)
 );
 
-assign error = |led[7:0];
+assign error = led[7:0];
 
-always @(posedge CLK)
-    tbassert(error == 1'd0);
+always @(posedge clk)
+  tbassert((|error == 0), error);
 
 // ============================================================================
 
