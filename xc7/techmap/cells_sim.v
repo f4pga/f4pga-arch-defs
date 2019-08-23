e7a21d899a9a828b9431a8802fccf4254183bf06
Maciej Kurc <mkurc@antmicro.com>
2019-08-23 12:35:19 +0200

    Initial SRL16 support. Works on HW. Had to disable pack patterns because VPR complained about them.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/techmap/cells_sim.v
+++ xc7/techmap/cells_sim.v
@@ -578,3 +578,28 @@ output Q, Q31
 
 endmodule
 
+// SRLC16E_VPR
+module SRLC16E_VPR
+(
+input CLK, CE, D,
+input A0, A1, A2, A3,
+output Q, Q15
+);
+  parameter [15:0] INIT = 16'd0;
+  parameter [0:0] IS_CLK_INVERTED = 1'b0;
+
+  reg [15:0] r = 0;// = INIT;
+
+  assign Q15 = r[15];
+  assign Q = r[{A3,A2,A1,A0}];
+
+  generate
+    if (IS_CLK_INVERTED) begin
+      always @(negedge CLK) if (CE) r <= { r[14:0], D };
+    end else begin
+      always @(posedge CLK) if (CE) r <= { r[14:0], D };
+    end
+  endgenerate
+
+endmodule
+
