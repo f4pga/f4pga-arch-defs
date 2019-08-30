be415ca728c5978dfdf7f8dc48be1036f1e266d4
Karol Gugala <kgugala@antmicro.com>
2019-08-30 11:19:56 +0200

    v2x: explicitly set top module
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- common/cmake/v2x.cmake
+++ common/cmake/v2x.cmake
@@ -195,7 +195,7 @@ function(VPR_TEST_PB_TYPE)
       ${YOSYS} ${YOSYS_TARGET}
       ${DEPENS_EBLIF} ${TECHMAP_DEP}
     COMMAND
-      ${YOSYS} -p "read_verilog ${PB_TYPE_VERILOG}\; techmap -map ${PB_TYPE_TECHMAP}\; proc\; opt\; write_blif ${YOSYS_OUTPUT_BLIF}"
+    ${YOSYS} -p "read_verilog ${PB_TYPE_VERILOG}\; hierarchy -top ${VPR_TEST_PB_TYPE_TOP_MODULE}\; techmap -map ${PB_TYPE_TECHMAP}\; flatten\; proc\; opt\; write_blif ${YOSYS_OUTPUT_BLIF}"
     WORKING_DIRECTORY
       ${CMAKE_CURRENT_BINARY_DIR}
   )
