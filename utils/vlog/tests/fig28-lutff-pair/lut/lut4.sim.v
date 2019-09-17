2f2bbf48671cef7dd6b9f22e3a30447ffee5917f
Karol Gugala <kgugala@antmicro.com>
2019-09-17 12:57:00 +0200

    tests: vlog: change delays in lut4
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- utils/vlog/tests/fig28-lutff-pair/lut/lut4.sim.v
+++ utils/vlog/tests/fig28-lutff-pair/lut/lut4.sim.v
@@ -1,7 +1,7 @@
 (* whitebox *)
 module LUT4 (I, O);
 	input wire [3:0] I;
-	(* DELAY_MATRIX_I="30e-12 30e-12 30e-12 30e-12" *)
+	(* DELAY_MATRIX_I="30e-12 20e-12 11e-12 3e-12" *)
 	output wire O;
 
 	localparam INIT = 16'h0000;
