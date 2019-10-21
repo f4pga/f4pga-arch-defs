3f4a56ef8c07c87d7546b8c155f10d9e86992f15
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-10-21 15:35:58 -0700

    Update print_connection_map_lookahead.py to match latest capnp.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- utils/print_connection_map_lookahead.py
+++ utils/print_connection_map_lookahead.py
@@ -24,34 +24,43 @@ def main():
     with open(args.lookahead_map, 'rb') as f:
         cost_map = connection_map.VprCostMap.read(f)
 
-    for idx, (m, offset) in enumerate(zip(cost_map.costMap, cost_map.offset)):
+    assert cost_map.costMap.dims[0] == cost_map.offset.dims[0]
+    assert cost_map.costMap.dims[1] == cost_map.offset.dims[1]
+    nsegment = cost_map.costMap.dims[0]
+    nconnection_box = cost_map.costMap.dims[1]
 
-        x_off = offset.x
-        y_off = offset.y
+    m_itr = iter(cost_map.costMap.data)
+    offset_itr = iter(cost_map.offset.data)
+    for segment in range(nsegment):
+        for connection_box in range(nconnection_box):
+            m = next(m_itr).value
+            offset = next(offset_itr).value
 
-        assert len(m.dims) == 2
-        x_dim = m.dims[0]
-        y_dim = m.dims[1]
+            x_off = offset.x
+            y_off = offset.y
 
-        print(
-            'Cost map for segment {} (size {}, {}, offset {}, {})'.format(
-                idx, x_dim, y_dim, x_off, y_off
+            assert len(m.dims) == 2
+            x_dim = m.dims[0]
+            y_dim = m.dims[1]
+
+            print(
+                'Cost map for segment {} connection box {} (size {}, {}, offset {}, {})'
+                .format(segment, connection_box, x_dim, y_dim, x_off, y_off)
             )
-        )
 
-        itr = iter(m.data)
+            itr = iter(m.data)
 
-        for x in range(x_dim):
-            for y in range(y_dim):
-                value = next(itr)
-                print(
-                    '({}, {}) = {{ delay = {}, congestion = {} }}'.format(
-                        x + x_off,
-                        y + y_off,
-                        value.value.delay,
-                        value.value.congestion,
+            for x in range(x_dim):
+                for y in range(y_dim):
+                    value = next(itr)
+                    print(
+                        '({}, {}) = {{ delay = {}, congestion = {} }}'.format(
+                            x + x_off,
+                            y + y_off,
+                            value.value.delay,
+                            value.value.congestion,
+                        )
                     )
-                )
 
 
 if __name__ == "__main__":
