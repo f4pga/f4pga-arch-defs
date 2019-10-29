7ad67b490a4c424b69a4c0dedcd23d8d1dac4e46
Alessandro Comodi <acomodi@antmicro.com>
2019-10-29 15:04:51 +0100

    oserdes: updated test and divide clock with PLL
    NOTE: this works only if PLL is supported
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/tests/serdes/oserdes_basys3_x1y0.v
+++ xc7/tests/serdes/oserdes_basys3_x1y0.v
@@ -1,4 +1,4 @@
-`default_nettype none
+`include "oserdes_test.v"
 
 // ============================================================================
 
@@ -7,10 +7,10 @@ module top
 input  wire clk,
 
 input  wire sw,
-output wire [1:0] led,
+output wire [10:0] led,
 
-input  wire [0:0] i_dat,
-output wire [0:0] o_dat
+input  wire [9:0] in,
+output wire [9:0] out
 );
 
 // ============================================================================
@@ -19,45 +19,156 @@ reg [3:0] rst_sr;
 
 initial rst_sr <= 4'hF;
 
-always @(posedge CLK)
-    if (sw)
+always @(posedge clk)
+    if (sw[0])
         rst_sr <= 4'hF;
     else
         rst_sr <= rst_sr >> 1;
 
+wire CLK;
+BUFG bufg_clk(.I(clk), .O(CLK));
 wire RST = rst_sr[0];
-
-wire CLK, CLKDIV;
-BUFG bufg(.I(clk), .O(CLK));
-
-wire clk_div_tmp;
-clk_div clk_div_1(.clk_in(clk), .clk_out(clk_div_tmp));
-clk_div clk_div_2(.clk_in(clk_div_tmp), .clk_out(CLKDIV));
 
 // ============================================================================
 // Clocks for OSERDES
 
+wire CLKDIV_2;
+wire CLKDIV_3;
+wire CLKDIV_4;
+wire CLKDIV_5;
+wire CLKDIV_6;
+wire CLKDIV_7;
+wire CLKDIV_8;
+
+wire pll_1_clk_fb_i;
+wire pll_1_clk_fb_o;
+wire pll_2_clk_fb_i;
+wire pll_2_clk_fb_o;
+
+wire locked_1;
+wire locked_2;
+
+PLLE2_ADV #
+(
+.BANDWIDTH          ("HIGH"),
+.COMPENSATION       ("ZHOLD"),
+
+.CLKOUT0_DIVIDE     (2),
+.CLKOUT0_DUTY_CYCLE (50000),
+
+.CLKOUT1_DIVIDE     (3),
+.CLKOUT1_DUTY_CYCLE (50000),
+
+.CLKOUT2_DIVIDE     (4),
+.CLKOUT2_DUTY_CYCLE (50000),
+
+.CLKOUT3_DIVIDE     (5),
+.CLKOUT3_DUTY_CYCLE (50000),
+
+.CLKOUT4_DIVIDE     (6),
+.CLKOUT4_DUTY_CYCLE (50000),
+
+.CLKOUT5_DIVIDE     (7),
+.CLKOUT5_DUTY_CYCLE (50000),
+
+.STARTUP_WAIT       ("FALSE")
+)
+pll_1
+(
+.CLKIN1     (CLK),
+.CLKINSEL   (1),
+
+.RST        (RST),
+.PWRDWN     (0),
+.LOCKED     (locked_1),
+
+.CLKFBIN    (pll_1_clk_fb_i),
+.CLKFBOUT   (pll_1_clk_fb_o),
+
+.CLKOUT0    (CLKDIV_2),
+.CLKOUT1    (CLKDIV_3),
+.CLKOUT2    (CLKDIV_4),
+.CLKOUT3    (CLKDIV_5),
+.CLKOUT4    (CLKDIV_6),
+.CLKOUT5    (CLKDIV_7)
+);
+
+PLLE2_ADV #
+(
+.BANDWIDTH          ("HIGH"),
+.COMPENSATION       ("ZHOLD"),
+
+.CLKOUT0_DIVIDE     (8),
+.CLKOUT0_DUTY_CYCLE (50000),
+
+.STARTUP_WAIT       ("FALSE")
+)
+pll_2
+(
+.CLKIN1     (CLK),
+.CLKINSEL   (1),
+
+.RST        (RST),
+.PWRDWN     (0),
+.LOCKED     (locked_2),
+
+.CLKFBIN    (pll_2_clk_fb_i),
+.CLKFBOUT   (pll_2_clk_fb_o),
+
+.CLKOUT0    (CLKDIV_8)
+);
 
 // ============================================================================
 // Test uints
-wire [0:0] error;
+wire [9:0] error;
 
+genvar i;
+generate for (i=9; i<10; i=i+1) begin
 
-oserdes_test #
-(
-.DATA_WIDTH   (8),
-.DATA_RATE    ("SDR")
-)
-oserdes_test
-(
-.CLK      (CLK),
-.CLKDIV   (CLKDIV),
-.RST      (RST),
+  localparam DATA_WIDTH = (i == 0) ?   2 :
+                          (i == 1) ?   3 :
+                          (i == 2) ?   4 :
+                          (i == 3) ?   5 :
+                          (i == 4) ?   6 :
+                          (i == 5) ?   7 :
+                          (i == 6) ?   8 :
+                          (i == 7) ?   4 :
+                          (i == 8) ?   6 :
+                        /*(i == 9) ?*/ 8;
 
-.O_DAT    (o_dat),
-.I_DAT   (i_dat),
-.O_ERROR  (error[0])
-);
+  wire CLKDIV = (i == 0) ?   CLKDIV_2 :
+                (i == 1) ?   CLKDIV_3 :
+                (i == 2) ?   CLKDIV_4 :
+                (i == 3) ?   CLKDIV_5 :
+                (i == 4) ?   CLKDIV_6 :
+                (i == 5) ?   CLKDIV_7 :
+                (i == 6) ?   CLKDIV_8 :
+                (i == 7) ?   CLKDIV_4 :
+                (i == 8) ?   CLKDIV_6 :
+              /*(i == 9) ?*/ CLKDIV_8;
+
+  localparam DATA_RATE =  (i <  7) ? "SDR" : "DDR";
+
+  wire out_data;
+  oserdes_test #
+  (
+  .DATA_WIDTH   (DATA_WIDTH),
+  .DATA_RATE    (DATA_RATE)
+  )
+  oserdes_test
+  (
+  .CLK      (CLK),
+  .CLKDIV   (CLKDIV),
+  .RST      (RST),
+
+  .I_DAT   (in[i]),
+  .O_DAT   (out_data),
+  .O_ERROR  (error[i])
+  );
+
+  OBUF obuf (.I(out_data), .O(out[i]));
+
+end endgenerate
 
 // ============================================================================
 // IOs
@@ -66,302 +177,17 @@ reg [24:0] heartbeat_cnt;
 always @(posedge CLK)
     heartbeat_cnt <= heartbeat_cnt + 1;
 
-assign led[0] = i_dat;
-assign led[1] = heartbeat_cnt[23];
+assign led[ 0] = !error[0];
+assign led[ 1] = !error[1];
+assign led[ 2] = !error[2];
+assign led[ 3] = !error[3];
+assign led[ 4] = !error[4];
+assign led[ 5] = !error[5];
+assign led[ 6] = !error[6];
+assign led[ 7] = !error[7];
+assign led[ 8] = !error[8];
+assign led[ 9] = !error[9];
+assign led[10] = heartbeat_cnt[23];
 
 endmodule
 
-module clk_div (
-    input clk_in,
-    output clk_out
-);
-
-    reg clk_out_tmp;
-    initial begin
-        clk_out_tmp <= 0;
-    end
-
-    always @(posedge clk_in) begin
-        clk_out_tmp <= ~clk_out;
-    end
-
-    assign clk_out = clk_out_tmp;
-
-endmodule
-
-// ============================================================================
-
-module lfsr #
-(
-parameter WIDTH = 16, // LFSR width
-parameter [WIDTH-1:0] POLY  = 16'hD008, // Polynomial
-parameter [WIDTH-1:0] SEED  = 1 // Initial value
-)
-(
-input  wire CLK,
-input  wire CE,
-input  wire RST,
-
-output reg [WIDTH-1:0] O
-);
-
-wire feedback = ^(O & POLY);
-
-always @(posedge CLK) begin
-  if(RST) begin
-    O <= SEED;
-  end else if(CE) begin
-    O <= {O[WIDTH-2:0], feedback};
-  end
-end
-
-endmodule
-
-// This module compares two bitstreams and automatically determines their
-// offset. This is done by iteratively changing bit delay for I_DAT_REF
-// every time the number of errors exceeds ERROR_COUNT. The output O_ERROR
-// signal is high for at least ERROR_HOLD cycles.
-
-// ============================================================================
-
-module comparator #
-(
-parameter ERROR_COUNT = 8,
-parameter ERROR_HOLD  = 2500000
-)
-(
-input  wire CLK,
-input  wire RST,
-
-input  wire I_DAT_REF,
-input  wire I_DAT_IOB,
-
-output wire O_ERROR
-);
-
-// ============================================================================
-// Data latch
-reg [2:0] i_dat_ref_sr;
-reg [2:0] i_dat_iob_sr;
-
-always @(posedge CLK)
-    i_dat_ref_sr <= (i_dat_ref_sr << 1) | I_DAT_REF;
-always @(posedge CLK)
-    i_dat_iob_sr <= (i_dat_iob_sr << 1) | I_DAT_IOB;
-
-wire i_dat_ref = i_dat_ref_sr[2];
-wire i_dat_iob = i_dat_iob_sr[2];
-
-// ============================================================================
-// Shift register for reference data, shift strobe generator.
-reg  [31:0] sreg;
-reg  [ 4:0] sreg_sel;
-wire        sreg_dat;
-reg         sreg_sh;
-
-always @(posedge CLK)
-    sreg <= (sreg << 1) | i_dat_ref;
-
-always @(posedge CLK)
-    if (RST)
-        sreg_sel <= 0;
-    else if(sreg_sh)
-        sreg_sel <= sreg_sel + 1;
-
-assign sreg_dat = sreg[sreg_sel];
-
-// ============================================================================
-// Comparator and error counter
-wire        cmp_err;
-reg  [31:0] err_cnt;
-
-assign cmp_err = sreg_dat ^ i_dat_iob;
-
-always @(posedge CLK)
-    if (RST)
-        err_cnt <= 0;
-    else if(sreg_sh)
-        err_cnt <= 0;
-    else if(cmp_err)
-        err_cnt <= err_cnt + 1;
-
-always @(posedge CLK)
-    if (RST)
-        sreg_sh <= 0;
-    else if(~sreg_sh && (err_cnt == ERROR_COUNT))
-        sreg_sh <= 1;
-    else
-        sreg_sh <= 0;
-
-// ============================================================================
-// Output generator
-reg [24:0] o_cnt;
-
-always @(posedge CLK)
-    if (RST)
-        o_cnt <= -1;
-    else if (cmp_err)
-        o_cnt <= ERROR_HOLD - 2;
-    else if (~o_cnt[24])
-        o_cnt <= o_cnt - 1;
-
-assign O_ERROR = !o_cnt[24];
-
-endmodule
-
-module oserdes_test #
-(
-parameter DATA_WIDTH    = 8,
-parameter DATA_RATE     = "SDR",
-parameter ERROR_HOLD    = 2500000
-)
-(
-// "Hi speed" clock and reset
-input  wire CLK,
-input  wire CLKDIV,
-input  wire RST,
-
-// Data out pin
-output wire O_DAT,
-
-// Data in pin
-input wire I_DAT,
-
-// Error indicator
-output wire O_ERROR
-);
-
-// ============================================================================
-// Generate CLK2 and CLKDIV for OSERDES using BUFRs
-
-localparam CLKDIV_DIVIDE = 
-    (DATA_RATE == "SDR" && DATA_WIDTH == 2) ? "2" :
-    (DATA_RATE == "SDR" && DATA_WIDTH == 3) ? "3" : 
-    (DATA_RATE == "SDR" && DATA_WIDTH == 4) ? "4" : 
-    (DATA_RATE == "SDR" && DATA_WIDTH == 5) ? "5" : 
-    (DATA_RATE == "SDR" && DATA_WIDTH == 6) ? "6" : 
-    (DATA_RATE == "SDR" && DATA_WIDTH == 7) ? "7" : 
-    (DATA_RATE == "SDR" && DATA_WIDTH == 8) ? "8" : 
-
-    (DATA_RATE == "DDR" && DATA_WIDTH == 4) ? "4" : 
-    (DATA_RATE == "DDR" && DATA_WIDTH == 6) ? "6" : 
-    (DATA_RATE == "DDR" && DATA_WIDTH == 8) ? "8" : "BYPASS";
-
-// ============================================================================
-// Data source
-reg         lfsr_stb;
-wire [7:0]  lfsr_dat;
-
-wire clkdiv_r;
-wire ce;
-
-always @(posedge CLK)
-    clkdiv_r <= CLKDIV;
-
-assign ce = clkdiv_r && !CLKDIV;
-
-lfsr lfsr
-(
-.CLK    (CLK),
-.RST    (RST),
-.CE     (ce),
-
-.O      (lfsr_dat)
-);
-
-always @(posedge CLK)
-    if (RST)
-        lfsr_stb <= 1'b0;
-    else
-        lfsr_stb <= ce;
-
-// Synchronize generated data wordst to the CLKDIV
-reg  [7:0] ser_dat;
-
-always @(posedge CLKDIV)
-    ser_dat <= lfsr_dat;
-
-
-// ============================================================================
-// OSERDES
-
-// OSERDES reset generator (required for it to work properly!)
-reg [3:0]  ser_rst_sr;
-initial    ser_rst_sr <= 4'hF;
-
-always @(posedge CLKDIV)
-    if (RST) ser_rst_sr <= 4'hF;
-    else     ser_rst_sr <= ser_rst_sr >> 1;
-
-wire ser_rst = ser_rst_sr[0];
-
-// OSERDES
-wire ser_oq;
-wire ser_tq;
-
-OSERDESE2 #
-(
-.DATA_RATE_OQ   (DATA_RATE),
-.DATA_WIDTH     (DATA_WIDTH),
-.DATA_RATE_TQ   ((DATA_RATE == "DDR" && DATA_WIDTH == 4) ? "DDR" : "SDR"),
-.TRISTATE_WIDTH ((DATA_RATE == "DDR" && DATA_WIDTH == 4) ? 4 : 1)
-)
-oserdes
-(
-.CLK    (CLK),
-.CLKDIV (CLKDIV),
-.RST    (ser_rst),
-
-.OCE    (1'b1),
-.D1     (ser_dat[0]),
-.D2     (ser_dat[1]),
-.D3     (ser_dat[2]),
-.D4     (ser_dat[3]),
-.D5     (ser_dat[4]),
-.D6     (ser_dat[5]),
-.D7     (ser_dat[6]),
-.D8     (ser_dat[7]),
-.OQ     (O_DAT),
-
-.TCE    (1'b0),
-.T1     (1'b0), // All 0 to keep OBUFT always on.
-.T2     (1'b0),
-.T3     (1'b0),
-.T4     (1'b0)
-);
-
-// ============================================================================
-// Reference data serializer
-reg  [7:0]  ref_sr;
-wire        ref_o;
-
-always @(posedge CLK)
-    if (RST)
-        ref_sr <= 0;
-    else if (ce)
-        ref_sr <= lfsr_dat;
-    else
-        ref_sr <= ref_sr >> 1;
-
-assign ref_o = ref_sr[0];
-
-// ============================================================================
-// Data comparator
-
-comparator #
-(
-.ERROR_COUNT    (16),
-.ERROR_HOLD     (ERROR_HOLD)
-)
-comparator
-(
-.CLK    (CLK),
-.RST    (RST),
-
-.I_DAT_REF  (ref_o),
-.I_DAT_IOB  (I_DAT),
-
-.O_ERROR    (O_ERROR)
-);
-
-endmodule
