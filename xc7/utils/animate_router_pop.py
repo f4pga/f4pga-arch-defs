3b8164f23796302ae1905a747ef88f1257a1550f
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-11-07 12:30:35 -0800

    Add full route to display.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- xc7/utils/animate_router_pop.py
+++ xc7/utils/animate_router_pop.py
@@ -10,7 +10,9 @@ def main():
 
     # rt_node: CLBLM_R_X27Y37/CLBLM_M_AMUX (1923687) (OPIN)
     # rt_node: CLBLL_L_X38Y24/CLBLL_LL_C1 (3202101) (IPIN)
-    PIN_RE = re.compile(r'rt_node: ([A-Za-z0-9_/]+) \([0-9]+\) \(([IO])PIN\)')
+    PIN_RE = re.compile(
+        r'rt_node: ([A-Za-z0-9_/]+) \([0-9]+\) \(([^\)]+)\)(\*?)'
+    )
 
     print(
         """
@@ -26,6 +28,8 @@ proc animate {idx node_length} {
 
     wires = []
 
+    final_route = []
+
     for l in sys.stdin:
         m = POP_RE.search(l)
 
@@ -34,16 +38,24 @@ proc animate {idx node_length} {
 
         m = PIN_RE.search(l)
         if m:
-            if m.group(2) == 'I':
-                ipin = m.group(1)
-            elif m.group(2) == 'O':
-                opin = m.group(1)
-            else:
-                assert False, l
+            if m.group(3) == '*':
+                continue
+
+            final_route.append((m.group(1), m.group(2)))
+
+    assert final_route[0][1] == 'OPIN', final_route[0]
+
+    for r in final_route[1:-1]:
+        assert r[1] in ['CHANX', 'CHANY'], r
+    assert final_route[-1][1] == 'IPIN', final_route[-1]
 
     print('unhighlight_objects')
-    print('highlight_objects [get_nodes -of [get_wires {}]]'.format(ipin))
-    print('highlight_objects [get_nodes -of [get_wires {}]]'.format(opin))
+
+    for idx, (wire, _) in enumerate(final_route):
+        print(
+            'highlight_objects -color_index {} [get_nodes -of [get_wires {}]]'.
+            format((idx % 20) + 1, wire)
+        )
 
     for wire in wires:
         print('select_objects [get_nodes -of [get_wires {}]]'.format(wire))
