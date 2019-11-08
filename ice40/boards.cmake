7a02297cbaafde2ee46db601e58341c8e462cfd5
Keith Rothman <537074+litghost@users.noreply.github.com>
2019-11-08 15:39:19 -0800

    Refactor CMake system to enable ROI-less xc7 graphs that share arches.
    Previously some xc7 properties were attached to devices, but belonged to
    the board.  This is now fixed.
    
    Moving the PINMAP to the board is less obvious, but I think late binding
    the PINMAP allows for less confusion, and more flexiblity.  This does
    result in some data duplication, but not a lot.
    
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- ice40/boards.cmake
+++ ice40/boards.cmake
@@ -1,10 +1,39 @@
 get_target_property_required(ICEPROG env ICEPROG)
 get_target_property(ICEPROG_TARGET env ICEPROG_TARGET)
 
+function(define_ice40_board)
+  set(options)
+  set(oneValueArgs BOARD DEVICE PACKAGE PROG_TOOL PROG_CMD)
+  set(multiValueArgs)
+  cmake_parse_arguments(
+    DEFINE_ICE40_BOARD
+     "${options}"
+     "${oneValueArgs}"
+     "${multiValueArgs}"
+     ${ARGN}
+    )
+
+  define_board(
+    BOARD ${DEFINE_ICE40_BOARD_BOARD}
+    DEVICE ${DEFINE_ICE40_BOARD_DEVICE}
+    PACKAGE ${DEFINE_ICE40_BOARD_PACKAGE}
+    PROG_TOOL ${DEFINE_ICE40_BOARD_PROG_TOOL}
+    PROG_CMD ${DEFINE_ICE40_BOARD_PROG_PROG_CMD}
+    )
+
+  get_target_property_required(PACKAGE_PINMAP ${DEFINE_ICE40_BOARD_DEVICE} ${DEFINE_ICE40_BOARD_PACKAGE}_PINMAP)
+
+  set_target_properties(
+    ${DEFINE_ICE40_BOARD_BOARD}
+    PROPERTIES
+      PINMAP ${PACKAGE_PINMAP}
+      )
+endfunction()
+
 # Lattice iCEstick
 # http://www.latticesemi.com/icestick
 # ---------------------------------------------
-define_board(
+define_ice40_board(
   BOARD icestick
   DEVICE hx1k
   PACKAGE tq144
@@ -15,7 +44,7 @@ define_board(
 # Lattice iCEblink40-LP1K Evaluation Kit
 # **HX** version is different!
 # ---------------------------------------------
-define_board(
+define_ice40_board(
   BOARD iceblink40-lp1k
   DEVICE lp1k
   PACKAGE qn84
@@ -23,10 +52,12 @@ define_board(
   PROG_CMD "${ICEPROG} -ew \${OUT_BIN}"
 )
 
+if (NOT DEFINED ENV{CI} OR NOT $ENV{CI})
+
 # iCE40-HX8K Breakout Board Evaluation Kit
 # iCE40-HX8K-CT256
 # ---------------------------------------------
-define_board(
+define_ice40_board(
   BOARD hx8k-b-evn
   DEVICE hx8k
   PACKAGE ct256
@@ -37,7 +68,7 @@ define_board(
 # DPControl icevision board
 # iCE40UP5K-SG48
 # ---------------------------------------------
-define_board(
+define_ice40_board(
   BOARD icevision
   DEVICE up5k
   PACKAGE sg48
@@ -54,7 +85,7 @@ get_target_property(TINYFPGAB_TARGET env TINYFPGAB_TARGET)
 # TinyFPGA B2
 # iCE40-LP8K-CM81
 # ---------------------------------------------
-define_board(
+define_ice40_board(
   BOARD tinyfpga-b2
   DEVICE lp8k
   PACKAGE cm81
@@ -72,10 +103,12 @@ get_target_property(TINYPROG_TARGET env TINYPROG_TARGET)
 # TinyFPGA BX
 # iCE40-LP8K-CM81
 # ---------------------------------------------
-define_board(
+define_ice40_board(
   BOARD tinyfpga-bx
   DEVICE lp8k
   PACKAGE cm81
   PROG_TOOL ${TINYPROG_TARGET}
   PROG_CMD "${TINYPROG} -p \${OUT_BIN}"
 )
+
+endif (NOT DEFINED ENV{CI} OR NOT $ENV{CI})
