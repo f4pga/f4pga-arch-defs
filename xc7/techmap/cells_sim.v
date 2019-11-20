dfdde6837af5a0c546abd4d33886a394bd023feb
Maciej Kurc <mkurc@antmicro.com>
2019-11-20 17:27:44 +0100

    Splitting OBUFTDS into OBUFTDS_M_VPR and OBUFTDS_S_VPR.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/techmap/cells_sim.v
+++ xc7/techmap/cells_sim.v
@@ -635,7 +635,7 @@ module IOBUF_VPR (
 endmodule
 
 
-module OBUFTDS_VPR (
+module OBUFTDS_M_VPR (
     input  I,
     input  T,
     output O,
@@ -644,6 +644,15 @@ module OBUFTDS_VPR (
 
   assign O  = (T == 1'b0) ?  I : 1'bz;
   assign OB = (T == 1'b0) ? !I : 1'bz;
+
+endmodule
+
+module OBUFTDS_S_VPR (
+    input  IB,
+    output OB
+);
+
+  assign OB = IB;
 
 endmodule
 
