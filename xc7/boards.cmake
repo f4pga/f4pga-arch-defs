194866340c67b3400218932311bee0819543b132
Tomasz Michalak <tmichalak@antmicro.com>
2019-11-21 11:43:33 +0100

    xc7: tests: Add arty-full board
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xc7/boards.cmake
+++ xc7/boards.cmake
@@ -46,6 +46,15 @@ add_xc7_board(
   PART xc7a35tcsg324-1
 )
 
+add_xc7_board(
+  BOARD arty-full
+  DEVICE xc7a50t
+  PACKAGE test
+  PROG_TOOL ${OPENOCD_TARGET}
+  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/board-digilent-basys3.cfg -c \\\"init $<SEMICOLON> pld load 0 \${OUT_BIN} $<SEMICOLON> exit\\\""
+  PART xc7a35tcsg324-1
+)
+
 # TODO: https://github.com/SymbiFlow/symbiflow-arch-defs/issues/344
 add_xc7_board(
   BOARD zybo
