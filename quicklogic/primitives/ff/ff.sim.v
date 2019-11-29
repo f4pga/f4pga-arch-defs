fc12db4c5aa3b682eed0fbcb8123da38f014b167
Mariusz Glebocki <mglebocki@antmicro.com>
2019-11-29 15:58:30 +0100

    Add set, reset, enable ports to FF
    Signed-off-by: Mariusz Glebocki <mglebocki@antmicro.com>

--- quicklogic/primitives/ff/ff.sim.v
+++ quicklogic/primitives/ff/ff.sim.v
@@ -1,16 +1,20 @@
 (* whitebox *) (* CLASS="flipflop" *)
-module FF(clk, D, Q);
-
+module FF(clk, D, S, R, E, Q);
 	(* PORT_CLASS = "clock" *)
 	input clk;
-
-	(* PORT_CLASS = "D" *) (* SETUP = "clk 10e-12" *)
+	(* PORT_CLASS = "D" *)
 	input D;
-
-	(* PORT_CLASS = "Q" *) (* CLK_TO_Q = "clk 10e-12" *)
+	input S;
+	input R;
+	input E;
+	(* PORT_CLASS = "Q" *)
 	output reg Q;
-
-	always @(posedge clk)
-		Q <= D;
-
+	always @(posedge clk or posedge S or posedge R) begin
+		if (S)
+			Q <= 1'b1;
+		else if (R)
+			Q <= 1'b0;
+		else if (E)
+			Q <= D;
+	end
 endmodule
