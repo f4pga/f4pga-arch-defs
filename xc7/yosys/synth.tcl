2422822a45245505576b11f312ab7bece0b4391d
Tomasz Michalak <tmichalak@antmicro.com>
2019-12-02 22:33:17 +0100

    fasm-extra-features: Modify synth script
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xc7/yosys/synth.tcl
+++ xc7/yosys/synth.tcl
@@ -1,8 +1,16 @@
 yosys -import
-
+plugin -i xdc
+plugin -i fasm
+#Import the commands from the plugins to the tcl interpreter
+yosys -import
 # -flatten is used to ensure that the output eblif has only one module.
 # Some of symbiflow expects eblifs with only one module.
 synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp
+
+if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
+  read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
+  write_fasm -part_json $::env(PART_JSON)  $::env(OUT_FASM_EXTRA)
+}
 
 write_verilog $::env(OUT_SYNTH_V).premap.v
 
