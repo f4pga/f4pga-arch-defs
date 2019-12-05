4ca534ffdd6f10874670599b03db654c0280dd32
Maciej Kurc <mkurc@antmicro.com>
2019-12-05 10:02:39 +0100

    Added back writing premap output verilog.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/yosys/synth.tcl
+++ xc7/yosys/synth.tcl
@@ -23,7 +23,7 @@ techmap -map $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/io_map.v
 
 # -flatten is used to ensure that the output eblif has only one module.
 # Some of symbiflow expects eblifs with only one module.
-synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp
+synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp -iopad
 
 if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
   read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
