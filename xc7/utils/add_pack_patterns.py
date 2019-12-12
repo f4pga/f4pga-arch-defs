5ab7869fa8e480e33762f019f2432391bc98fec5
Tomasz Michalak <tmichalak@antmicro.com>
2019-12-12 15:50:11 +0100

    xc7: utils: Add OSERDES to OBUFTDS pack pattern
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xc7/utils/add_pack_patterns.py
+++ xc7/utils/add_pack_patterns.py
@@ -134,6 +134,16 @@ def main():
         ]):
             add_pack_pattern(direct, 'OSERDES_IOBUF')
 
+        # Adding OSERDES to differential OBUF pack patterns
+        if "OQ_to_IOB33M" in dir_name or check_direct(direct, [
+            ('OBUFTDS_M', 'I'),
+            ('OBUFTDS_M', 'O'),
+            ('OSERDES', 'OQ'),
+            ('IOB33M', 'I'),
+            ('IOB33M', 'O'),
+        ]):
+            add_pack_pattern(direct, 'OSERDES_OBUFTDS_M')
+
         # Adding ISERDES pack patterns
         if IOPAD_ILOGIC_REGEX.match(dir_name) or check_direct(direct, [
             ('NO_IBUF', 'I'),
