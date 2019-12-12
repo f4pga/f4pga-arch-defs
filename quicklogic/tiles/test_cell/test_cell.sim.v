e1c8325b96617d217fe175284644e8cb94eb45c3
Mariusz Glebocki <mglebocki@antmicro.com>
2019-12-12 23:19:49 +0100

    Quicklogic: test_cell: add mux
    Signed-off-by: Mariusz Glebocki <mglebocki@antmicro.com>

--- quicklogic/tiles/test_cell/test_cell.sim.v
+++ quicklogic/tiles/test_cell/test_cell.sim.v
@@ -1,12 +1,17 @@
 `include "../../primitives/ff/ff.sim.v"
+`include "../../primitives/mux/mux.sim.v"
 
-module TEST_CELL (clk, D, S, R, E, Q);
+module TEST_CELL (clk, D1, D2, DS, S, R, E, Q);
 	input wire clk;
-	input wire D;
+	input wire D1;
+	input wire D2;
+	input wire DS;
 	input wire S;
 	input wire R;
 	input wire E;
 	output wire Q;
 
-	FF ff0(clk, D, S, R, E, Q);
+	wire d;
+	MUX mux0(D1, D2, DS, d);
+	FF ff0(clk, d, S, R, E, Q);
 endmodule
