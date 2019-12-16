37f615254f104c680a0cd9f6e40249b71afbabc1
Alessandro Comodi <acomodi@antmicro.com>
2019-12-16 16:54:19 +0100

    xc7: tests: fixed tests indentation
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/tests/counter/counter_arty.v
+++ xc7/tests/counter/counter_arty.v
@@ -1,21 +1,21 @@
 module top (
-	input  wire clk,
+    input  wire clk,
 
     input  wire rx,
-	output wire tx,
+    output wire tx,
 
-	input  wire [7:0] sw,
-	output wire [7:0] led
+    input  wire [7:0] sw,
+    output wire [7:0] led
 );
 
-	localparam BITS = 1;
-	localparam LOG2DELAY = 25;
+    localparam BITS = 1;
+    localparam LOG2DELAY = 25;
 
-	reg [BITS+LOG2DELAY-1:0] counter = 0;
+    reg [BITS+LOG2DELAY-1:0] counter = 0;
 
-	always @(posedge clk) begin
-		counter <= counter + 1;
-	end
+    always @(posedge clk) begin
+    	counter <= counter + 1;
+    end
 
-	assign led[7] = counter >> LOG2DELAY;
+    assign led[7] = counter >> LOG2DELAY;
 endmodule
