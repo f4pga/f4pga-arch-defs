cfec8a50badef3a610f0905bb7b70ef9ade38158
Alessandro Comodi <acomodi@antmicro.com>
2019-12-16 18:10:46 +0100

    xc7: tests: fix routing issues when using common pcf
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/tests/counter/counter_arty.v
+++ xc7/tests/counter/counter_arty.v
@@ -1,15 +1,12 @@
 module top (
     input  wire clk,
 
-    input  wire rx,
-    output wire tx,
-
     input  wire [7:0] sw,
     output wire [7:0] led
 );
 
-    localparam BITS = 1;
-    localparam LOG2DELAY = 25;
+    localparam BITS = 8;
+    localparam LOG2DELAY = 28;
 
     reg [BITS+LOG2DELAY-1:0] counter = 0;
 
@@ -17,5 +14,5 @@ module top (
     	counter <= counter + 1;
     end
 
-    assign led[7] = counter >> LOG2DELAY;
+    assign led = counter >> LOG2DELAY;
 endmodule
