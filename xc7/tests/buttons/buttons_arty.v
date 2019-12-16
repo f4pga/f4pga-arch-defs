025396308bf99d0c1239d46695ca6859d706799b
Alessandro Comodi <acomodi@antmicro.com>
2019-12-16 16:54:19 +0100

    xc7: tests: fixed tests indentation
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc7/tests/buttons/buttons_arty.v
+++ xc7/tests/buttons/buttons_arty.v
@@ -1,11 +1,11 @@
 module top (
-	input  wire clk,
+    input  wire clk,
 
     input  wire rx,
-	output wire tx,
+    output wire tx,
 
-	input  wire [7:0] sw,
-	output wire [7:0] led
+    input  wire [7:0] sw,
+    output wire [7:0] led
 );
-  assign led = sw;
+    assign led = sw;
 endmodule
