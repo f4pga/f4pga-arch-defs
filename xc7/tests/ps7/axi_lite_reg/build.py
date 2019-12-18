407b8b7fd3b7af9e74155606fb0a89a66f9f68ec
Maciej Kurc <mkurc@antmicro.com>
2019-12-18 13:04:26 +0100

    Code formatting.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc7/tests/ps7/axi_lite_reg/build.py
+++ xc7/tests/ps7/axi_lite_reg/build.py
@@ -4,23 +4,35 @@ import os
 work_root = 'build'
 
 files = [
-  {'name' : os.path.realpath('AxiPeriph.v'), 'file_type' : 'verilogSource'},
-  {'name' : os.path.realpath('axi_reg.v'), 'file_type' : 'verilogSource'},
-  {'name' : os.path.realpath('zybo.xdc'), 'file_type' : 'xdc'},
+    {
+        'name': os.path.realpath('AxiPeriph.v'),
+        'file_type': 'verilogSource'
+    },
+    {
+        'name': os.path.realpath('axi_reg.v'),
+        'file_type': 'verilogSource'
+    },
+    {
+        'name': os.path.realpath('zybo.xdc'),
+        'file_type': 'xdc'
+    },
 ]
 
 tool = 'vivado'
 
 edam = {
-  'files' : files,
-  'name'  : 'axi_regs',
-  'toplevel': 'top',
-  'tool_options' : {'vivado' : {'part' : 'xc7z010clg400-1'}}
-  }
+    'files': files,
+    'name': 'axi_regs',
+    'toplevel': 'top',
+    'tool_options': {
+        'vivado': {
+            'part': 'xc7z010clg400-1'
+        }
+    }
+}
 
 backend = edalize.get_edatool(tool)(edam=edam, work_root=work_root)
 
-os.makedirs(work_root, exist_ok = True)
+os.makedirs(work_root, exist_ok=True)
 backend.configure("")
 backend.build()
-
