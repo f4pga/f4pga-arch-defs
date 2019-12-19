3edd43eaed8b3dc909260df580cf389f9397148c
Tim 'mithro' Ansell <me@mith.ro>
2019-12-19 17:40:20 +0100

    kokoro: Fixing the number of runners calaculation.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- .github/kokoro/steps/hostinfo.sh
+++ .github/kokoro/steps/hostinfo.sh
@@ -21,9 +21,10 @@ echo "----------------------------------------"
 cat /proc/meminfo
 echo "----------------------------------------"
 export MEM_GB=$(($(awk '/MemTotal/ {print $2}' /proc/meminfo)/(1024*1024)))
-echo "Memory (GB): $CORES"
-export MEM_CORES=$(($MEM_GB/4))
-echo "Cores (4 GB per): $MEM_CORES"
+echo "Total Memory (GB): $MEM_GB"
+export MEM_PER_RUN=16
+export MEM_CORES=$(($MEM_GB/$MEM_PER_RUN))
+echo "Simultaneous runs ($MEM_PER_RUN GB each): $MEM_CORES"
 export MAX_CORES_NO_MIN=$(($MEM_CORES>$CORES?$CORES:$MEM_CORES))
 export MAX_CORES=$(($MAX_CORES_NO_MIN<1?1:$MAX_CORES_NO_MIN))
 echo "Max cores: $MAX_CORES"
