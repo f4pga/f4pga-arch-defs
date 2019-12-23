d4aeb08b12789f7078861bdb13ca8e33bd8fa76d
Mariusz Glebocki <mglebocki@antmicro.com>
2019-12-23 01:49:30 +0100

    Test cell WIP: routing mux
    Signed-off-by: Mariusz Glebocki <mglebocki@antmicro.com>

--- quicklogic/tiles/test_cell/test_cell.sim.v
+++ quicklogic/tiles/test_cell/test_cell.sim.v
@@ -1,18 +1,23 @@
-`include "../../primitives/ff/ff.sim.v"
+// `include "../../primitives/ff/ff.sim.v"
 `include "../../primitives/mux/mux.sim.v"
+`include "logicbox.sim.v"
 
-module TEST_CELL (clk, D1, D2, DS, S, R, E, Q);
-	input wire clk;
+module TEST_CELL (D1, D2, Q);
+	// input wire clk;
 	input wire D1;
 	input wire D2;
-	input wire DS;
-	input wire S;
-	input wire R;
-	input wire E;
 	output wire Q;
 
-	(* pack = "DFF" *)
-	wire d;
-	MUX mux0(D1, D2, DS, d);
-	FF ff0(clk, d, S, R, E, Q);
+	wire i0;
+	wire i1;
+	LOGICBOX lbox0(D1, i0);
+	LOGICBOX lbox1(D2, i1);
+
+	// (* pack = "DFF" *)
+	// wire d;
+
+	parameter DS = "I0";
+
+	MUX #(.MODE(DS)) mux0(i0, i1, Q);
+	// FF ff0(clk, d, S, R, E, Q);
 endmodule
