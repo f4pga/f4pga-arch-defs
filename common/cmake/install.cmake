5a908740527df3c65805894c848e58f63c2a7789
Karol Gugala <kgugala@antmicro.com>
2019-12-30 14:15:33 +0100

    install: use install target for device files
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- common/cmake/install.cmake
+++ common/cmake/install.cmake
@@ -3,6 +3,7 @@ function(INSTALL_DEVICE_FILES)
   # INSTALL_DEVICE_FILES(
   #   DEVICE <device>
   #   PACKAGE <package>
+  #   DEVICE_TYPE <device_type>
   #   )
   # ~~~
   #
@@ -20,16 +21,12 @@ function(INSTALL_DEVICE_FILES)
   set(DEVICE ${INSTALL_DEVICE_FILES_DEVICE})
   set(DEVICE_TYPE ${INSTALL_DEVICE_FILES_DEVICE_TYPE})
   set(PACKAGE ${INSTALL_DEVICE_FILES_PACKAGE})
-  set(INSTALL_DEST "${CMAKE_INSTALL_PREFIX}/share/arch/${DEVICE}_${PACKAGE}")
 
   get_target_property(USE_ROI ${DEVICE_TYPE} USE_ROI)
   if(USE_ROI OR USE_ROI STREQUAL "USE_ROI-NOTFOUND")
     message(STATUS "Skipping device files installation for ${DEVICE}-${PACKAGE} type: ${DEVICE_TYPE}")
     return()
   endif()
-
-  add_custom_target(${DEVICE}_${PACKAGE}_ARCH_INSTALL_DIR
-                    COMMAND ${CMAKE_COMMAND} -E make_directory ${INSTALL_DEST})
 
   set(INSTALL_FILES)
 
@@ -52,7 +49,6 @@ function(INSTALL_DEVICE_FILES)
 
   get_target_property(CHANNELS_DB_FILE ${DEVICE_TYPE} CHANNELS_DB)
   if(NOT ${CHANNELS_DB_FILE} STREQUAL "CHANNELS_DB_FILE-NOTFOUND")
-    message(WARNING "channels DB: ${CHANNELS_DB_FILE}")
     list(APPEND INSTALL_FILES ${CHANNELS_DB_FILE})
   endif()
 
@@ -60,19 +56,17 @@ function(INSTALL_DEVICE_FILES)
   foreach(FILE ${INSTALL_FILES})
     get_file_location(SRC_FILE ${FILE})
     get_filename_component(FILE_NAME ${SRC_FILE} NAME)
-    set(DST_FILE "${INSTALL_DEST}/${FILE_NAME}")
-    add_custom_command(
-      OUTPUT ${DST_FILE}
-      COMMAND ${CMAKE_COMMAND} -E copy
-      ${SRC_FILE}
-      ${DST_FILE}
-      DEPENDS ${SRC_FILE} ${DEVICE}_${PACKAGE}_ARCH_INSTALL_DIR
-    )
+    append_file_dependency(DEPS ${FILE})
+    # Create a custom target and add it to ALL target.
+    # We do this because install targets depend only on
+    # all.
     add_custom_target(
       "INSTALL_${DEVICE}_${PACKAGE}_${FILE_NAME}"
-      DEPENDS ${DST_FILE})
-
-    add_dependencies(install_architectures "INSTALL_${DEVICE}_${PACKAGE}_${FILE_NAME}")
+      ALL
+      DEPENDS ${DEPS})
+    install(FILES ${SRC_FILE}
+      DESTINATION "share/arch/${DEVICE}_${PACKAGE}")
   endforeach()
 
 endfunction()
+
