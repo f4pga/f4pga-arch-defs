84e6deacdecd37ddd5acbdbae7b034015849540a
Karol Gugala <kgugala@antmicro.com>
2020-01-08 14:25:28 +0100

    kokoro: run make install test
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- .github/kokoro/xc7.sh
+++ .github/kokoro/xc7.sh
@@ -3,7 +3,7 @@
 SCRIPT_SRC="$(realpath ${BASH_SOURCE[0]})"
 SCRIPT_DIR="$(dirname "${SCRIPT_SRC}")"
 
-export CMAKE_FLAGS=-GNinja
+export CMAKE_FLAGS="-GNinja -DCMAKE_INSTALL_PREFIX=`pwd`/install"
 export BUILD_TOOL=ninja
 source ${SCRIPT_DIR}/common.sh
 
@@ -12,11 +12,22 @@ echo "========================================"
 echo "Running xc7 tests (make all_xc7)"
 echo "----------------------------------------"
 (
-	cd build
+	pushd build
 	export VPR_NUM_WORKERS=${CORES}
 	ninja -j${MAX_CORES} all_xc7
 	ninja print_qor > xc7_qor.csv
+	popd
 )
 echo "----------------------------------------"
 
+echo
+echo "========================================"
+echo "Running install tests (make install)"
+echo "----------------------------------------"
+(
+	pushd build
+	ninja -j${MAX_CORES} install
+	popd
+)
+echo "----------------------------------------"
 source ${SCRIPT_DIR}/package_results.sh
