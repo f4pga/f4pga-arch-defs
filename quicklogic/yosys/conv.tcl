416a61492dc2208e86120948582d639eed8b178d
Maciej Kurc <mkurc@antmicro.com>
2020-01-15 12:21:35 +0100

    Disables emission of VCC and GND cells to the eblif.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/yosys/conv.tcl
+++ quicklogic/yosys/conv.tcl
@@ -4,8 +4,4 @@ yosys -import
 opt_clean
 
 # Write EBLIF
-write_blif -attr -cname -param \
- -true VCC VCC \
- -false GND GND \
- -undef VCC VCC \
- $::env(OUT_EBLIF)
+write_blif -attr -cname -param $::env(OUT_EBLIF)
