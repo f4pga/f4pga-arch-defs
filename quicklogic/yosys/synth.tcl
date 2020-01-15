f6a0f6d9776dd8bd46f045785c9062bf42ca8725
Maciej Kurc <mkurc@antmicro.com>
2020-01-15 13:40:12 +0100

    Added loading the geneated cells_sim.v file to Yosys. NO DEPENDENCY YET!
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/yosys/synth.tcl
+++ quicklogic/yosys/synth.tcl
@@ -1,8 +1,15 @@
 yosys -import
 
+# Read VPR cells library
+read_verilog -lib $::env(symbiflow-arch-defs_BINARY_DIR)/quicklogic/techmap/cells_sim.v
+
+# Synthesize (TODO: Use synth_quicklogic here!)
 synth -top top -flatten
 abc -lut 4
 opt_clean
+
+# Map to the VPR cell library
+techmap -map  $::env(symbiflow-arch-defs_SOURCE_DIR)/quicklogic/techmap/cells_map.v
 
 write_json $::env(OUT_JSON)
 write_verilog $::env(OUT_SYNTH_V)
