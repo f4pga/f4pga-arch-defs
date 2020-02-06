96849ec5afaa5079313f2ac7064ce2ee74a6d1a7
Maciej Kurc <mkurc@antmicro.com>
2020-02-06 11:36:24 +0100

    Enabled 'synth_quicklogic', Added reduction of logic_0 and logic_1 cells, added temporary reduction of inpad/ckpad/outpad cells. Added a test for consts.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/techmap/cells_map.v
+++ quicklogic/techmap/cells_map.v
@@ -1,3 +1,38 @@
+// ============================================================================
+// Constant sources
+
+// Reduce logic_0 and logic_1 cells to 1'b0 and 1'b1. Const sources suitable
+// for VPR will be added by Yosys during EBLIF write.
+module logic_0(output a);
+    assign a = 0;
+endmodule
+
+module logic_1(output a);
+    assign a = 1;
+endmodule
+
+
+// ============================================================================
+// IO and clock buffers
+
+// TODO: Those are not supported in the SymbiFlow yet. Map them to passthrough
+// wires.
+module inpad(output Q, input P);
+  assign Q = P;
+endmodule
+
+module outpad(output P, input A);
+  assign P = A;
+endmodule
+
+module ckpad(output Q, input P);
+  assign Q = P;
+endmodule
+
+
+// ============================================================================
+// LUTs
+
 module LUT1 (
   output O,
   input  I0
--- quicklogic/yosys/synth.tcl
+++ quicklogic/yosys/synth.tcl
@@ -3,11 +3,8 @@ yosys -import
 # Read VPR cells library
 read_verilog -lib $::env(symbiflow-arch-defs_BINARY_DIR)/quicklogic/techmap/cells_sim.v
 
-# Synthesize (TODO: Use synth_quicklogic here!)
-read_verilog -lib +/quicklogic/cells_sim.v
-synth -top top -flatten
-abc -lut 4
-opt_clean
+# Synthesize
+synth_quicklogic -flatten
 
 # Map to the VPR cell library
 techmap -map  $::env(symbiflow-arch-defs_SOURCE_DIR)/quicklogic/techmap/cells_map.v
