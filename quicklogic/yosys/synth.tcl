578f239c09dc02b86991ceff27865f3052830683
Maciej Kurc <mkurc@antmicro.com>
2020-02-17 16:00:45 +0100

    Added writing pre-mapped verilog.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/yosys/synth.tcl
+++ quicklogic/yosys/synth.tcl
@@ -6,6 +6,9 @@ read_verilog -lib $::env(symbiflow-arch-defs_BINARY_DIR)/quicklogic/techmap/cell
 # Synthesize
 synth_quicklogic -flatten
 
+# Write a pre-mapped design
+write_verilog $::env(OUT_SYNTH_V).premap.v
+
 # Map to the VPR cell library
 techmap -map  $::env(symbiflow-arch-defs_SOURCE_DIR)/quicklogic/techmap/cells_map.v
 
