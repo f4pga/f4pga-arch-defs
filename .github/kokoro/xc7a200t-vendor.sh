3cd4456dc8b4c3ef73856baa66b018f9c60b3606
Keith Rothman <537074+litghost@users.noreply.github.com>
2020-02-28 13:00:54 -0800

    Enable XC7A200T on CI.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- .github/kokoro/xc7a200t-vendor.sh
+++ .github/kokoro/xc7a200t-vendor.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
-true
+
+SCRIPT_SRC="$(realpath ${BASH_SOURCE[0]})"
+SCRIPT_DIR="$(dirname "${SCRIPT_SRC}")"
+
+export CMAKE_FLAGS=-GNinja
+export BUILD_TOOL=ninja
+export XRAY_VIVADO_SETTINGS=/opt/Xilinx/Vivado/2017.2/settings64.sh
+source ${SCRIPT_DIR}/common.sh
+source third_party/prjxray/.github/kokoro/steps/xilinx.sh
+echo
+echo "========================================"
+echo "Vivado version"
+echo "----------------------------------------"
+(
+    source third_party/prjxray/utils/environment.sh
+    $XRAY_VIVADO -version
+)
+echo "----------------------------------------"
+
+echo
+echo "========================================"
+echo "Running artix7_200t_vendor tests (make all_artix7_200t_diff_fasm)"
+echo "----------------------------------------"
+(
+	cd build
+	export VPR_NUM_WORKERS=${CORES}
+	# Running with -k0 to attempt all tests, and show which ones failed.
+	ninja -k0 -j${MAX_CORES} all_artix7_200t_diff_fasm
+)
+echo "----------------------------------------"
+
+source ${SCRIPT_DIR}/package_results.sh
