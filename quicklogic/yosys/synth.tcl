8694819860d6c8f9b001781d2ed3579465aac3af
Maciej Kurc <mkurc@antmicro.com>
2020-03-05 10:29:44 +0100

    Added connecting of unconnected inputs to GND in the Yosys synthesis script.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/yosys/synth.tcl
+++ quicklogic/yosys/synth.tcl
@@ -12,5 +12,14 @@ write_verilog $::env(OUT_SYNTH_V).premap.v
 # Map to the VPR cell library
 techmap -map  $::env(symbiflow-arch-defs_SOURCE_DIR)/quicklogic/techmap/cells_map.v
 
+# opt_expr -undriven makes sure all nets are driven, if only by the $undef
+# net.
+opt_expr -undriven
+opt_clean
+
+setundef -zero -params
+
+stat
+
 write_json $::env(OUT_JSON)
 write_verilog $::env(OUT_SYNTH_V)
