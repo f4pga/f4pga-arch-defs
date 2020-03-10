trabucayre/symbiflow-arch-defs

install: do not duplicate synth.tcl
Signed-off-by: Karol Gugala <kgugala@antmicro.com>

xc7/yosys/synth.tcl
@@ -7,16 +7,16 @@ yosys -import
 
 # Map (actually rename) explicitly instantiated IBUFs/OBUFs in order to
 # distinguish them from the inferred ones.
-techmap -map $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/iob_map.v
+techmap -map $::env(TECHMAP_PATH)/iob_map.v
 
 # -flatten is used to ensure that the output eblif has only one module.
 # Some of symbiflow expects eblifs with only one module.
 synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp -iopad
 
 # Map inferred IBUFs/OBUFs to wires. This way ROI targets will work.
-techmap -map $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/iob_make_wires.v
+techmap -map $::env(TECHMAP_PATH)/iob_make_wires.v
 # Map previously renamed IBUFs/OBUFs back to their former names.
-techmap -map $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/iob_unmap.v
+techmap -map $::env(TECHMAP_PATH)/iob_unmap.v
 
 if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
   read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
@@ -26,8 +26,8 @@ if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
 write_verilog $::env(OUT_SYNTH_V).premap.v
 
 # Map Xilinx tech library to 7-series VPR tech library.
-read_verilog -lib $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/cells_sim.v
-techmap -map  $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/cells_map.v
+read_verilog -lib $::env(TECHMAP_PATH)/cells_sim.v
+techmap -map  $::env(TECHMAP_PATH)/cells_map.v
 
 # opt_expr -undriven makes sure all nets are driven, if only by the $undef
 # net.

f4pga/f4pga-arch-defs

xc7/yosys/synth.tcl
@@ -18,14 +18,13 @@ if { $::env(USE_ROI) == "TRUE" } {
     read_verilog -lib +/xilinx/cells_xtra.v
 
     # Overwrite some models (e.g. IBUF with more parameters)
-    read_verilog -lib $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/iobs.v
+    read_verilog -lib $::env(TECHMAP_PATH)/iobs.v
 
     hierarchy -check -auto-top
 
     # Start flow after library reading
     synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp -iopad -run prepare:check
 }
-
 if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
   read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
   write_fasm -part_json $::env(PART_JSON)  $::env(OUT_FASM_EXTRA)
@@ -34,8 +33,8 @@ if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
 write_verilog $::env(OUT_SYNTH_V).premap.v
 
 # Map Xilinx tech library to 7-series VPR tech library.
-read_verilog -lib $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/cells_sim.v
-techmap -map  $::env(symbiflow-arch-defs_SOURCE_DIR)/xc7/techmap/cells_map.v
+read_verilog -lib $::env(TECHMAP_PATH)/cells_sim.v
+techmap -map  $::env(TECHMAP_PATH)/cells_map.v
 
 # opt_expr -undriven makes sure all nets are driven, if only by the $undef
 # net.