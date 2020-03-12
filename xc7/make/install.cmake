ea7a1722d2dd068a975aa1e160cc3db0d9bc1aa4
Karol Gugala <kgugala@antmicro.com>
2020-03-12 08:58:11 +0100

    install: fix constraints wrapper file name
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- xc7/make/install.cmake
+++ xc7/make/install.cmake
@@ -23,7 +23,7 @@ function(DEFINE_XC7_TOOLCHAIN_TARGET)
   string(JOIN " " VPR_ARGS ${VPR_BASE_ARGS} "--route_chan_width ${ROUTE_CHAN_WIDTH}" ${VPR_ARCH_ARGS})
   get_target_property_required(FASM_TO_BIT ${ARCH} FASM_TO_BIT)
 
-  set(WRAPPERS env generate_constrains pack place route synth write_bitstream write_fasm)
+  set(WRAPPERS env generate_constraints pack place route synth write_bitstream write_fasm)
   set(TOOLCHAIN_WRAPPERS)
 
   foreach(WRAPPER ${WRAPPERS})
