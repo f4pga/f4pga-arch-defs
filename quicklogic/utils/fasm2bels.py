85b7f44a415e82d2e049a8fec702eba225cb999d
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-03-23 15:56:39 +0100

    quicklogic: fasm2bels: added converting fasm to network of connections
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- quicklogic/utils/fasm2bels.py
+++ quicklogic/utils/fasm2bels.py
@@ -1,140 +1,242 @@
-#!/usr/bin/env python3
 import argparse
 import pickle
 import re
-from collections import defaultdict
-
+from collections import defaultdict, namedtuple
 import fasm
+from connections import get_name_and_hop
 
-import sys
-sys.path.append("../")
-from data_structs import *
+from pathlib import Path
+from data_structs import Loc, SwitchboxPinLoc, PinDirection
+from pprint import pprint as pp
 
-# =============================================================================
+Feature = namedtuple('Feature', 'loc typ signature value')
+RouteEntry = namedtuple('RouteEntry', 'typ stage_id switch_id mux_id sel_id')
 
-def decode_switchbox(switchbox, features):
 
-    # Sort switchbox connecions by destinations
-    conn_by_dst = defaultdict(lambda: set())
-    for c in switchbox.connections:
-        conn_by_dst[c.dst].add(c)
+class Fasm2Bels(object):
+    '''Class for parsing FASM file and producing BEL representation.
+    '''
 
-    # Prepare data structure
-    mux_sel = {}
-    for stage_id, stage in switchbox.stages.items():
-        mux_sel[stage_id] = {}
-        for switch_id, switch in stage.switches.items():
-            mux_sel[stage_id][switch_id] = {}
-            for mux_id, mux in switch.muxes.items():
-                mux_sel[stage_id][switch_id][mux_id] = None
+    class Fasm2BelsException(Exception):
+        '''Exception for Fasm2Bels errors and unsupported features.
+        '''
 
-    # Decode mux settings
-    for feature in features:
+        def __init__(self, message):
+            self.message = message
 
-        # Skip unset features
-        if feature.value == 0:
-            continue
+        def __str__(self):
+            return self.message
 
-        # Decode HIGHWAY muxes
-        match = re.match(r"^.*I_highway\.IM([0-9]+)\.I_pg([0-9]+)$", feature.feature)
+    def __init__(self, vpr_db):
+        '''Prepares required structures.
+
+        Parameters
+        ----------
+        vpr_db: dict
+            A dictionary containing cell_library, loc_map, vpr_tile_types,
+            vpr_tile_grid, vpr_switchbox_types, vpr_switchbox_grid,
+            connections, vpr_package_pinmaps
+        '''
+        self.cells_library = db["cells_library"]
+        self.loc_map = db["loc_map"]
+        self.vpr_tile_types = db["vpr_tile_types"]
+        self.vpr_tile_grid  = db["vpr_tile_grid"]
+        self.vpr_switchbox_types = db["vpr_switchbox_types"]
+        self.vpr_switchbox_grid  = db["vpr_switchbox_grid"]
+        self.connections = db["connections"]
+
+        self.designconnections = defaultdict(dict)
+        self.designhops = defaultdict(dict)
+
+        self.connections_by_loc =defaultdict(list)
+        for connection in self.connections:
+            self.connections_by_loc[connection.dst].append(connection)
+            self.connections_by_loc[connection.src].append(connection)
+
+        self.featureparsers = {
+            'LOGIC': self.parse_logic_line,
+            'QMUX': self.parse_logic_line,
+            'GMUX': self.parse_logic_line,
+            'INTERFACE': self.parse_interface_line,
+            'ROUTING': self.parse_routing_line
+        }
+
+        self.routingdata = defaultdict(list)
+        self.belinversions = defaultdict(lambda: defaultdict(list))
+        self.interfaces = defaultdict(lambda: defaultdict(list))
+
+    def parse_logic_line(self, feature: Feature):
+        belname, setting = feature.signature.split('.', 1)
+        if feature.value == 1:
+            self.belinversions[feature.loc][belname].append(setting)
+
+    def parse_interface_line(self, feature: Feature):
+        belname, setting = feature.signature.split('.', 1)
+        if feature.value == 1:
+            self.interfaces[feature.loc][belname].append(setting)
+
+    def parse_routing_line(self, feature: Feature):
+        match = re.match(
+            r'^I_highway\.IM(?P<switch_id>[0-9]+)\.I_pg(?P<sel_id>[0-9]+)$',
+            feature.signature)
         if match:
-            stage_id    = 3  # FIXME: Get HIGHWAY stage id from the switchbox def!
-            switch_id   = int(match.group(1))
-            mux_id      = 0
-            sel_id      = int(match.group(2))
+            typ = 'HIGHWAY'
+            stage_id = 3 # FIXME: Get HIGHWAY stage id from the switchbox def
+            switch_id = int(match.group('switch_id'))
+            mux_id = 0
+            sel_id = int(match.group('sel_id'))
+        match = re.match(
+            r'^I_street\.Isb(?P<stage_id>[0-9])(?P<switch_id>[0-9])\.I_M(?P<mux_id>[0-9]+)\.I_pg(?P<sel_id>[0-9]+)$',
+            feature.signature)
+        if match:
+            typ = 'STREET'
+            stage_id = int(match.group('stage_id')) - 1
+            switch_id = int(match.group('switch_id')) - 1
+            mux_id = int(match.group('mux_id'))
+            sel_id = int(match.group('sel_id'))
+        self.routingdata[feature.loc].append(RouteEntry(
+            typ=typ,
+            stage_id=stage_id,
+            switch_id=switch_id,
+            mux_id=mux_id,
+            sel_id=sel_id))
+
+    def parse_fasm_lines(self, fasmlines):
+        '''Parses FASM lines.
         
-#            print("HIGHWAY", stage_id, switch_id, mux_id, sel_id)
-
-            assert mux_sel[stage_id][switch_id][mux_id] is None, ("HIGHWAY", stage_id, switch_id, mux_id, sel_id)
-            mux_sel[stage_id][switch_id][mux_id] = sel_id
-
-        # Decode STREET muxes
-        match = re.match(r"^.*I_street\.Isb([0-9])([0-9])\.I_M([0-9]+)\.I_pg([0-9]+)$", feature.feature)
-        if match:
-            stage_id    = int(match.group(1)) - 1
-            switch_id   = int(match.group(2)) - 1
-            mux_id      = int(match.group(3))
-            sel_id      = int(match.group(4))
-
-#            print("STREET", stage_id, switch_id, mux_id, sel_id)
-
-            assert mux_sel[stage_id][switch_id][mux_id] is None, ("STREET", stage_id, switch_id, mux_id, sel_id)
-            mux_sel[stage_id][switch_id][mux_id] = sel_id
-
-    def expand_mux(out_loc):
-        """
-        Expands a multiplexer output until a switchbox input is reached.
-        Returns name of the input or None if not found.
-        """
-
-        # Get mux selection, If it is set to None then the mux is
-        # not active
-        sel = mux_sel[out_loc.stage_id][out_loc.switch_id][out_loc.mux_id]
-        if sel is None:
-            return None
-
-        # Check if we have hit a top-level input
-        stage  = switchbox.stages[out_loc.stage_id]
-        switch = stage.switches[out_loc.switch_id]
-        mux    = switch.muxes[out_loc.mux_id]
-        pin    = mux.inputs[sel]
-
-        if pin.name is not None:
-            return pin.name
-
-        # Make location of the mux input pin
-        inp_loc = SwitchboxPinLoc(
-            stage_id    = out_loc.stage_id,
-            switch_id   = out_loc.switch_id,
-            mux_id      = out_loc.mux_id,
-            pin_id      = sel,
-            pin_direction = PinDirection.INPUT
-        )        
-
-        # Expand all "upstream" muxes that connect to the selected
-        # input pin
-        assert inp_loc in conn_by_dst, inp_loc
-        for c in conn_by_dst[inp_loc]:
-            inp_name = expand_mux(c.src)
-            if inp_name is not None:
-                return inp_name
-
-        # Nothing found
-        return None
-
-    # For each output pin of a switchbox determine tw which input is it
-    # connected to.
-    routes = {}
-    for out_pin in switchbox.outputs.values():
-        out_loc = out_pin.locs[0]
-        routes[out_pin.name] = expand_mux(out_loc)
-
-    return routes
-
-
-def process_switchbox(loc, switchbox, features, connections_by_loc):
-
-    # Decode switchbox routes
-    routes = decode_switchbox(switchbox, features)
-
-    # Find local connections
+        Parameters
+        ----------
+        fasmlines: list
+            A list of FasmLine objects
+        '''
     
+        loctyp = re.compile(r'^X(?P<x>[0-9]+)Y(?P<y>[0-9]+)\.(?P<type>[A-Z]+)\.(?P<signature>.*)$')  # noqa: E501
 
-    # DEBUG
-    for k,v in routes.items():
-        if v is not None:
-            print("X{}Y{} {:<10} <= {:<5}".format(loc.x, loc.y, k, v))
+        for line in fasmlines:
+            if not line.set_feature:
+                continue
+            match = loctyp.match(line.set_feature.feature)
+            if not match:
+                raise self.Fasm2BelsException(f'FASM features have unsupported format:  {line.set_feature}')  # noqa: E501
+            loc = Loc(
+                x=int(match.group('x')),
+                y=int(match.group('y')))
+            typ=match.group('type')
+            feature = Feature(
+                loc=loc,
+                typ=typ,
+                signature=match.group('signature'),
+                value=line.set_feature.value)
+            self.featureparsers[typ](feature)
 
-    # F2A_io_t_1  => IE
-    # F2A_io_t_2  => INEN
-    # F2A_io_t_4  => OQI
-    # A2F_hop_t_1 <= IZ
-    # A2F_hop_t_3 <= IZ
+    def decode_switchbox(self, switchbox, features):
+        # Group switchbox connections by destinationa
+        conn_by_dst = defaultdict(set)
+        for c in switchbox.connections:
+            conn_by_dst[c.dst].add(c)
 
-# =============================================================================
+        # Prepare data structure
+        mux_sel = {}
+        for stage_id, stage in switchbox.stages.items():
+            mux_sel[stage_id] = {}
+            for switch_id, switch in stage.switches.items():
+                mux_sel[stage_id][switch_id] = {}
+                for mux_id, mux in switch.muxes.items():
+                    mux_sel[stage_id][switch_id][mux_id] = None
 
-def main():
-    
+        for feature in features:
+            assert mux_sel[feature.stage_id][feature.switch_id][feature.mux_id] is None, feature
+            mux_sel[feature.stage_id][feature.switch_id][feature.mux_id] = feature.sel_id
+
+        def expand_mux(out_loc):
+            """
+            Expands a multiplexer output until a switchbox input is reached.
+            Returns name of the input or None if not found.
+            """
+
+            # Get mux selection, If it is set to None then the mux is
+            # not active
+            sel = mux_sel[out_loc.stage_id][out_loc.switch_id][out_loc.mux_id]
+            if sel is None:
+                return None  # TODO can we return None?
+
+            stage = switchbox.stages[out_loc.stage_id]
+            switch = stage.switches[out_loc.switch_id]
+            mux = switch.muxes[out_loc.mux_id]
+            pin = mux.inputs[sel]
+
+            if pin.name is not None:
+                return pin.name
+
+            inp_loc = SwitchboxPinLoc(
+                stage_id=out_loc.stage_id,
+                switch_id=out_loc.switch_id,
+                mux_id=out_loc.mux_id,
+                pin_id=sel,
+                pin_direction=PinDirection.INPUT
+            )
+
+            # Expand all "upstream" muxes that connect to the selected
+            # input pin
+            assert inp_loc in conn_by_dst, inp_loc
+            for c in conn_by_dst[inp_loc]:
+                inp = expand_mux(c.src)
+                if inp is not None:
+                    return inp
+
+            # Nothing found
+            return None  # TODO can we return None?
+
+        # For each output pin of a switchbox determine to which input is it
+        # connected to.
+        routes = {}
+        for out_pin in switchbox.outputs.values():
+            out_loc = out_pin.locs[0]
+            routes[out_pin.name] = expand_mux(out_loc)
+
+        return routes
+
+    def process_switchbox(self, loc, switchbox, features):
+        routes = self.decode_switchbox(switchbox, features)
+        for k, v in routes.items():
+            if v is not None:
+                if re.match('[VH][0-9][LRBT][0-9]', k):
+                    self.designhops[(loc.x, loc.y)][k] = v
+                else:
+                    self.designconnections[(loc.x, loc.y)][k] = v
+                print(f"X{loc.x:02d}Y{loc.y:02d} {k:<10} <= {v}")
+
+    def resolve_hops(self):
+        for loc, conns in self.designconnections.items():
+            for pin, source in conns.items():
+                # print(f' PIN:  {loc} {pin} {source}')
+                hop = get_name_and_hop(source)
+                tloc = loc
+                while hop[1] is not None:
+                    # print(f'{(loc[0] + hop[1][0], loc[1] + hop[1][1])} {hop[0]}')
+                    # print(f'{loc} {hop}')
+                    tloc = (tloc[0] + hop[1][0], tloc[1] + hop[1][1])
+                    hop = get_name_and_hop(self.designhops[tloc][hop[0]])
+                self.designconnections[loc][pin] = (tloc, hop[0])
+
+    def resolve_connections(self):
+        '''Resolves connections between BELs based on switchboxes.
+        '''
+        keys = sorted(self.routingdata.keys(), key=lambda loc: (loc.x, loc.y))
+        for phy_loc in keys:
+            routingfeatures = self.routingdata[phy_loc]
+            # map location to VPR coordinates
+            if phy_loc not in self.loc_map.fwd:
+                continue
+            loc = self.loc_map.fwd[phy_loc]
+
+            if loc in self.vpr_switchbox_grid:
+                typ = self.vpr_switchbox_grid[loc]
+                switchbox = self.vpr_switchbox_types[typ]
+                self.process_switchbox(loc, switchbox, routingfeatures)
+
+
+if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -158,59 +260,28 @@ def main():
     print("Loading database...")
     with open(args.vpr_db, "rb") as fp:
         db = pickle.load(fp)
+    print('Database loaded')
 
-#        cells_library  = db["cells_library"]
-        loc_map        = db["loc_map"]
-#        vpr_tile_types = db["vpr_tile_types"]
-#        vpr_tile_grid  = db["vpr_tile_grid"]
-        vpr_switchbox_types = db["vpr_switchbox_types"]
-        vpr_switchbox_grid  = db["vpr_switchbox_grid"]
-        connections    = db["connections"]
+    f2b = Fasm2Bels(db)
 
-    # Load fasm features
-    features_by_loc = defaultdict(lambda: set())
-    for fasm_line in fasm.parse_fasm_filename(args.fasm_file):
+    print('Fasm2bels created')
+    fasmlines = [line for line in fasm.parse_fasm_filename(args.fasm_file)]
+    f2b.parse_fasm_lines(fasmlines)
+    f2b.resolve_connections()
 
-        if not fasm_line.set_feature:
-            continue
+    print('Connections resolved')
 
-        # Decode loc
-        parts = fasm_line.set_feature.feature.split(".", maxsplit=1)
-        match = re.match(r"^X([0-9]+)Y([0-9]+)$", parts[0])
-        assert match is not None, parts
-
-        phy_loc = Loc(
-            x=int(match.group(1)),
-            y=int(match.group(2)),
-        )
-
-        # Store the feature
-        features_by_loc[phy_loc].add(fasm_line.set_feature)
-
-    # Sort connections by locations they mention
-    connections_by_loc = defaultdict(lambda: [])
-    for connection in connections:
-        connections_by_loc[connection.dst].append(connections)
-        connections_by_loc[connection.src].append(connections)
-
-    # Process features
-    keys = sorted(features_by_loc.keys(), key=lambda loc: (loc.x, loc.y))
-    for phy_loc in keys:
-        features = features_by_loc[phy_loc]
-
-        # Map location to VPR coordinates
-        if phy_loc not in loc_map.fwd:
-            continue
-        loc = loc_map.fwd[phy_loc]
-
-        # Process the switchbox
-        if loc in vpr_switchbox_grid:
-            type      = vpr_switchbox_grid[loc]
-            switchbox = vpr_switchbox_types[type]
-            process_switchbox(loc, switchbox, features, connections_by_loc)
- 
-
-# =============================================================================
-
-if __name__ == "__main__":
-    main()
+    # print('BEL set features')
+    # pp(f2b.belinversions)
+    # print('BEL interfaces')
+    # pp(f2b.interfaces)
+    # print('Routing data')
+    # pp(f2b.routingdata)
+    # pp(f2b.connections)
+    print('DESIGN HOPS')
+    pp(f2b.designhops)
+    print('DESIGN CONNECTIONS')
+    pp(f2b.designconnections)
+    f2b.resolve_hops()
+    print('DESIGN CONNECTIONS v2')
+    pp(f2b.designconnections)
