3dba30bc6c315617534a63ba4f8e71e616e33d00
Karol Gugala <kgugala@antmicro.com>
2020-03-27 10:04:26 +0100

    quicklogic: techmap: add dffc
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- quicklogic/techmap/cells_map.v
+++ quicklogic/techmap/cells_map.v
@@ -379,6 +379,31 @@ module dff(
 
 endmodule
 
+module dffc(
+  output Q,
+  input  D,
+  input  CLK,
+  input  CLR
+);
+
+  parameter [0:0] INIT = 1'b0;
+
+  Q_FRAG # (
+  .Z_QCKS (1'b1)
+  )
+  _TECHMAP_REPLACE_
+  (
+  .QCK(CLK),
+  .QST(1'b0),
+  .QRT(CLR),
+  .QEN(1'b1),
+  .QDI(D),
+  .QDS(1'b1), // FIXME: Always select QDI as the FF's input
+  .CZI(),
+  .QZ (Q)
+  );
+
+endmodule
 // ============================================================================
 // The "qlal4s3b_cell_macro" macro
 
