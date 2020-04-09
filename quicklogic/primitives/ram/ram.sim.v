b1cf1c9b3fbaee6842d76c685dc0f97e514d9f01
Karol Gugala <kgugala@antmicro.com>
2020-04-09 16:10:38 +0200

    QL: remove non-routable pins from ram model
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- quicklogic/primitives/ram/ram.sim.v
+++ quicklogic/primitives/ram/ram.sim.v
@@ -7,7 +7,7 @@ module RAM (
     input CLK1_0,
     input CLK1_1,
     output Almost_Empty_0, Almost_Empty_1, Almost_Full_0, Almost_Full_1,
-    input ASYNC_FLUSH_0, ASYNC_FLUSH_1,CLK2_0, CLK2_1, ASYNC_FLUSH_S0, ASYNC_FLUSH_S1, CLK1EN_0, CLK1EN_1, CLK1S_0, CLK1S_1,CLK2EN_0,CLK2EN_1, CLK2S_0, CLK2S_1, CONCAT_EN_0, CONCAT_EN_1, CS1_0, CS1_1,CS2_0, CS2_1, DIR_0, DIR_1, FIFO_EN_0, FIFO_EN_1, P1_0, P1_1, P2_0,P2_1, PIPELINE_RD_0, PIPELINE_RD_1,
+    input ASYNC_FLUSH_0, ASYNC_FLUSH_1,CLK2_0, CLK2_1, CLK1EN_0, CLK1EN_1, CLK2EN_0,CLK2EN_1, CONCAT_EN_0, CONCAT_EN_1, CS1_0, CS1_1,CS2_0, CS2_1, DIR_0, DIR_1, FIFO_EN_0, FIFO_EN_1, P1_0, P1_1, P2_0,P2_1, PIPELINE_RD_0, PIPELINE_RD_1,
     output [3:0] POP_FLAG_0,
     output [3:0] POP_FLAG_1,
     output [3:0] PUSH_FLAG_0,
@@ -23,8 +23,10 @@ module RAM (
     input [1:0] WIDTH_SELECT1_1,
     input [1:0] WIDTH_SELECT2_0,
     input [1:0] WIDTH_SELECT2_1,
-    input SD,DS,LS,SD_RB1,LS_RB1,DS_RB1,RMEA,RMEB,TEST1A,TEST1B,
+    input SD,DS,LS,SD_RB1,LS_RB1,DS_RB1,RMEA,RMEB,
     input [3:0] RMA,
-    input [3:0] RMB);
+    input [3:0] RMB,
+    input TEST1A, TEST1B
+    );
 
 endmodule /* ram8k_2x1_cell_macro */
