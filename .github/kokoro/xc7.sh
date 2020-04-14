19ce1ae0685f4e17efb5cdd4ec1c854c3472a878
Alessandro Comodi <acomodi@antmicro.com>
2020-04-14 19:05:18 +0200

    ci: move install from xc7 to install script
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/kokoro/xc7.sh
+++ .github/kokoro/xc7.sh
@@ -3,7 +3,6 @@
 SCRIPT_SRC="$(realpath ${BASH_SOURCE[0]})"
 SCRIPT_DIR="$(dirname "${SCRIPT_SRC}")"
 INSTALL_DIR="$(pwd)/install"
-GIT_DESCRIBE="$(git describe)"
 
 export CMAKE_FLAGS="-GNinja -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
 export BUILD_TOOL=ninja
@@ -18,41 +17,6 @@ echo "----------------------------------------"
 	export VPR_NUM_WORKERS=${CORES}
 	ninja -j${MAX_CORES} all_xc7
 	ninja print_qor > xc7_qor.csv
-	popd
-)
-echo "----------------------------------------"
-
-echo
-echo "========================================"
-echo "Running install tests (make install)"
-echo "----------------------------------------"
-(
-	pushd build
-	export VPR_NUM_WORKERS=${CORES}
-	ninja -j${MAX_CORES} install
-	popd
-)
-echo "----------------------------------------"
-
-echo
-echo "========================================"
-echo "Compressing install directory"
-echo "----------------------------------------"
-(
-	tar -c --use-compress-program="pigz" -f symbiflow-arch-defs-install-${GIT_DESCRIBE}.tar.gz ${INSTALL_DIR}
-)
-echo "----------------------------------------"
-
-echo
-echo "========================================"
-echo "Running installed toolchain tests"
-echo "----------------------------------------"
-(
-
-	pushd build
-	export VPR_NUM_WORKERS=${CORES}
-	export CTEST_OUTPUT_ON_FAILURE=1
-	ctest -R binary_toolchain_test -j${MAX_CORES}
 	popd
 )
 echo "----------------------------------------"
