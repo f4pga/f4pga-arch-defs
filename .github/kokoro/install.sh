d002db6172097543fc581ec4a5f60f17bdd58620
Alessandro Comodi <acomodi@antmicro.com>
2020-04-14 19:09:55 +0200

    ci: fix install paths
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/kokoro/install.sh
+++ .github/kokoro/install.sh
@@ -2,10 +2,9 @@
 
 SCRIPT_SRC="$(realpath ${BASH_SOURCE[0]})"
 SCRIPT_DIR="$(dirname "${SCRIPT_SRC}")"
-INSTALL_DIR="$(pwd)/install"
-ROOT_DIR="$(pwd)"
-GIT_DESCRIBE=$(git describe)
+INSTALL_DIR="$(pwd)/github/${KOKORO_DIR}/install"
 
+export GIT_DESCRIBE=$(git describe)
 export CMAKE_FLAGS="-GNinja -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
 export BUILD_TOOL=ninja
 source ${SCRIPT_DIR}/common.sh
@@ -27,9 +26,7 @@ echo "========================================"
 echo "Compressing and uploading install dir"
 echo "----------------------------------------"
 (
-	pushd ${ROOT_DIR}
-	tar -cf install | xz -9 -T`nproc` > ${ROOT_DIR}/symbiflow-arch-defs-install-$(git describe).tar.xz
-	popd
+	tar -cf install | xz -9 -T${CORES} > symbiflow-arch-defs-install-${GIT_DESCRIBE}.tar.xz
 )
 echo "----------------------------------------"
 
