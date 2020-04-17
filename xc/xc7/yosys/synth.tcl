13d26942be74e39b5f062f192c342f777c90c3bb
Alessandro Comodi <acomodi@antmicro.com>
2020-04-17 10:28:03 +0200

    Changes to have new master+wip yosys integrated
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc/xc7/yosys/synth.tcl
+++ xc/xc7/yosys/synth.tcl
@@ -11,10 +11,10 @@ yosys -import
 #
 # Do not infer IOBs for targets that use a ROI.
 if { $::env(USE_ROI) == "TRUE" } {
-    synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp
+    synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp -noiopad -nowidelut
 } else {
     # Read Yosys baseline library first.
-    read_verilog -lib -D_EXPLICIT_CARRY +/xilinx/cells_sim.v
+    read_verilog -lib -specify -D_EXPLICIT_CARRY +/xilinx/cells_sim.v
     read_verilog -lib +/xilinx/cells_xtra.v
 
     # Overwrite some models (e.g. IBUF with more parameters)
@@ -23,7 +23,7 @@ if { $::env(USE_ROI) == "TRUE" } {
     hierarchy -check -auto-top
 
     # Start flow after library reading
-    synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp -iopad -run prepare:check
+    synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp -nowidelut -run prepare:check
 }
 if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
   read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
@@ -43,6 +43,9 @@ opt_clean
 
 setundef -zero -params
 stat
+
+# TODO: remove this as soon as new VTR master+wip is pushed: https://github.com/SymbiFlow/vtr-verilog-to-routing/pull/525
+attrmap -remove hdlname
 
 # Write the design in JSON format.
 write_json $::env(OUT_JSON)
