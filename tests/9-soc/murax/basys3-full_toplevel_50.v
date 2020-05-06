5542f231cec6afaa26aaa9f169ef0ab7d3daaf28
Maciej Kurc <mkurc@antmicro.com>
2020-05-06 13:17:20 +0200

    Cleaned up Murax
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- tests/9-soc/murax/basys3-full_toplevel_50.v
+++ tests/9-soc/murax/basys3-full_toplevel_50.v
@@ -1,12 +1,19 @@
 `timescale 1ns / 1ps
 
-module top (
-    input  clk,
-    output tx,
-    input  rx,
-    input  [7:0] sw,
-    output [7:0] led
+module toplevel(
+    input   clk,
+    output  tx,
+    input   rx,
+    input [15:0] sw,
+    output [15:0] io_led
   );
+
+  wire clk100;
+  BUFG bufg(.I(clk), .O(clk100));
+
+  reg clk50;
+  always @(posedge clk100)
+    clk50 <= !clk50;
 
   wire [31:0] io_gpioA_read;
   wire [31:0] io_gpioA_write;
@@ -19,22 +26,19 @@ module top (
   wire io_uart_txd;
   wire io_uart_rxd;
 
-  assign led = io_gpioA_write[7: 0];
-  assign io_gpioA_read[7:0] = sw;
-
-  wire clk_bufg;
-  BUFG bufg (.I(io_mainClk), .O(clk_bufg));
+  assign io_led = io_gpioA_write[15: 0];
+  assign io_gpioA_read[15:0] = sw;
 
   Murax murax (
     .io_asyncReset(0),
-    .io_mainClk (clk_bufg),
+    .io_mainClk (clk50),
     .io_jtag_tck(1'b0),
     .io_jtag_tdi(1'b0),
     .io_jtag_tms(1'b0),
     .io_gpioA_read       (io_gpioA_read),
     .io_gpioA_write      (io_gpioA_write),
     .io_gpioA_writeEnable(io_gpioA_writeEnable),
-    .io_uart_txd(txd),
-    .io_uart_rxd(rxd)
+    .io_uart_txd(tx),
+    .io_uart_rxd(rx)
   );
 endmodule
--- tests/9-soc/murax/nexys_video.sdc
+++
@@ -1,1 +0,0 @@
-create_clock -period 10 clk_bufg
