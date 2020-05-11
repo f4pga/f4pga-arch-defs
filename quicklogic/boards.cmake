d82b5dfc0ed4caf7e3b6aed8901d02c3891abe8e
Maciej Kurc <mkurc@antmicro.com>
2020-05-11 14:04:47 +0200

    Added QuickFeather board target, ported some demo designs to it.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/boards.cmake
+++ quicklogic/boards.cmake
@@ -6,6 +6,13 @@ add_quicklogic_board(
 )
 
 add_quicklogic_board(
+  BOARD quickfeather
+  DEVICE ql-eos-s3
+  PACKAGE wlcsp
+  FABRIC_PACKAGE PU90
+)
+
+add_quicklogic_board(
   BOARD jimbob4
   DEVICE ql-pp3e
   PACKAGE wlcsp
