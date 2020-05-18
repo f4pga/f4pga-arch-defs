93a933049f59c3911ec0d59315f6acb2aafdf5de
Maciej Kurc <mkurc@antmicro.com>
2020-05-18 16:44:51 +0200

    ODDR arch + techmap
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc/xc7/techmap/cells_sim.v
+++ xc/xc7/techmap/cells_sim.v
@@ -1124,6 +1124,30 @@ module IDDR_VPR (
 
 endmodule
 
+(* blackbox *)
+module ODDR_VPR (
+  input  CK,
+  input  CE,
+  input  SR,
+  input  D1,
+  input  D2,
+  output Q
+);
+
+  parameter [0:0] ZINV_CLK = 1'b1;
+  parameter [0:0] ZINV_D1  = 1'b1;
+  parameter [0:0] ZINV_D2  = 1'b1;
+  parameter [0:0] INV_D1   = 1'b0;
+  parameter [0:0] INV_D2   = 1'b0;
+
+  parameter [0:0] SRTYPE_SYNC = 1'b0;
+  parameter [0:0] SAME_EDGE   = 1'b1;
+
+  parameter [0:0] ZINIT_Q  = 1'b1;
+  parameter [0:0] ZSRVAL_Q = 1'b1;
+
+endmodule
+
 // ============================================================================
 // IDELAYE2
 
