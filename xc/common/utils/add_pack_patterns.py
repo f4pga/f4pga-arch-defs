429d329e2f7a20e1671736ca638660f1da3d1340
Maciej Kurc <mkurc@antmicro.com>
2020-05-19 14:17:28 +0200

    Pack patterns for IDDR and ODDR
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc/common/utils/add_pack_patterns.py
+++ xc/common/utils/add_pack_patterns.py
@@ -19,6 +19,7 @@ between the various sites of the tile (e.g. ISERDES, IDELAY, IOB33 and OSERDES).
 import lxml.etree as ET
 import argparse
 import re
+import itertools
 
 # Regular Expressions to select the directs that need additional pack patterns.
 # Being multiple IOB and IOPAD types, the name of the direct changes according
@@ -57,6 +58,10 @@ def check_direct(element, list_to_check):
             - element: direct that needs to be checked;
             - list_to_check: operational mode or pb_type and port of the direct to select.
     """
+
+    if element.tag != "direct":
+        return False
+
     interconnect = element.getparent()
     mode_or_pb_type = interconnect.getparent()
 
@@ -72,18 +77,25 @@ def check_direct(element, list_to_check):
     return False
 
 
-def add_pack_pattern(direct, pack_pattern_prefix):
-    """ Adds the pack pattern to the given direct with a specified prefix. """
-    top_pb_type = get_top_pb_type(direct)
+def add_pack_pattern(element, pack_pattern_prefix, for_input=None):
+    """ Adds the pack pattern to the given direct / mux with a specified
+        prefix. """
+    top_pb_type = get_top_pb_type(element)
 
     pack_pattern_name = "{}_{}".format(
         pack_pattern_prefix, top_pb_type.attrib['name']
     )
-    pack_pattern_in_port = direct.attrib['input']
-    pack_pattern_out_port = direct.attrib['output']
+
+    if for_input is not None:
+        assert for_input in element.attrib['input']
+        pack_pattern_in_port = for_input
+    else:
+        pack_pattern_in_port = element.attrib['input']
+
+    pack_pattern_out_port = element.attrib['output']
 
     ET.SubElement(
-        direct,
+        element,
         'pack_pattern',
         attrib={
             'name': pack_pattern_name,
@@ -92,6 +104,53 @@ def add_pack_pattern(direct, pack_pattern_prefix):
         }
     )
 
+# =============================================================================
+
+
+def maybe_add_pack_pattern(element, pack_pattern_prefix, list_to_check):
+    """
+    Adds a pack pattern to the element ("direct" or "mux") that spans the
+    connection only if both of its endpoints are found in the list.
+
+    The pack pattern is prefixed with the pack_pattern_prefix and with a name
+    of the topmost pb_type in the hierarchy.
+
+    The list_to_check must contain tuples specifying connection endpoints in
+    the same way as in the arch.xml ie. "<pb_name>.<port_name>".
+    """
+
+    # Check if we have a direct under an interconnect inside a mode/pb_type
+    interconnect = element.getparent()
+    if interconnect.tag != "interconnect":
+        return
+
+    mode_or_pb_type = interconnect.getparent()
+    if mode_or_pb_type.tag not in ['mode', 'pb_type']:
+        return
+
+    # A direct connection
+    if element.tag == "direct":
+        inp = element.attrib['input']
+        out = element.attrib['output']
+
+        if (inp, out) in list_to_check:
+            add_pack_pattern(element, pack_pattern_prefix)
+
+    # A mux connections (match the input)
+    elif element.tag == "mux":
+        ins = element.attrib['input'].split()
+        out = element.attrib['output']
+
+        for inp in ins:
+            if (inp, out) in list_to_check:
+                add_pack_pattern(element, pack_pattern_prefix, inp)
+
+    # Shouldn't happen
+    else:
+        assert False, element.tag
+
+
+# =============================================================================
 
 # =============================================================================
 
@@ -107,8 +166,9 @@ def main():
     arch_xml = ET.ElementTree()
     xml_parser = ET.XMLParser(remove_blank_text=True)
     root_element = arch_xml.parse(args.in_arch, xml_parser)
-
-    for direct in root_element.iter('direct'):
+    
+    gen = itertools.chain(root_element.iter('direct'), root_element.iter('mux'))
+    for direct in gen:
         if 'name' not in direct.attrib:
             continue
 
@@ -119,6 +179,25 @@ def main():
             top_name = ""
 
         dir_name = direct.attrib['name']
+
+        #
+        # ODDR
+        #
+
+        # Adding ODDR via OBUF pack patterns
+        if IOPAD_OLOGIC_REGEX.match(dir_name):
+            add_pack_pattern(direct, 'ODDR_OBUF')
+
+        maybe_add_pack_pattern(direct, 'ODDR_OBUF', [
+            ('ODDR.Q',          'OLOGIC_OFF.OQ'),
+            ('OLOGIC_OFF.OQ',   'OLOGICE3.OQ'),
+            ('IOB33M.O',        'IOB33_MODES.O'),
+            ('IOB33S.O',        'IOB33_MODES.O'),
+            ('IOB33.O',         'IOB33_MODES.O'),
+            ('IOB33_MODES.O',   'OBUF_VPR.I'),
+            ('OBUF_VPR.O',      'outpad.outpad'),
+        ])
+
 
         #
         # OSERDES
@@ -183,6 +262,42 @@ def main():
                 ('IOB33M', 'O'),
             ]):
                 add_pack_pattern(direct, 'OSERDES_IOBUFDS_M')
+
+        #
+        # IDDR
+        #
+
+        # Adding IDDR via IBUF pack patterns
+        if IOPAD_ILOGIC_REGEX.match(dir_name):
+            add_pack_pattern(direct, 'IDDR_IBUF')
+
+        maybe_add_pack_pattern(direct, 'IDDR_IBUF', [
+            ('inpad.inpad',     'IBUF_VPR.I'),
+            ('IBUF_VPR.O',      'IOB33_MODES.I'),
+            ('IOB33_MODES.I',   'IOB33.I'),
+            ('IOB33_MODES.I',   'IOB33M.I'),
+            ('IOB33_MODES.I',   'IOB33S.I'),
+            ('ILOGICE3.D',      'IFF.D')
+        ])
+
+        # TODO: IDDR via IOBUF, IDDR via IOBUFDS
+
+        # Adding IDDR+IDELAY via IBUF pack patterns
+        if 'I_to_IDELAYE2' in dir_name:
+            add_pack_pattern(direct, 'IDDR_IDELAY_IBUF')
+
+        maybe_add_pack_pattern(direct, 'IDDR_IDELAY_IBUF', [
+            ('inpad.inpad',     'IBUF_VPR.I'),
+            ('IBUF_VPR.O',      'IOB33_MODES.I'),
+            ('IOB33_MODES.I',   'IOB33.I'),
+            ('IOB33_MODES.I',   'IOB33M.I'),
+            ('IOB33_MODES.I',   'IOB33S.I'),
+            ('IDELAYE2.DATAOUT','ILOGICE3.DDLY'),
+            ('ILOGICE3.DDLY',   'IFF.D')
+        ])
+
+        # TODO: IDDR+IDELAY via IOBUF, IDDR+IDELAY via IOBUFDS
+
 
         #
         # ISERDES, no IDELAY
