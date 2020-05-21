2a94f5183aba39288e27634abbda53e140d49de4
Maciej Kurc <mkurc@antmicro.com>
2020-05-21 15:11:38 +0200

    Added techmap for BUFGCE
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc/xc7/techmap/cells_map.v
+++ xc/xc7/techmap/cells_map.v
@@ -2985,6 +2985,25 @@ module BUFGCE (
   );
 endmodule
 
+module BUFGCE (
+  input  I,
+  input  CE,
+  output O
+);
+
+  BUFGCTRL _TECHMAP_REPLACE_ (
+    .O(O),
+    .CE0(CE),
+    .CE1(1'b0),
+    .I0(I),
+    .I1(1'b1),
+    .IGNORE0(1'b0),
+    .IGNORE1(1'b1),
+    .S0(1'b1),
+    .S1(1'b0)
+  );
+endmodule
+
 module BUFGCTRL (
 output O,
 input I0, input I1,
