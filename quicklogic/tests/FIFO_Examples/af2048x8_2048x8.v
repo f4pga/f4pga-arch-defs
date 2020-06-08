982e83d780ae1ec31dd8cd78deefac59e1eeb16f
Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>
2020-06-08 19:22:25 +0530

    Adding S3 FIFOs Examples
    Signed-off-by: Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>

--- quicklogic/tests/FIFO_Examples/af2048x8_2048x8.v
+++ quicklogic/tests/FIFO_Examples/af2048x8_2048x8.v
@@ -1,20 +1,20 @@
-module af512x32_512x32 (DIN,Fifo_Push_Flush,Fifo_Pop_Flush,PUSH,POP,Push_Clk,Pop_Clk,Push_Clk_En,Pop_Clk_En,Fifo_Dir,Async_Flush,Almost_Full,Almost_Empty,PUSH_FLAG,POP_FLAG,DOUT);
+module af2048x8_2048x8(DIN,Fifo_Push_Flush,Fifo_Pop_Flush,PUSH,POP,Push_Clk,Pop_Clk,Push_Clk_En,Pop_Clk_En,Fifo_Dir,Async_Flush,Almost_Full,Almost_Empty,PUSH_FLAG,POP_FLAG,DOUT);
 
 input Fifo_Push_Flush,Fifo_Pop_Flush;
 input Push_Clk,Pop_Clk;
 input PUSH,POP;
-input [31:0] DIN;
+input [7:0] DIN;
 input Push_Clk_En,Pop_Clk_En,Fifo_Dir,Async_Flush;
-output [31:0] DOUT;
+output [7:0] DOUT;
 output [3:0] PUSH_FLAG,POP_FLAG;
 output Almost_Full,Almost_Empty;
 
 parameter Concatenation_En = 1;
 
-parameter wr_depth_int0 = 512;
-parameter rd_depth_int0 = 512;
-parameter wr_width_int0 = 32;
-parameter rd_width_int0 = 32;
+parameter wr_depth_int0 = 2048;
+parameter rd_depth_int0 = 2048;
+parameter wr_width_int0 = 8;
+parameter rd_width_int0 = 8;
 parameter reg_rd_int0 = 0;
 parameter sync_fifo_int0 = 0;
 
@@ -38,7 +38,7 @@ FIFO_INST      (.DIN0(DIN),
 				.Almost_Full0(Almost_Full),
 				.Almost_Empty0(Almost_Empty),
 				.DOUT0(DOUT),
-		
+				
 				.LS(1'b0),
 				.SD(1'b0),
 				.DS(1'b0),
--- quicklogic/tests/FIFO_Examples/f1024x16_1024x16.v
+++ quicklogic/tests/FIFO_Examples/f1024x16_1024x16.v
@@ -1,22 +1,22 @@
-module af512x32_512x32 (DIN,Fifo_Push_Flush,Fifo_Pop_Flush,PUSH,POP,Push_Clk,Pop_Clk,Push_Clk_En,Pop_Clk_En,Fifo_Dir,Async_Flush,Almost_Full,Almost_Empty,PUSH_FLAG,POP_FLAG,DOUT);
+module f1024x16_1024x16(DIN,Fifo_Push_Flush,Fifo_Pop_Flush,PUSH,POP,Clk,Clk_En,Fifo_Dir,Async_Flush,Almost_Full,Almost_Empty,PUSH_FLAG,POP_FLAG,DOUT);
 
 input Fifo_Push_Flush,Fifo_Pop_Flush;
-input Push_Clk,Pop_Clk;
+input Clk;
 input PUSH,POP;
-input [31:0] DIN;
-input Push_Clk_En,Pop_Clk_En,Fifo_Dir,Async_Flush;
-output [31:0] DOUT;
+input [15:0] DIN;
+input Clk_En,Fifo_Dir,Async_Flush;
+output [15:0] DOUT;
 output [3:0] PUSH_FLAG,POP_FLAG;
 output Almost_Full,Almost_Empty;
 
 parameter Concatenation_En = 1;
 
-parameter wr_depth_int0 = 512;
-parameter rd_depth_int0 = 512;
-parameter wr_width_int0 = 32;
-parameter rd_width_int0 = 32;
+parameter wr_depth_int0 = 1024;
+parameter rd_depth_int0 = 1024;
+parameter wr_width_int0 = 16;
+parameter rd_width_int0 = 16;
 parameter reg_rd_int0 = 0;
-parameter sync_fifo_int0 = 0;
+parameter sync_fifo_int0 = 1;
 
 
 FIFO_16K_BLK   #(Concatenation_En,
@@ -27,18 +27,18 @@ FIFO_INST      (.DIN0(DIN),
 				.POP0(POP),
 				.Fifo_Push_Flush0(Fifo_Push_Flush),
 				.Fifo_Pop_Flush0(Fifo_Pop_Flush),
-				.Push_Clk0(Push_Clk),
-				.Pop_Clk0(Pop_Clk),
+				.Push_Clk0(Clk),
+				.Pop_Clk0(Clk),
 				.PUSH_FLAG0(PUSH_FLAG),
 				.POP_FLAG0(POP_FLAG),
-				.Push_Clk0_En(Push_Clk_En),
-				.Pop_Clk0_En(Pop_Clk_En),
+				.Push_Clk0_En(Clk_En),
+				.Pop_Clk0_En(Clk_En),
 				.Fifo0_Dir(Fifo_Dir),
 				.Async_Flush0(Async_Flush),
 				.Almost_Full0(Almost_Full),
 				.Almost_Empty0(Almost_Empty),
 				.DOUT0(DOUT),
-		
+				
 				.LS(1'b0),
 				.SD(1'b0),
 				.DS(1'b0),
