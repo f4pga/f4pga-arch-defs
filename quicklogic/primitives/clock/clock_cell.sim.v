b2b13b41af185f28db2316d671e313c651ae9001
Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>
2020-06-30 03:33:05 +0530

    added specify block
    Signed-off-by: Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>

--- quicklogic/primitives/clock/clock_cell.sim.v
+++ quicklogic/primitives/clock/clock_cell.sim.v
@@ -1,3 +1,4 @@
+`timescale 1ns/10ps
 (* whitebox *)
 (* FASM_FEATURES="INTERFACE.ASSP.INV.ASSPInvPortAlias" *)
 module CLOCK_CELL(I_PAD, O_CLK);
@@ -8,6 +9,10 @@ module CLOCK_CELL(I_PAD, O_CLK);
     (* CLOCK=0 *)
     (* DELAY_CONST_I_PAD="{iopath_IP_IC}" *)
     output wire O_CLK;
+	
+	specify
+        (I_PAD=>O_CLK)="";
+    endspecify
 
     assign O_CLK = I_PAD;
 
