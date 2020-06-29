c72f6f401544766eb6396dbfa27d9199ad40ca67
Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>
2020-06-30 04:07:23 +0530

    added fpga_interconnect.sim.v file
    Signed-off-by: Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>

--- quicklogic/primitives/fpga_interconnect/fpga_interconnect.sim.v
+++ quicklogic/primitives/fpga_interconnect/fpga_interconnect.sim.v
@@ -1,1 +1,16 @@
+`timescale 1ns/10ps
+(* whitebox *)
+module fpga_interconnect(
+		datain,
+		dataout
+		);
+    input wire datain;
+    output wire dataout;
 
+    specify
+        (datain=>dataout)="";
+    endspecify
+
+    assign dataout = datain;
+
+endmodule
