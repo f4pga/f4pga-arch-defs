04229c1ba3382abe2ef806983e41181aa0b21ec8
Tomasz Michalak <tmichalak@antmicro.com>
2020-08-11 13:54:23 +0200

    zynqusp: Add missing files to make configuration finish
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xc/xcup/boards.cmake
+++ xc/xcup/boards.cmake
@@ -1,12 +1,12 @@
-get_target_property_required(OPENOCD env OPENOCD)
-get_target_property_required(OPENOCD_TARGET env OPENOCD_TARGET)
+#get_target_property_required(OPENOCD env OPENOCD)
+#get_target_property_required(OPENOCD_TARGET env OPENOCD_TARGET)
 
 add_xc_board(
   BOARD ultra96v2
   DEVICE xczu3eg
   PACKAGE test
-  PROG_TOOL ${OPENOCD_TARGET}
-  PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/avnet_ultra96v2.cfg & $<SEMICOLON> gdb --command=${PRJXRAY_DIR}/utils/openocd/load_script.gdb $<SEMICOLON> kill %1"
+  #PROG_TOOL ${OPENOCD_TARGET}
+  #PROG_CMD "${OPENOCD} -f ${PRJXRAY_DIR}/utils/openocd/avnet_ultra96v2.cfg & $<SEMICOLON> gdb --command=${PRJXRAY_DIR}/utils/openocd/load_script.gdb $<SEMICOLON> kill %1"
   PART xczu3eg-sbva484-1-e
 )
 
