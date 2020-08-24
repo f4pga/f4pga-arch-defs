17eb825dd56464295448c0f80cd100e942078e16
Tomasz Michalak <tmichalak@antmicro.com>
2020-08-24 15:20:00 +0200

    common: cmake: Integrate SDC plugin
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xc/xc7/yosys/synth.tcl
+++ xc/xc7/yosys/synth.tcl
@@ -4,6 +4,7 @@ plugin -i xdc
 plugin -i fasm
 plugin -i params
 plugin -i selection
+plugin -i sdc
 
 # Import the commands from the plugins to the tcl interpreter
 yosys -import
@@ -28,16 +29,22 @@ if { $::env(USE_ROI) == "TRUE" } {
     update_pll_params
 
     # Start flow after library reading
-    synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp -iopad -nowidelut -run prepare:check
+    synth_xilinx -vpr -flatten -abc9 -nosrl -nodsp -iopad -nowidelut -run prepare:check
 }
 
 # Check that post-synthesis cells match libraries.
 hierarchy -check
 
+# Read the SDC file
+read_sdc $::env(SDC_FILE)
+
 if { [info exists ::env(INPUT_XDC_FILE)] && $::env(INPUT_XDC_FILE) != "" } {
   read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
   write_fasm -part_json $::env(PART_JSON)  $::env(OUT_FASM_EXTRA)
 }
+
+# Perform clock propagation based on the information from the SDC commands
+propagate_clocks
 
 write_verilog $::env(OUT_SYNTH_V).premap.v
 
@@ -65,3 +72,5 @@ attrmap -remove hdlname
 write_json $::env(OUT_JSON)
 # Write the design in Verilog format.
 write_verilog $::env(OUT_SYNTH_V)
+#Write the SDC file
+write_sdc $::env(OUT_SDC)
