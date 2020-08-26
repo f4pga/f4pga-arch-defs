79200e34ba56aee1f0548451e4884e0306285d49
Andrew Butt <butta@seas.upenn.edu>
2020-08-26 16:53:57 -0400

    Updated doc example for issue
    Signed-off-by: Andrew Butt <butta@seas.upenn.edu>

--- xc/xc7/tests/switch_processing/switch_processing_arty_overlay.v
+++ xc/xc7/tests/switch_processing/switch_processing_arty_overlay.v
@@ -14,15 +14,25 @@ module top (
 );
     // 'data' buffers
     wire [3:0] inter;
-    genvar i;
-    generate
-        for (i=0; i < 8; i=i+1) begin
-            SYN_OBUF in_obuf_pr1(.I(sw[i]), .O(in_pr1[i]));
-            SYN_IBUF out_ibuf_pr1(.I(out_pr1[i]), .O(inter[i]));
-            SYN_OBUF in_obuf_pr2(.I(inter[i]), .O(in_pr2[i]));
-            SYN_IBUF out_ibuf2(.I(out_pr2[i]), .O(led[i]));
-        end
-    endgenerate
+    SYN_OBUF in_obuf_pr1_0(.I(sw[0]), .O(in_pr1[0]));
+    SYN_OBUF in_obuf_pr1_1(.I(sw[1]), .O(in_pr1[1]));
+    SYN_OBUF in_obuf_pr1_2(.I(sw[2]), .O(in_pr1[2]));
+    SYN_OBUF in_obuf_pr1_3(.I(sw[3]), .O(in_pr1[3]));
+
+    SYN_IBUF out_ibuf_pr1_0(.I(out_pr1[0]), .O(inter[0]));
+    SYN_IBUF out_ibuf_pr1_1(.I(out_pr1[1]), .O(inter[1]));
+    SYN_IBUF out_ibuf_pr1_2(.I(out_pr1[2]), .O(inter[2]));
+    SYN_IBUF out_ibuf_pr1_3(.I(out_pr1[3]), .O(inter[3]));
+
+    SYN_OBUF in_obuf_pr2_0(.I(inter[0]), .O(in_pr2[0]));
+    SYN_OBUF in_obuf_pr2_1(.I(inter[1]), .O(in_pr2[1]));
+    SYN_OBUF in_obuf_pr2_2(.I(inter[2]), .O(in_pr2[2]));
+    SYN_OBUF in_obuf_pr2_3(.I(inter[3]), .O(in_pr2[3]));
+
+    SYN_IBUF out_ibuf2_0(.I(out_pr2[0]), .O(led[0]));
+    SYN_IBUF out_ibuf2_1(.I(out_pr2[1]), .O(led[1]));
+    SYN_IBUF out_ibuf2_2(.I(out_pr2[2]), .O(led[2]));
+    SYN_IBUF out_ibuf2_3(.I(out_pr2[3]), .O(led[3]));
 
     // clock buffers
     IBUF clk_ibuf(.I(clk),      .O(clk_ibuf));
