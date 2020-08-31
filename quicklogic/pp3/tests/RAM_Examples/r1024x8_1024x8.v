37fc47355de0a1d1d6dad714f145de888f8bc2c4
Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>
2020-08-31 09:44:12 +0530

    Modified RAMs with new macros
    Signed-off-by: Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>

--- quicklogic/pp3/tests/RAM_Examples/r1024x8_1024x8.v
+++ quicklogic/pp3/tests/RAM_Examples/r1024x8_1024x8.v
@@ -1,24 +1,24 @@
-module r2048x8_2048x8 (WA,RA,WD,WClk,RClk,WClk_En,RClk_En,WEN,RD);
+module r1024x8_1024x8 (WA,RA,WD,WClk,RClk,WClk_En,RClk_En,WEN,RD);
 
-input [10:0] WA;
-input [10:0] RA;
+input [9:0] WA;
+input [9:0] RA;
 input WClk,RClk;
 input WClk_En,RClk_En;
 input WEN;
 input [7:0] WD;
 output [7:0] RD;
 
-parameter [16383:0] INIT = 16384'b0;
-parameter INIT_FILE="init_2048x8.hex";	
+parameter [8191:0] INIT = 8192'b0;
+parameter INIT_FILE="init_1024x8.hex";	
 
-parameter addr_int = 11 ;
-parameter data_depth_int = 2048;
+parameter addr_int = 10 ;
+parameter data_depth_int = 1024;
 parameter data_width_int = 8;
 parameter wr_enable_int = 1;
 parameter reg_rd_int = 0;
 
 
-RAM_16K_BLK #(.addr_int(addr_int),.data_depth_int(data_depth_int),.data_width_int(data_width_int),.wr_enable_int(wr_enable_int),.reg_rd_int(reg_rd_int),
+RAM_8K_BLK #( .addr_int(addr_int),.data_depth_int(data_depth_int),.data_width_int(data_width_int),.wr_enable_int(wr_enable_int),.reg_rd_int(reg_rd_int),
               .INIT(INIT),.INIT_FILE(INIT_FILE)
 			  )
 RAM_INST (	.WA(WA),
--- quicklogic/pp3/tests/RAM_Examples/r512x16_512x16.v
+++ quicklogic/pp3/tests/RAM_Examples/r512x16_512x16.v
@@ -1,7 +1,7 @@
-module r1024x16_1024x16 (WA,RA,WD,WClk,RClk,WClk_En,RClk_En,WEN,RD);
+module r512x16_512x16 (WA,RA,WD,WClk,RClk,WClk_En,RClk_En,WEN,RD);
 
-input [9:0] WA;
-input [9:0] RA;
+input [8:0] WA;
+input [8:0] RA;
 input WClk,RClk;
 input WClk_En,RClk_En;
 input [1:0] WEN;
@@ -9,16 +9,15 @@ input [15:0] WD;
 output [15:0] RD;
 
 parameter [16383:0] INIT = 16384'b0;
-parameter INIT_FILE="init_1024x16.hex";	
+parameter INIT_FILE="init_512x16.hex";	
 
-parameter addr_int = 10 ;
-parameter data_depth_int = 1024;
+parameter addr_int = 9 ;
+parameter data_depth_int = 512;
 parameter data_width_int = 16;
 parameter wr_enable_int = 2;
 parameter reg_rd_int = 0;
 
-
-RAM_16K_BLK #(.addr_int(addr_int),.data_depth_int(data_depth_int),.data_width_int(data_width_int),.wr_enable_int(wr_enable_int),.reg_rd_int(reg_rd_int),
+RAM_8K_BLK #(.addr_int(addr_int),.data_depth_int(data_depth_int),.data_width_int(data_width_int),.wr_enable_int(wr_enable_int),.reg_rd_int(reg_rd_int),
 			  .INIT(INIT),.INIT_FILE(INIT_FILE)
 			  )
 RAM_INST (	.WA(WA),
