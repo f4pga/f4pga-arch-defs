55ec45edd862c39d3af43e7807863611c1e372c6
Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>
2020-08-31 09:41:03 +0530

    Updated FIFOs with new FIFO macros
    Signed-off-by: Rakesh Moolacheri <54097448+rakeshm75@users.noreply.github.com>

--- quicklogic/pp3/tests/FIFO_Examples/f1024x16_1024x16.v
+++ quicklogic/pp3/tests/FIFO_Examples/f1024x16_1024x16.v
@@ -1,4 +1,4 @@
-module f1024x16_1024x16(DIN,Fifo_Push_Flush,Fifo_Pop_Flush,PUSH,POP,Clk,Clk_En,Fifo_Dir,Async_Flush,Almost_Full,Almost_Empty,PUSH_FLAG,POP_FLAG,DOUT);
+module f1024x16_1024x16 (DIN,Fifo_Push_Flush,Fifo_Pop_Flush,PUSH,POP,Clk,Clk_En,Fifo_Dir,Async_Flush,Almost_Full,Almost_Empty,PUSH_FLAG,POP_FLAG,DOUT);
 
 input Fifo_Push_Flush,Fifo_Pop_Flush;
 input Clk;
@@ -9,43 +9,30 @@ output [15:0] DOUT;
 output [3:0] PUSH_FLAG,POP_FLAG;
 output Almost_Full,Almost_Empty;
 
-parameter Concatenation_En = 1;
+parameter data_depth_int = 1024;
+parameter data_width_int = 16;
+parameter reg_rd_int = 0;
+parameter sync_fifo_int = 1;
 
-parameter wr_depth_int0 = 1024;
-parameter rd_depth_int0 = 1024;
-parameter wr_width_int0 = 16;
-parameter rd_width_int0 = 16;
-parameter reg_rd_int0 = 0;
-parameter sync_fifo_int0 = 1;
-
-
-FIFO_16K_BLK   #(Concatenation_En,
-				 wr_depth_int0,rd_depth_int0,wr_width_int0,rd_width_int0,reg_rd_int0,sync_fifo_int0
-				 ) 
-FIFO_INST      (.DIN0(DIN),
-				.PUSH0(PUSH),
-				.POP0(POP),
-				.Fifo_Push_Flush0(Fifo_Push_Flush),
-				.Fifo_Pop_Flush0(Fifo_Pop_Flush),
-				.Push_Clk0(Clk),
-				.Pop_Clk0(Clk),
-				.PUSH_FLAG0(PUSH_FLAG),
-				.POP_FLAG0(POP_FLAG),
-				.Push_Clk0_En(Clk_En),
-				.Pop_Clk0_En(Clk_En),
-				.Fifo0_Dir(Fifo_Dir),
-				.Async_Flush0(Async_Flush),
-				.Almost_Full0(Almost_Full),
-				.Almost_Empty0(Almost_Empty),
-				.DOUT0(DOUT),
+FIFO_16K_BLK  # (.data_depth_int(data_depth_int),.data_width_int(data_width_int),.reg_rd_int(reg_rd_int),.sync_fifo_int(sync_fifo_int)
+        				 ) 
+  FIFO_INST    (
+                .DIN(DIN),
+                .PUSH(PUSH),
+                .POP(POP),
+                .Fifo_Push_Flush(Fifo_Push_Flush),
+                .Fifo_Pop_Flush(Fifo_Pop_Flush),
+                .Push_Clk(Clk),
+                .Pop_Clk(Clk),
+                .PUSH_FLAG(PUSH_FLAG),
+                .POP_FLAG(POP_FLAG),
+                .Push_Clk_En(Clk_En),
+                .Pop_Clk_En(Clk_En),
+                .Fifo_Dir(Fifo_Dir),
+                .Async_Flush(Async_Flush),
+                .Almost_Full(Almost_Full),
+                .Almost_Empty(Almost_Empty),
+                .DOUT(DOUT)				
+                );
 				
-				.LS(1'b0),
-				.SD(1'b0),
-				.DS(1'b0),
-				.LS_RB1(1'b0),
-				.SD_RB1(1'b0),
-				.DS_RB1(1'b0)				
-				);
-
 endmodule
-
