9d0739e715dda9fa8eb493fb50f3a104b2292023
Tomasz Michalak <tmichalak@antmicro.com>
2020-09-08 13:05:27 +0200

    xc7: yosys: Change multiplier value for DUTY_CYCLE
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xc/xc7/yosys/utils.tcl
+++ xc/xc7/yosys/utils.tcl
@@ -2,10 +2,10 @@
 # Due to the fact that Yosys doesn't support floating parameter values
 # i.e. treats them as strings, the parameter values need to be multiplied by 1000
 # for the PLL registers to have correct values calculated during techmapping.
-proc update_param { param_name cell } {
+proc multiply_param { cell param_name multiplier } {
     set param_value [getparam $param_name $cell]
     if {$param_value ne ""} {
-	set new_param_value [expr int(round($param_value * 1000))]
+	set new_param_value [expr int(round($param_value * $multiplier))]
 	setparam -set $param_name $new_param_value $cell
 	puts "Updated parameter $param_name of cell $cell from $param_value to $new_param_value"
     }
@@ -14,8 +14,8 @@ proc update_param { param_name cell } {
 proc update_pll_params {} {
     foreach cell [selection_to_tcl_list "t:PLLE2_ADV"] {
 	for {set output 0} {$output < 6} {incr output} {
-	    update_param "CLKOUT${output}_PHASE" $cell
-	    update_param "CLKOUT${output}_DUTY_CYCLE" $cell
+	    multiply_param $cell "CLKOUT${output}_PHASE" 1000
+	    multiply_param $cell "CLKOUT${output}_DUTY_CYCLE" 100000
 	}
     }
 }
