e61861c6fca89f56793b2428294c5c0a84cc77ea
Maciej Kurc <mkurc@antmicro.com>
2020-09-29 12:18:55 +0200

    Isolated common code for *_import_tile_capacity.py
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- xc/common/utils/prjuray_import_tile_capacity.py
+++ xc/common/utils/prjuray_import_tile_capacity.py
@@ -1,1 +1,52 @@
 #!/usr/bin/env python3
+import argparse
+import json
+
+import prjuray.db
+from prjuray.site_type import SitePinDirection
+
+from import_tile_capacity import import_tile_capacity
+
+
+def get_wires(site, site_type):
+    """Get wires related to a site"""
+    input_wires = set()
+    output_wires = set()
+
+    for site_pin in site.site_pins:
+        if site_type.get_site_pin(
+                site_pin.name).direction == SitePinDirection.IN:
+            input_wires.add(site_pin.wire)
+        elif site_type.get_site_pin(
+                site_pin.name).direction == SitePinDirection.OUT:
+            output_wires.add(site_pin.wire)
+        else:
+            assert False, site_pin
+
+    return input_wires, output_wires
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument('--db_root', required=True)
+    parser.add_argument('--part', required=True)
+    parser.add_argument('--output_directory', required=True)
+    parser.add_argument('--site_directory', required=True)
+    parser.add_argument('--tile_type', required=True)
+    parser.add_argument('--pb_types', required=True)
+    parser.add_argument('--pin_assignments', required=True)
+
+    args = parser.parse_args()
+
+    with open(args.pin_assignments) as f:
+        pin_assignments = json.load(f)
+
+    db = prjuray.db.Database(args.db_root, args.part)
+    tile_type = db.get_tile_type(args.tile_type)
+
+    import_tile_capacity(args, db, pin_assignments, get_wires)
+
+
+if __name__ == "__main__":
+    main()
--- xc/common/utils/prjxray_import_tile_capacity.py
+++ xc/common/utils/prjxray_import_tile_capacity.py
@@ -1,10 +1,11 @@
-""" Generates project xray. """
+#!/usr/bin/env python3
 import argparse
 import json
+
 import prjxray.db
 from prjxray.site_type import SitePinDirection
-from lib.pb_type_xml import start_heterogeneous_tile, add_switchblock_locations
-import lxml.etree as ET
+
+from import_tile_capacity import import_tile_capacity
 
 
 def get_wires(site, site_type):
@@ -44,46 +45,7 @@ def main():
     db = prjxray.db.Database(args.db_root, args.part)
     tile_type = db.get_tile_type(args.tile_type)
 
-    sites = {}
-
-    pb_types = args.pb_types.split(',')
-
-    equivalent_sites_dict = dict()
-    for pb_type in pb_types:
-        try:
-            site, equivalent_sites = pb_type.split("/")
-        except ValueError:
-            site = pb_type
-            equivalent_sites = None
-
-        sites[site] = []
-
-        equivalent_sites_dict[site] = equivalent_sites.split(
-            ':'
-        ) if equivalent_sites else []
-
-    for site in tile_type.get_sites():
-        if site.type not in sites.keys():
-            continue
-
-        site_type = db.get_site_type(site.type)
-        input_wires, output_wires = get_wires(site, site_type)
-
-        sites[site.type].append((site, input_wires, output_wires))
-
-    tile_xml = start_heterogeneous_tile(
-        args.tile_type,
-        pin_assignments,
-        sites,
-        equivalent_sites_dict,
-    )
-
-    add_switchblock_locations(tile_xml)
-
-    with open('{}/{}.tile.xml'.format(args.output_directory,
-                                      args.tile_type.lower()), 'w') as f:
-        tile_str = ET.tostring(tile_xml, pretty_print=True).decode('utf-8')
-        f.write(tile_str)
+    import_tile_capacity(args, db, pin_assignments, get_wires)
 
 
 if __name__ == "__main__":
