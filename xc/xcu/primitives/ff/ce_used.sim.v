fb7733b3ee89fed9ef54f1a94086d40049924057
Alessandro Comodi <acomodi@antmicro.com>
2020-10-01 12:56:51 +0200

    uray: slice: add fake CE/SR used timing
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc/xcu/primitives/ff/ce_used.sim.v
+++ xc/xcu/primitives/ff/ce_used.sim.v
@@ -1,6 +1,8 @@
 (* whitebox *)
 module CE_USED(CE, CE_OUT);
     input wire CE;
+
+    (* DELAY_CONST_CE="1e-10" *)
     output wire CE_OUT;
 
     assign CE_OUT = CE;
--- xc/xcu/primitives/ff/sr_used.sim.v
+++ xc/xcu/primitives/ff/sr_used.sim.v
@@ -1,6 +1,8 @@
 (* whitebox *)
 module SR_USED(SR, SR_OUT);
     input wire SR;
+
+    (* DELAY_CONST_SR="1e-10" *)
     output wire SR_OUT;
 
     assign SR_OUT = SR;
