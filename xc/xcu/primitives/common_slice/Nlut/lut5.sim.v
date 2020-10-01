fdb5fb99c36a2188f75a1d953d968c839094eb4a
Alessandro Comodi <acomodi@antmicro.com>
2020-10-01 12:56:29 +0200

    uray: slice: luts: changed to fracturable lut5
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ xc/xcu/primitives/common_slice/Nlut/lut5.sim.v
@@ -0,0 +1,8 @@
+(* CLASS="lut" *)
+(* whitebox *)
+module LUT5 (in, out);
+    (* PORT_CLASS="lut_in" *)
+    input wire [4:0] in;
+    (* PORT_CLASS="lut_out" *)
+    output wire out;
+endmodule
--- xc/xcu/primitives/common_slice/Nlut/ntemplate.Nlut.sim.v
+++ xc/xcu/primitives/common_slice/Nlut/ntemplate.Nlut.sim.v
@@ -1,15 +1,16 @@
+`include "lut5.sim.v"
+`include "f6mux.sim.v"
+
 // D6LUT, C6LUT, B6LUT, A6LUT == W6LUT
 // A fracturable 6 input LUT. Can either be;
 //  - 2 * 5 input, 1 output LUT
 //  - 1 * 6 input, 1 output LUT
-module {N}LUT(A1, A2, A3, A4, A5, A6, O6, O5);
+(* FASM_TYPE_2LUT5="SPLIT_LUT" *)
+(* FASM_LUT_2LUT5="{N}LUT.INIT[31:0]={N}5LUT[0];{N}LUT.INIT[63:32]={N}5LUT[1]" *)
+(* MODES="2LUT5" *)
+module {N}LUT(A, O6, O5);
 
-	input wire A1;
-	input wire A2;
-	input wire A3;
-	input wire A4;
-	input wire A5;
-	input wire A6;
+	input wire [5:0] A;
 
 	(* DELAY_CONST_A1 = "1e-10" *)
 	(* DELAY_CONST_A2 = "1e-10" *)
@@ -24,25 +25,23 @@ module {N}LUT(A1, A2, A3, A4, A5, A6, O6, O5);
 	(* DELAY_CONST_A3 = "1e-10" *)
 	(* DELAY_CONST_A4 = "1e-10" *)
 	(* DELAY_CONST_A5 = "1e-10" *)
-	(* DELAY_CONST_A6 = "1e-10" *)
 	output wire O5;
 
-	parameter [63:0] INIT = 0;
-	// LUT5 (upper)
-	wire [15: 0] upper_s4 = A5 ?       INIT[63:48] :     INIT[47:32];
-	wire [ 7: 0] upper_s3 = A4 ?   upper_s4[15: 8] : upper_s4[ 7: 0];
-	wire [ 3: 0] upper_s2 = A3 ?   upper_s3[ 7: 4] : upper_s3[ 3: 0];
-	wire [ 1: 0] upper_s1 = A2 ?   upper_s2[ 3: 2] : upper_s2[ 1: 0];
-	wire         upper_O  = A1 ?   upper_s1[    1] : upper_s1[    0];
+	parameter MODE = "";
+	if (MODE == "2LUT5") begin
+		wire [1:0] LUT5_OUT;
+		genvar i;
+		generate
+			for (i = 0; i < 2; i = i+1) begin
+				LUT5 {N}LUT5(.in(A[4:0]), .out(LUT5_OUT[i]));
+			end
+		endgenerate
 
-	// LUT5 (lower)
-	wire [15: 0] lower_s4 = A5 ?       INIT[31:16] :     INIT[15: 0];
-	wire [ 7: 0] lower_s3 = A4 ?   lower_s4[15: 8] : lower_s4[ 7: 0];
-	wire [ 3: 0] lower_s2 = A3 ?   lower_s3[ 7: 4] : lower_s3[ 3: 0];
-	wire [ 1: 0] lower_s1 = A2 ?   lower_s2[ 3: 2] : lower_s2[ 1: 0];
-	wire         lower_O  = A1 ?   lower_s1[    1] : lower_s1[    0];
-	assign O5 = lower_O;
+		wire F6MUX_O;
+		F6MUX F6MUX(.I0(LUT5_OUT[0]), .I0(LUT5_OUT[1]), .S(A[5]), .O(F6MUX_O));
 
-	// MUXF6
-	assign O6 = A6 ? upper_O : lower_O;
+		assign O5 = LUT5_OUT[0];
+		assign O6 = F6MUX_O;
+	end
+
 endmodule
