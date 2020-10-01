1b3ff86cb0e2243f47fb4ce7720cb10a8f58d8a9
Alessandro Comodi <acomodi@antmicro.com>
2020-10-01 14:53:50 +0200

    uray: slice: lut: add max/min fake delay matrices
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc/xcu/primitives/common_slice/Nlut/lut5.sim.v
+++ xc/xcu/primitives/common_slice/Nlut/lut5.sim.v
@@ -4,5 +4,7 @@ module LUT5 (in, out);
     (* PORT_CLASS="lut_in" *)
     input wire [4:0] in;
     (* PORT_CLASS="lut_out" *)
+    (* DELAY_MATRIX_MAX_in="1e-10;1e-10;1e-10;1e-10;1e-10" *)
+    (* DELAY_MATRIX_MIN_in="1e-12;1e-12;1e-12;1e-12;1e-12" *)
     output wire out;
 endmodule
