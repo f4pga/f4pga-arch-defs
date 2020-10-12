55d4cf2830b8233336e00aa9ddeebe72a2fbb769
Alessandro Comodi <acomodi@antmicro.com>
2020-10-12 20:14:49 +0200

    run make format and added docstrings
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- xc/xc7/tests/soc/litex/fixup_xdc.py
+++ xc/xc7/tests/soc/litex/fixup_xdc.py
@@ -1,7 +1,25 @@
 import argparse
+"""
+Currently, litex outputs XDC constraints in which the create_clock commands
+cannot be correctly parsed yet by the XDC yosys plugin.
+
+Example of failing XDC command:
+    create_clock -name clk100 -period 10.0 [get_nets clk100]
+
+Example of working XDC command:
+    create_clock -period 10.0 clk100
+
+This script fixes the generated XDC and translates the failing commands
+into the working ones.
+
+This script is a temporary workaround and needs to be avoided.
+"""
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Fixup script to modify the XDC output of LiteX")
+    parser = argparse.ArgumentParser(
+        description="Fixup script to modify the XDC output of LiteX"
+    )
     parser.add_argument("--xdc", required=True)
 
     args = parser.parse_args()
@@ -21,7 +39,9 @@ def main():
 
                     # Old line: create_clock -name clk100 -period 10.0 [get_nets clk100]
                     # New line: create_clock -period 10.0 clk100
-                    new_line = " ".join([groups[0], groups[3], groups[4], groups[2], '\n'])
+                    new_line = " ".join(
+                        [groups[0], groups[3], groups[4], groups[2], '\n']
+                    )
 
                     lines_to_add.append(new_line)
             else:
@@ -30,6 +50,7 @@ def main():
     with open(args.xdc, "w") as xdc:
         for line in lines_to_add:
             xdc.write(line)
+
 
 if __name__ == "__main__":
     main()
