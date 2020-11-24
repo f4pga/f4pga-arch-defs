0bd830a5d9919b69c17c439ccff8d63fdfe3a67d
Alessandro Comodi <acomodi@antmicro.com>
2020-11-24 14:52:05 +0100

    check-status: move stdm library call within the check-status script
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/check-status/check-status.py
+++ .github/check-status/check-status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from os import environ
+from os import environ, path
 from github import Github
+from stdm import get_latest_artifact_url
 
 gh_ref = environ['GITHUB_REPOSITORY']
 gh_sha = environ['INPUT_SHA']
@@ -17,3 +18,19 @@ for item in status.statuses:
 if status.state != 'success':
     print('Status not successful. Skipping...')
     exit(1)
+
+artifacts, _ = get_latest_artifact_url()
+
+for artifact in artifacts:
+    name = artifact["name"].split(".")[0]
+    url = artifact["url"]
+
+    if name.startswith("symbiflow-arch-defs-install"):
+        file_name = "symbiflow-toolchain-latest"
+    elif name.startswith("symbiflow-arch-defs-benchmarks"):
+        file_name = "symbiflow-benchmarks-latest"
+    else:
+        file_name = name + "-latest"
+
+    with open(path.join("install", file_name), "w") as f:
+        f.write(url)
