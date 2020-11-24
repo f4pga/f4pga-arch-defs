1b062074cd63bf0f002fe9c8d60cc9c272a63bc2
Alessandro Comodi <acomodi@antmicro.com>
2020-11-24 14:52:05 +0100

    check-status: move stdm library call within the check-status script
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/check-status/entrypoint.sh
+++ .github/check-status/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-/check-status.py && mkdir install && symbiflow_get_latest_artifact_url > install/latest || echo '::set-output name=skip::true'
+mkdir install && /check-status.py || echo '::set-output name=skip::true'
