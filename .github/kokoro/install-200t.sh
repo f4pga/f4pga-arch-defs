96743442d8f357392df7406db0928f0b04915134
Alessandro Comodi <acomodi@antmicro.com>
2020-12-01 19:38:05 +0100

    kokoro: disable install-200t
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/kokoro/install-200t.sh
+++ .github/kokoro/install-200t.sh
@@ -11,48 +11,12 @@ export CMAKE_FLAGS="-GNinja -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DINSTALL_DEVI
 export BUILD_TOOL=ninja
 source ${SCRIPT_DIR}/common.sh
 
+# This CI was executing the same exact script
+# as the XC7 - Install CI, therefore it is now
+# set to do nothing.
+# TODO: Remove this CI upstream so it does not run at all
 echo
 echo "========================================"
-echo "Running install tests (make install)"
-echo "----------------------------------------"
-(
-	source env/conda/bin/activate symbiflow_arch_def_base
-	pushd build
-	export VPR_NUM_WORKERS=${CORES}
-	ninja -j${MAX_CORES} install
-	popd
-	cp environment.yml install/
-)
-echo "----------------------------------------"
-
-echo
+echo "Doing nothing!"
 echo "========================================"
-echo "Running installed toolchain tests"
-echo "----------------------------------------"
-(
-	source env/conda/bin/activate symbiflow_arch_def_base
-	pushd build
-	export VPR_NUM_WORKERS=${CORES}
-	export CTEST_OUTPUT_ON_FAILURE=1
-	ctest -R binary_toolchain_test_50t -j${MAX_CORES}
-	ctest -R binary_toolchain_test_100t -j${MAX_CORES}
-	ctest -R binary_toolchain_test_200t -j${MAX_CORES}
-	popd
-)
-echo "----------------------------------------"
-
 echo
-echo "========================================"
-echo "Compressing and uploading install dir"
-echo "----------------------------------------"
-(
-	rm -rf build
-	du -ah install
-	export GIT_HASH=$(git rev-parse --short HEAD)
-	tar cfJv symbiflow-arch-defs-install-${GIT_HASH}.tar.xz -C install bin share/symbiflow/techmaps share/symbiflow/scripts environment.yml
-	for device in $(ls install/share/symbiflow/arch)
-	do
-		tar cfJv symbiflow-$device.tar.xz -C install share/symbiflow/arch/$device
-	done
-)
-echo "----------------------------------------"
