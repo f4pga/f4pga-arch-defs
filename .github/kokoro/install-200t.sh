4f157a578de8621b9455fba6aeac8e60335a6af4
Alessandro Comodi <acomodi@antmicro.com>
2020-12-01 15:20:34 +0100

    install: do not pushd into install dir
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/kokoro/install-200t.sh
+++ .github/kokoro/install-200t.sh
@@ -48,10 +48,8 @@ echo "----------------------------------------"
 (
 	rm -rf build
 	du -ah install
-	pushd install
 	export GIT_HASH=$(git rev-parse --short HEAD)
 	tar cfJv symbiflow-arch-defs-install-200t-${GIT_HASH}.tar.xz -C install bin share environment.yml
 	tar cfJv symbiflow-arch-defs-benchmarks-200t-${GIT_HASH}.tar.xz -C install benchmarks
-	popd
 )
 echo "----------------------------------------"
