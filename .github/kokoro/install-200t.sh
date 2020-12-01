431f864cb3d78ab23d8e91c530346ba3d0dca748
Alessandro Comodi <acomodi@antmicro.com>
2020-12-01 19:38:05 +0100

    kokor: disable install-200t
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/kokoro/install-200t.sh
+++ .github/kokoro/install-200t.sh
@@ -11,46 +11,12 @@ export CMAKE_FLAGS="-GNinja -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DINSTALL_DEVI
 export BUILD_TOOL=ninja
 source ${SCRIPT_DIR}/common.sh
 
+# This CI was executing the same exact script
+# as the XC7 - Install CI, therefore it is now
+# set to do nothing.
+# TODO: Remove this CI upstream so it does not run at all
 echo
 echo "========================================"
-echo "Running install tests (make install)"
-echo "----------------------------------------"
-(
-	source env/conda/bin/activate symbiflow_arch_def_base
-	pushd build
-	export VPR_NUM_WORKERS=${CORES}
-	ninja -j${MAX_CORES} install
-	popd
-	cp environment.yml install/
-)
-echo "----------------------------------------"
-
-echo
+echo "Doing nothing!"
 echo "========================================"
-echo "Running installed toolchain tests"
-echo "----------------------------------------"
-(
-	source env/conda/bin/activate symbiflow_arch_def_base
-	pushd build
-	export VPR_NUM_WORKERS=${CORES}
-	export CTEST_OUTPUT_ON_FAILURE=1
-	ctest -R binary_toolchain_test_50t -j${MAX_CORES}
-	ctest -R binary_toolchain_test_100t -j${MAX_CORES}
-	ctest -R binary_toolchain_test_200t -j${MAX_CORES}
-	popd
-)
-echo "----------------------------------------"
-
 echo
-echo "========================================"
-echo "Compressing and uploading install dir"
-echo "----------------------------------------"
-(
-	rm -rf build
-	du -ah install
-	pushd install
-	export GIT_HASH=$(git rev-parse --short HEAD)
-	tar vcf - * | xz -9 -T${MAX_CORES} - > ../symbiflow-arch-defs-install-200t-${GIT_HASH}.tar.xz
-	popd
-)
-echo "----------------------------------------"
