050e75fd234fd2d1a6f03b77f2fdf50b19942457
Maciej Kurc <mkurc@antmicro.com>
2021-02-17 11:38:54 +0100

    Removed files non-relevant to QuickLogic OpenFPGA k4_N8 devices
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/openfpga/boards.cmake
+++ quicklogic/openfpga/boards.cmake
@@ -1,12 +1,4 @@
 add_quicklogic_board(
-  BOARD   ql-openfpga-k4_N4_board
-  FAMILY  openfpga
-  DEVICE  ql-openfpga-k4_N4
-  PACKAGE ql-openfpga-k4_N4
-  FABRIC_PACKAGE ql-openfpga-k4_N4
-)
-
-add_quicklogic_board(
   BOARD   ql-openfpga-k4_N8_board
   FAMILY  openfpga
   DEVICE  ql-openfpga-k4_N8
