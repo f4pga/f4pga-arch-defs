59219297886695ff668aa05d5d2ef87afb8bc9a3
Maciej Kurc <mkurc@antmicro.com>
2021-02-23 09:56:50 +0100

    Removed the QuickLogic test suite and the unneeded dummy.eblif file from this repository.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/openfpga/tests/quicklogic_testsuite/camif/mux2x0.v
+++
@@ -1,9 +0,0 @@
-module mux2x0_top (A,B,S,Q); 
-input A,B,S;
-output Q;
-
-assign Q= (S)? A: B;
-
-endmodule
-
-
--- quicklogic/openfpga/tests/quicklogic_testsuite/multiplier_8bit/multiplier_8bit.v
+++
@@ -1,8 +0,0 @@
-module multiplier_8bit (prod,a_in,b_in);
-
-output [15:0] prod;
-input [7:0] a_in, b_in;
-
-assign prod = a_in * b_in;
-
-endmodule 
