8774cb6be663c64579f47759098a64447e87a3df
Maciej Kurc <mkurc@antmicro.com>
2021-03-19 12:11:45 +0100

    Added comments to qlf_k4n8 cells_sim.v and cells_map.v
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- quicklogic/qlf_k4n8/techmap/cells_map.v
+++ quicklogic/qlf_k4n8/techmap/cells_map.v
@@ -1,1 +1,5 @@
 // Yosys techmap library that adapts the design for the VPR architecture.
+
+// Currently there is one-to-one relationship between cells emmited by synthesis tools (Yosys) and those expected by VPR.
+// There is no need for VPR specific technology mapping hence for now this file remains empty.
+
