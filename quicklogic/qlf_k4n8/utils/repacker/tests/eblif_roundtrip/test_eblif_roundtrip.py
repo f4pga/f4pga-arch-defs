ce73e11dfcd16c11459b09531a79e5e6d228a8fc
Maciej Kurc <mkurc@antmicro.com>
2021-03-26 16:34:23 +0100

    Added tests for the repacker code parts
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

---
+++ quicklogic/qlf_k4n8/utils/repacker/tests/eblif_roundtrip/test_eblif_roundtrip.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import sys
+import tempfile
+
+import pytest
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from eblif_netlist import Eblif
+
+# =============================================================================
+
+def test_netlist_roundtrip():
+
+    basedir = os.path.dirname(__file__)
+    golden_file = os.path.join(basedir, "netlist.golden.eblif")
+
+    # Load and parse the EBLIF file
+    eblif = Eblif.from_file(golden_file)
+
+    with tempfile.TemporaryDirectory() as tempdir:
+
+        # Write the EBLIF back
+        output_file = os.path.join(tempdir, "netlist.output.eblif")
+        eblif.to_file(output_file)
+
+        # Compare the two files
+        with open(golden_file, "r") as fp:
+            golden_data = fp.read().rstrip()
+        with open(output_file, "r") as fp:
+            output_data = fp.read().rstrip()
+
+        assert golden_data == output_data
+
