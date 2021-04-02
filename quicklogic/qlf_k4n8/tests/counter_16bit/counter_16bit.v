30b6a715e039c1c0c71ce399ac670741748c45ff
Alessandro Comodi <acomodi@antmicro.com>
2021-04-02 15:44:16 +0200

    quicklogic: add some basic regression tests
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- quicklogic/qlf_k4n8/tests/counter_16bit/counter_16bit.v
+++ quicklogic/qlf_k4n8/tests/counter_16bit/counter_16bit.v
@@ -1,9 +1,12 @@
+//
+// Copyright (c) 2020 QuickLogic Corporation.  All Rights Reserved.
+//
 // Description :
 //    Example of asimple 16 bit up counter in Verilog HDL
 //
 // Version 1.0 : Initial Creation
 //
-module counter_16bit_top (clk, reset, enable, count);
+module top (clk, reset, enable, count);
 input clk, reset, enable;
 output [15:0] count;
 reg [15:0] count;                                   
