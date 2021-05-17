f9c9dc9ba24f59862c346bf98622fbe8973ce257
Alessandro Comodi <acomodi@antmicro.com>
2021-05-17 14:54:45 +0200

    quicklogic: utils: fix repacker
    This commit fixes the repacker to do the following:
    
    - allow using top IO ports for clock constraints
    - LUT initialization padding and bit ordering
    - fixed handling of repacker constraints
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>
    Co-authored-by: Maciej Kurc <mkurc@antmicro.com>
    Co-authored-by: Alessandro Comodi <acomodi@antmicro.com>

--- quicklogic/qlf_k4n8/utils/repacker/netlist_cleaning.py
+++ quicklogic/qlf_k4n8/utils/repacker/netlist_cleaning.py
@@ -8,7 +8,7 @@ import logging
 # =============================================================================
 
 
-def absorb_buffer_luts(netlist):
+def absorb_buffer_luts(netlist, outputs=False):
     """
     Performs downstream absorbtion of buffer LUTs. All buffer LUTs are absorbed
     except for those that drive top-level output ports not to change output
@@ -29,11 +29,17 @@ def absorb_buffer_luts(netlist):
         # A pass-through LUT
         if cell.type == "$lut" and cell.init == [0, 1]:
 
+            # Get input and output nets
+            assert INP_PORT in cell.ports, cell
+            net_inp = cell.ports[INP_PORT]
+
             assert OUT_PORT in cell.ports, cell
             net_out = cell.ports[OUT_PORT]
 
-            # Must not be driving any top-level outputs
-            if net_out in netlist.outputs:
+            # Must not be driving any top-level outputs unless explicitly
+            # allowed
+            if (net_inp in netlist.inputs
+                    or net_out in netlist.outputs) and not outputs:
                 return False
 
             return True
@@ -58,18 +64,42 @@ def absorb_buffer_luts(netlist):
         assert OUT_PORT in cell.ports, cell
         net_out = cell.ports[OUT_PORT]
 
-        # Replace the output net in all cells with the input one
-        for c in netlist.cells.values():
-            for port, net in c.ports.items():
-                if net == net_out:
-                    c.ports[port] = net_inp
+        # Cannot be a buffer connecting an input to an output directly
+        if net_out in netlist.outputs and net_inp in netlist.inputs:
+            continue
 
-        # Update net map
-        for net in net_map:
-            if net_map[net] == net_out:
-                net_map[net] = net_inp
+        # This cell drives a top-level output directly. Change input nets and
+        # leave the output one
+        if net_out in netlist.outputs:
 
-        net_map[net_out] = net_inp
+            # Replace the output net in all cells with the input one
+            for c in netlist.cells.values():
+                for port, net in c.ports.items():
+                    if net == net_inp:
+                        c.ports[port] = net_out
+
+            # Update net map
+            for net in net_map:
+                if net_map[net] == net_inp:
+                    net_map[net] = net_out
+
+            net_map[net_inp] = net_out
+
+        # A regular buffer
+        else:
+
+            # Replace the output net in all cells with the input one
+            for c in netlist.cells.values():
+                for port, net in c.ports.items():
+                    if net == net_out:
+                        c.ports[port] = net_inp
+
+            # Update net map
+            for net in net_map:
+                if net_map[net] == net_out:
+                    net_map[net] = net_inp
+
+            net_map[net_out] = net_inp
 
         # Remove the cell
         del netlist.cells[cell.name]
