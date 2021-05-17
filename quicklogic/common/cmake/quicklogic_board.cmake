33b6df01ba052bd40ebdfc4467376bc6e768ea82
Alessandro Comodi <acomodi@antmicro.com>
2021-05-17 16:29:41 +0200

    quicklogic: add slow and fast corners architectures
    This commit adds two different devices and boards for two different
    corners.
    
    The CMake infrastructure does not allow for a same device to use different arch
    XML and RR graphs (that have different corner data).
    
    The multiple corner testing can be properly added to the installation
    package testing suite (which still needs to be developed) so that we
    can allow and test usage of different corners for the same device during
    different parts of the flow (e.g. SLOW corner for PnR and FAST corner
    for analysis)
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- quicklogic/common/cmake/quicklogic_board.cmake
+++ quicklogic/common/cmake/quicklogic_board.cmake
@@ -41,13 +41,13 @@ function(ADD_QUICKLOGIC_BOARD)
     get_target_property_required(VPR_DB_FILE ${DEVICE_TYPE} VPR_DB_FILE)
     get_file_location(VPR_DB_FILE_LOC ${VPR_DB_FILE})
     get_file_target(VPR_DB_TARGET ${VPR_DB_FILE})
-  
+
     # Generate clock pad map CSV file
     set(CREATE_CLKMAP_CSV ${symbiflow-arch-defs_SOURCE_DIR}/quicklogic/common/utils/create_clkmap_csv.py)
     set(CLKMAP_CSV ${BOARD}_clkmap.csv)
     set(CLKMAP_CSV_DEPS ${PYTHON3} ${PYTHON3_TARGET} ${CREATE_CLKMAP_CSV})
     append_file_dependency(CLKMAP_CSV_DEPS ${VPR_DB_FILE})
-  
+
     add_custom_command(
       OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${CLKMAP_CSV}
       COMMAND ${PYTHON3} ${CREATE_CLKMAP_CSV}
@@ -55,19 +55,19 @@ function(ADD_QUICKLOGIC_BOARD)
         --db ${VPR_DB_FILE_LOC}
       DEPENDS ${CLKMAP_CSV_DEPS}
     )
-  
+
     add_file_target(FILE ${CLKMAP_CSV} GENERATED)
-  
+
     # Generate pinmap CSV file
     set(CREATE_PINMAP_CSV ${symbiflow-arch-defs_SOURCE_DIR}/quicklogic/common/utils/create_pinmap_csv.py)
     set(PINMAP_CSV ${BOARD}_pinmap.csv)
     set(PINMAP_CSV_DEPS ${PYTHON3} ${PYTHON3_TARGET} ${CREATE_PINMAP_CSV})
     append_file_dependency(PINMAP_CSV_DEPS ${VPR_DB_FILE})
-  
+
     # Make the pinmap depend on clkmap. This way it is build without the need for
     # adding the dependency elsewhere.
     append_file_dependency(PINMAP_CSV_DEPS ${CLKMAP_CSV})
-  
+
     # TODO: Use the PACKAGE in the pinmap CSV generation.
     add_custom_command(
       OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_CSV}
@@ -77,9 +77,9 @@ function(ADD_QUICKLOGIC_BOARD)
         --db ${VPR_DB_FILE_LOC}
       DEPENDS ${PINMAP_CSV_DEPS}
     )
-  
+
     add_file_target(FILE ${PINMAP_CSV} GENERATED)
-  
+
     # Set the board properties
     set_target_properties(
       ${BOARD}
@@ -89,7 +89,7 @@ function(ADD_QUICKLOGIC_BOARD)
         CLKMAP
         ${CMAKE_CURRENT_SOURCE_DIR}/${CLKMAP_CSV}
     )
-  
+
     set_target_properties(
       dummy_${ARCH}_${DEVICE}_${PACKAGE}
       PROPERTIES
@@ -145,11 +145,11 @@ function(ADD_QUICKLOGIC_BOARD)
     set(PINMAP_CSV ${BOARD}_pinmap.csv)
     set(PINMAP_CSV_DEPS ${PYTHON3} ${PYTHON3_TARGET} ${CREATE_PINMAP_CSV} ${TECHFILE})
     append_file_dependency(PINMAP_CSV_DEPS ${RR_GRAPH_FILE})
-  
+
     # Make the pinmap depend on clkmap. This way it is build without the need for
     # adding the dependency elsewhere.
     append_file_dependency(PINMAP_CSV_DEPS ${CLKMAP_CSV})
-  
+
     # TODO: Use the PACKAGE in the pinmap CSV generation.
     add_custom_command(
       OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_CSV}
@@ -160,7 +160,7 @@ function(ADD_QUICKLOGIC_BOARD)
         --vpr-grid-map ${VPR_GRID_MAP_LOCATION}
       DEPENDS ${PINMAP_CSV_DEPS}
     )
-  
+
     add_file_target(FILE ${PINMAP_CSV} GENERATED)
 
     # Set the board properties
@@ -172,7 +172,7 @@ function(ADD_QUICKLOGIC_BOARD)
         CLKMAP
         ${CMAKE_CURRENT_SOURCE_DIR}/${CLKMAP_CSV}
     )
-  
+
     set_target_properties(
       dummy_${ARCH}_${DEVICE}_${PACKAGE}
       PROPERTIES
@@ -193,33 +193,41 @@ function(ADD_QUICKLOGIC_BOARD)
     set(PINMAP_CSV ${PINMAP})
 
     # Copy the pinmap XML
-    add_custom_command(
-      OUTPUT
-        ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_XML}
-      COMMAND
-        ${CMAKE_COMMAND} -E copy
-          ${CMAKE_CURRENT_SOURCE_DIR}/devices/umc22/${PINMAP_XML}
+    get_file_target(PINMAP_XML_TARGET ${PINMAP_XML})
+
+    if(NOT TARGET ${PINMAP_XML_TARGET})
+      add_custom_command(
+        OUTPUT
           ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_XML}
-    )
-    add_file_target(FILE ${PINMAP_XML} GENERATED)
+        COMMAND
+          ${CMAKE_COMMAND} -E copy
+            ${CMAKE_CURRENT_SOURCE_DIR}/devices/umc22/${PINMAP_XML}
+            ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_XML}
+      )
+      add_file_target(FILE ${PINMAP_XML} GENERATED)
+    endif()
 
     # Make the pinmap CSV depend on pinmap XML. This way the file will be
     # created  without the need for adding the dependency elsewhere.
     set(PINMAP_CSV_DEPS)
     append_file_dependency(PINMAP_CSV_DEPS ${PINMAP_XML})
 
-    # Copy the pinmap CSV
-    add_custom_command(
-      OUTPUT
-        ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_CSV}
-      COMMAND
-        ${CMAKE_COMMAND} -E copy
-          ${CMAKE_CURRENT_SOURCE_DIR}/devices/umc22/${PINMAP_CSV}
+    get_file_target(PINMAP_CSV_TARGET ${PINMAP_CSV})
+
+    if(NOT TARGET ${PINMAP_CSV_TARGET})
+      # Copy the pinmap CSV
+      add_custom_command(
+        OUTPUT
           ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_CSV}
-      DEPENDS
-        ${PINMAP_CSV_DEPS}
-    )
-    add_file_target(FILE ${PINMAP_CSV} GENERATED)
+        COMMAND
+          ${CMAKE_COMMAND} -E copy
+            ${CMAKE_CURRENT_SOURCE_DIR}/devices/umc22/${PINMAP_CSV}
+            ${CMAKE_CURRENT_BINARY_DIR}/${PINMAP_CSV}
+        DEPENDS
+          ${PINMAP_CSV_DEPS}
+      )
+      add_file_target(FILE ${PINMAP_CSV} GENERATED)
+    endif()
 
 
     # Set the board properties
--- quicklogic/qlf_k4n8/boards.cmake
+++ quicklogic/qlf_k4n8/boards.cmake
@@ -1,9 +1,22 @@
+set(SLOW_CORNER_DEVICE "qlf_k4n8-qlf_k4n8_umc22_slow")
+set(FAST_CORNER_DEVICE "qlf_k4n8-qlf_k4n8_umc22_fast")
+
 add_quicklogic_board(
-  BOARD      qlf_k4n8-qlf_k4n8_umc22_board
+  BOARD      ${FAST_CORNER_DEVICE}_board
   FAMILY     qlf_k4n8
-  DEVICE     qlf_k4n8-qlf_k4n8_umc22
+  DEVICE     ${FAST_CORNER_DEVICE}
   PINMAP_XML interface-mapping_24x24.xml
-  PACKAGE    qlf_k4n8-qlf_k4n8_umc22
+  PACKAGE    ${FAST_CORNER_DEVICE}
+  PINMAP     qlf_k4n8-qlf_k4n8_umc22.csv
+  FABRIC_PACKAGE qlf_k4n8_umc22
+)
+
+add_quicklogic_board(
+  BOARD      ${SLOW_CORNER_DEVICE}_board
+  FAMILY     qlf_k4n8
+  DEVICE     ${SLOW_CORNER_DEVICE}
+  PINMAP_XML interface-mapping_24x24.xml
+  PACKAGE    ${SLOW_CORNER_DEVICE}
   PINMAP     qlf_k4n8-qlf_k4n8_umc22.csv
   FABRIC_PACKAGE qlf_k4n8_umc22
 )
