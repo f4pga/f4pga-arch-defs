3a9a4d7188239b09951e5971e3f432d22a235ae2
Alessandro Comodi <acomodi@antmicro.com>
2021-05-17 14:40:41 +0200

    quicklogic: improve toolchain with bit generation
    This commit was created by squashing multiple commits to have the
    following feature:
    
    - toolchain wrappers able to generate bitstreams and 4byte data
    - usage of the external qlf-fasm library
    - bump of qlfpga-symbiflow-plugins with correct architecture data
    
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>
    Co-authored-by: Maciej Kurc <mkurc@antmicro.com>
    Co-authored-by: Alessandro Comodi <acomodi@antmicro.com>
    Co-authored-by: Lalit Sharma <lsharma@quicklogic.com>
    Co-authored-by: kkumar23 <kkumar@quicklogic.com>
    Co-authored-by: Tarachand Pagarani <tpagarani@quicklogic.com>

--- quicklogic/common/toolchain_wrappers/conda_build_install_package.sh
+++ quicklogic/common/toolchain_wrappers/conda_build_install_package.sh
@@ -25,7 +25,7 @@ conda update $CONDA_FLAGS -q conda
 curl https://storage.googleapis.com/symbiflow-arch-defs-install/quicklogic-arch-defs-63c3d8f9.tar.gz --output arch.tar.gz
 tar -C $INSTALL_DIR -xvf arch.tar.gz && rm arch.tar.gz
 conda install $CONDA_FLAGS -c litex-hub/label/main yosys="0.9_5266_g0fb4224e 20210301_104249_py37"
-conda install $CONDA_FLAGS -c litex-hub/label/main symbiflow-yosys-plugins="1.0.0_7_284_gb6c5f5d 20210318_102115"
+conda install $CONDA_FLAGS -c litex-hub/label/main symbiflow-yosys-plugins="1.0.0_7_307_gc14d794=20210420_072542"
 conda install $CONDA_FLAGS -c litex-hub/label/main vtr-optimized="8.0.0_3452_ge7d45e013 20210318_102115"
 conda install $CONDA_FLAGS -c litex-hub iverilog
 conda install $CONDA_FLAGS -c tfors gtkwave
@@ -34,4 +34,5 @@ conda activate
 pip install python-constraint
 pip install serial
 pip install git+https://github.com/QuickLogic-Corp/quicklogic-fasm@318abca
+pip install git+https://github.com/QuickLogic-Corp/ql_fasm@e7d0f2fdf5c404b621b24e78fdea51fcb1937672
 conda deactivate
