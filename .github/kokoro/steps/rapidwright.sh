69d4a038ac106e32442ab35df57650d014adf4b2
Alessandro Comodi <acomodi@antmicro.com>
2021-06-24 14:14:06 +0200

    kokoro: update to jdk 11
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/kokoro/steps/rapidwright.sh
+++ .github/kokoro/steps/rapidwright.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-RAPIDWRIGHT_TAG="v2020.2.4-beta"
-
 export RAPIDWRIGHT_PATH=$(pwd)/github/$KOKORO_DIR/env/RapidWright
 mkdir -p "${RAPIDWRIGHT_PATH}"
 
-# Using SymbiFlow/RapidWright fork to control ingestion of upstream merges.
 git clone https://github.com/Xilinx/RapidWright.git "${RAPIDWRIGHT_PATH}"
 pushd "${RAPIDWRIGHT_PATH}"
-git checkout $RAPIDWRIGHT_TAG
 make update_jars
-pushd interchange
-make all
-popd
 popd
