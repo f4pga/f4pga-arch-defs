antmicro/symbiflow-arch-defs

Added MMCM support to placement constraint generator
Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

xc/common/utils/prjxray_create_place_constraints.py
@@ -23,6 +23,22 @@ CLOCKS = {
             "type":
                 "PLLE2_ADV",
         },
+    "MMCME2_ADV_VPR":
+        {
+            "sinks":
+                frozenset(("CLKFBIN", "CLKIN1", "CLKIN2", "DCLK", "PSCLK")),
+            "sources":
+                frozenset(
+                    (
+                        "CLKFBOUT", "CLKFBOUTB", "CLKOUT0", "CLKOUT0B",
+                        "CLKOUT1", "CLKOUT1B", "CLKOUT2", "CLKOUT2B",
+                        "CLKOUT3", "CLKOUT3B", "CLKOUT4", "CLKOUT5",
+                        "CLKOUT6",
+                    )
+                ),
+            "type":
+                "MMCME2_ADV",
+        },
     "BUFGCTRL_VPR":
         {
             "sinks": frozenset(("I0", "I1")),
@@ -228,6 +244,7 @@ class ClockPlacer(object):
             'BOT': [],
         }
         self.pll_cmts = list()
+        self.mmcm_cmts = list()
 
         cmt_dict = vpr_grid.get_cmt_dict()
         site_type_dict = vpr_grid.get_site_type_dict()
@@ -270,6 +287,9 @@ class ClockPlacer(object):
 
         for _, _, clk_region in site_type_dict['PLLE2_ADV']:
             self.pll_cmts.append(clk_region)
+
+        for _, _, clk_region in site_type_dict['MMCME2_ADV']:
+            self.mmcm_cmts.append(clk_region)
 
         self.input_pins = {}
         if not self.roi:
@@ -467,6 +487,11 @@ class ClockPlacer(object):
                 if CLOCKS[clock['subckt']]['type'] == 'PLLE2_ADV':
                     problem.addConstraint(
                         lambda cmt: cmt in self.pll_cmts, (clock_name, )
+                    )
+
+                if CLOCKS[clock['subckt']]['type'] == 'MMCME2_ADV':
+                    problem.addConstraint(
+                        lambda cmt: cmt in self.mmcm_cmts, (clock_name, )
                     )
 
                 if net in self.input_pins:

f4pga/f4pga-arch-defs

xc/common/utils/prjxray_create_place_constraints.py
@@ -25,6 +25,22 @@ CLOCKS = {
             "type":
                 "PLLE2_ADV",
         },
+    "MMCME2_ADV_VPR":
+        {
+            "sinks":
+                frozenset(("CLKFBIN", "CLKIN1", "CLKIN2", "DCLK", "PSCLK")),
+            "sources":
+                frozenset(
+                    (
+                        "CLKFBOUT", "CLKFBOUTB", "CLKOUT0", "CLKOUT0B",
+                        "CLKOUT1", "CLKOUT1B", "CLKOUT2", "CLKOUT2B",
+                        "CLKOUT3", "CLKOUT3B", "CLKOUT4", "CLKOUT5",
+                        "CLKOUT6",
+                    )
+                ),
+            "type":
+                "MMCME2_ADV",
+        },
     "BUFGCTRL_VPR":
         {
             "sinks": frozenset(("I0", "I1")),
@@ -259,6 +275,7 @@ class ClockPlacer(object):
             'BOT': [],
         }
         self.pll_cmts = set()
+        self.mmcm_cmts = set()
         self.gtp_cmts = set()
 
         cmt_dict = vpr_grid.get_cmt_dict()
@@ -307,6 +324,9 @@ class ClockPlacer(object):
             assert "IBUFDS_GTE2" in site_type_dict
             for _, _, clk_region in site_type_dict['IBUFDS_GTE2']:
                 self.gtp_cmts.add(clk_region)
+
+        for _, _, clk_region in site_type_dict['MMCME2_ADV']:
+            self.mmcm_cmts.add(clk_region)
 
         self.input_pins = {}
         if not self.roi:
@@ -522,6 +542,11 @@ class ClockPlacer(object):
                 if CLOCKS[clock['subckt']]['type'] == 'PLLE2_ADV':
                     problem.addConstraint(
                         lambda cmt: cmt in self.pll_cmts, (clock_name, )
+                    )
+
+                if CLOCKS[clock['subckt']]['type'] == 'MMCME2_ADV':
+                    problem.addConstraint(
+                        lambda cmt: cmt in self.mmcm_cmts, (clock_name, )
                     )
 
                 if net in self.input_pins: