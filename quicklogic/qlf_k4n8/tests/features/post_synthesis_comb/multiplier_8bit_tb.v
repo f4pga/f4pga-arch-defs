d9f8fd86cc5f9789b182af0f953a9a5c2ceb8c37
Pawel Czarnecki <pczarnecki@antmicro.com>
2022-05-19 15:30:57 +0200

    ql: qlf_k4n8: tests: fix tests IO
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- quicklogic/qlf_k4n8/tests/features/post_synthesis_comb/multiplier_8bit_tb.v
+++ quicklogic/qlf_k4n8/tests/features/post_synthesis_comb/multiplier_8bit_tb.v
@@ -8,7 +8,9 @@ module multiplier_8bit_tb( );
     reg status; 
 
     multiplier_8bit DUT (
-        .\a_in[0] (x0[0]), .\a_in[1] (x0[1]), .\a_in[2] (x0[2]), .\a_in[3] (x0[3]), .\a_in[4] (x0[4]), .\a_in[5] (x0[5]), .\a_in[6] (x0[6]), .\a_in[7] (x0[7]), .\b_in[0] (y0[0]), .\b_in[1] (y0[1]), .\b_in[2] (y0[2]), .\b_in[3] (y0[3]), .\b_in[4] (y0[4]), .\b_in[5] (y0[5]), .\b_in[6] (y0[6]), .\b_in[7] (y0[7]),	.\prod[0] (mult_out[0]), .\prod[1] (mult_out[1]), .\prod[2] (mult_out[2]), .\prod[3] (mult_out[3]), .\prod[4] (mult_out[4]), .\prod[5] (mult_out[5]), .\prod[6] (mult_out[6]), .\prod[7] (mult_out[7]), .\prod[8] (mult_out[8]), .\prod[9] (mult_out[9]), .\prod[10] (mult_out[10]), .\prod[11] (mult_out[11]), .\prod[12] (mult_out[12]), .\prod[13] (mult_out[13]), .\prod[14] (mult_out[14]), .\prod[15] (mult_out[15]));
+        .a_in(x0),
+	.b_in(y0),
+	.prod(mult_out));
 
     initial begin
 	    status = 0;
