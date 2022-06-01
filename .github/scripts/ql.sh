96abc6cec0648dd659f59d4b93acd71d2ce4c776
Pawel Czarnecki <pczarnecki@antmicro.com>
2022-06-01 12:15:22 +0200

    CI: scripts: QL: set env variables for installed toolchain tests
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- .github/scripts/ql.sh
+++ .github/scripts/ql.sh
@@ -6,6 +6,8 @@ INSTALL_DIR="$(pwd)/install"
 
 export CMAKE_FLAGS="-GNinja -DINSTALL_FAMILIES=qlf_k4n8,pp3 -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
 
+export F4PGA_FAM=eos-s3
+export F4PGA_INSTALL_DIR="placeholder"
 source $(dirname "$0")/setup-and-activate.sh
 
 pushd build
@@ -17,6 +19,8 @@ heading "Running installed toolchain tests"
 (
 	pushd build
 	export CTEST_OUTPUT_ON_FAILURE=1
+	export F4PGA_ENV_SHARE=${INSTALL_DIR}/share/symbiflow
+	export F4PGA_ENV_BIN=${INSTALL_DIR}/bin/
 	heading "Testing installed toolchain on qlf_k4n8"
 	ctest -j${MAX_CORES} -R "quicklogic_toolchain_test_.*_qlf_k4n8" -VV
 	echo "----------------------------------------"
