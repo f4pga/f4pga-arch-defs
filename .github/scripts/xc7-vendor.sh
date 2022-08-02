10313f0ab9b9688e00f91b362065d4a8cb8f1cae
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-02 03:41:05 +0200

    ci/scripts: reuse xc7 for xc7-vendor
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- .github/scripts/xc7-vendor.sh
+++ .github/scripts/xc7-vendor.sh
@@ -9,18 +9,7 @@ enable_vivado 2017.2
 # https://support.xilinx.com/s/article/63253?language=en_US
 export XILINX_LOCAL_USER_DATA="no"
 
-export CMAKE_FLAGS="-GNinja"
-source $(dirname "$0")/setup-and-activate.sh
-
-pushd build
-
 case "$1" in
-  a200t)
-    make_target all_artix7_200t_diff_fasm "Running xc7 200T vendor tests (make all_xc7_200t_diff_fasm)" 0
-  ;;
-  *)
-    make_target all_xc7_diff_fasm "Running xc7 vendor tests (make all_xc7_diff_fasm)" 0
-  ;;
+  a200t) $(dirname "$0")/xc7.sh a200t-vendor ;;
+  *)     $(dirname "$0")/xc7.sh vendor ;;
 esac
-
-popd
--- .github/scripts/xc7.sh
+++ .github/scripts/xc7.sh
@@ -6,12 +6,10 @@ source $(dirname "$0")/setup-and-activate.sh
 pushd build
 
 case "$1" in
-  a200t)
-    make_target all_xc7_200t "Running xc7 200T tests (make all_xc7_200t)"
-  ;;
-  *)
-    make_target all_xc7 "Running xc7 tests (make all_xc7)"
-  ;;
+  a200t-vendor) make_target all_artix7_200t_diff_fasm "Running xc7 200T vendor tests (make all_xc7_200t_diff_fasm)" 0 ;;
+  vendor)       make_target all_xc7_diff_fasm "Running xc7 vendor tests (make all_xc7_diff_fasm)" 0 ;;
+  a200t)        make_target all_xc7_200t "Running xc7 200T tests (make all_xc7_200t)" ;;
+  *)            make_target all_xc7 "Running xc7 tests (make all_xc7)" ;;
 esac
 
 ninja print_qor > xc7_qor.csv
