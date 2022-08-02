8e4332e3528b3cb5deb285ec637ee4cc4b1126ac
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-02 03:34:07 +0200

    ci/scripts: reuse xc7 for xc7a200t
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- .github/scripts/xc7.sh
+++ .github/scripts/xc7.sh
@@ -5,7 +5,15 @@ source $(dirname "$0")/setup-and-activate.sh
 
 pushd build
 
-make_target all_xc7 "Running xc7 tests (make all_xc7)"
+case "$1" in
+  a200t)
+    make_target all_xc7_200t "Running xc7 200T tests (make all_xc7_200t)"
+  ;;
+  *)
+    make_target all_xc7 "Running xc7 tests (make all_xc7)"
+  ;;
+esac
+
 ninja print_qor > xc7_qor.csv
 
 popd
--- .github/scripts/xc7a200t.sh
+++ .github/scripts/xc7a200t.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-export CMAKE_FLAGS="-GNinja"
-
-source $(dirname "$0")/setup-and-activate.sh
-
-pushd build
-
-make_target all_xc7_200t "Running xc7 200T tests (make all_xc7_200t)"
-ninja print_qor > xc7_qor.csv
-
-popd
+source $(dirname "$0")/xc7.sh a200t
