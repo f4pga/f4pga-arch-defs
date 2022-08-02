1d57c800cc53220da7e10b87b7e6fb5f739a9de5
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-02 03:35:55 +0200

    ci/scripts: reuse xc7-vendor for xc7a200t-vendor
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- .github/scripts/xc7-vendor.sh
+++ .github/scripts/xc7-vendor.sh
@@ -5,16 +5,22 @@ source $(dirname "$0")/common.sh
 
 enable_vivado 2017.2
 
-export CMAKE_FLAGS="-GNinja"
-
 # Fix Xilinx TCL app store issues with multiple jobs:
 # https://support.xilinx.com/s/article/63253?language=en_US
 export XILINX_LOCAL_USER_DATA="no"
 
+export CMAKE_FLAGS="-GNinja"
 source $(dirname "$0")/setup-and-activate.sh
 
 pushd build
 
-make_target all_xc7_diff_fasm "Running xc7 vendor tests (make all_xc7_diff_fasm)" 0
+case "$1" in
+  a200t)
+    make_target all_artix7_200t_diff_fasm "Running xc7 200T vendor tests (make all_xc7_200t_diff_fasm)" 0
+  ;;
+  *)
+    make_target all_xc7_diff_fasm "Running xc7 vendor tests (make all_xc7_diff_fasm)" 0
+  ;;
+esac
 
 popd
